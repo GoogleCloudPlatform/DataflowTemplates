2b28af8dd3223ab9cb373a8db4e0f99188754e59
realiteer <miked@infusionsoft.com>
2019-06-07 22:08:58 -0700

    DATA-1281 - added template to migrate email content to Cloud Storage from Datastore

--- src/main/java/com/infusionsoft/dataflow/templates/migration/EmailContent.java
+++ src/main/java/com/infusionsoft/dataflow/templates/migration/EmailContent.java
@@ -21,6 +21,8 @@ import org.apache.http.entity.ContentType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Map;
+
 /**
  * A template that moves email content to cloud storage (because that is 10x cheaper than using Datastore)
  *
