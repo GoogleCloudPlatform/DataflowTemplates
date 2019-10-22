cebd4e5098167cb9cdeaddc5717d5ebae90ea47d
Behrooz Shafiee <behrooz.shafieesarjaz@shopify.com>
2019-10-22 13:30:06 -0700

    addes a template for exporting Bigtable to partitioned parquet

---
+++ src/main/java/com/google/cloud/teleport/bigtable/BigtableToPartitionedParquet.java
@@ -0,0 +1,264 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.cloud.teleport.bigtable;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.List;
+import com.google.bigtable.v2.Cell;
+import com.google.bigtable.v2.Column;
+import com.google.bigtable.v2.Family;
+import com.google.bigtable.v2.Row;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.generic.GenericRecordBuilder;
+import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.PipelineResult;
+import org.apache.beam.sdk.coders.AvroCoder;
+import org.apache.beam.sdk.coders.StringUtf8Coder;
+import org.apache.beam.sdk.io.FileIO;
+import org.apache.beam.sdk.io.gcp.bigtable.BigtableIO;
+import org.apache.beam.sdk.io.parquet.ParquetIO;
+import org.apache.beam.sdk.options.Description;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.options.ValueProvider;
+import org.apache.beam.sdk.transforms.Combine;
+import org.apache.beam.sdk.transforms.Count;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.PTransform;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.View;
+import org.apache.beam.sdk.transforms.windowing.FixedWindows;
+import org.apache.beam.sdk.transforms.windowing.Window;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.PCollectionView;
+import org.joda.time.Duration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * Dataflow pipeline that exports data from a Cloud Bigtable table to partitioned Parquet
+ * files in GCS.
+ */
+public class BigtableToPartitionedParquet {
+
+  private static final Logger LOG = LoggerFactory.getLogger(BigtableToPartitionedParquet.class);
+
+  /** Options for the export pipeline. */
+  public interface Options extends PipelineOptions {
+    @Description("The project that contains the table to export.")
+    ValueProvider<String> getBigtableProjectId();
+
+    @SuppressWarnings("unused")
+    void setBigtableProjectId(ValueProvider<String> projectId);
+
+    @Description("The Bigtable instance id that contains the table to export.")
+    ValueProvider<String> getBigtableInstanceId();
+
+    @SuppressWarnings("unused")
+    void setBigtableInstanceId(ValueProvider<String> instanceId);
+
+    @Description("The Bigtable table id to export.")
+    ValueProvider<String> getBigtableTableId();
+
+    @SuppressWarnings("unused")
+    void setBigtableTableId(ValueProvider<String> tableId);
+
+    @Description("The output location to write to (e.g. gs://mybucket/somefolder/)")
+    ValueProvider<String> getOutputDirectory();
+
+    @SuppressWarnings("unused")
+    void setOutputDirectory(ValueProvider<String> outputDirectory);
+
+    @Description("The prefix for each exported file in outputDirectory")
+    ValueProvider<String> getFilenamePrefix();
+
+    @SuppressWarnings("unused")
+    void setFilenamePrefix(ValueProvider<String> filenamePrefix);
+  }
+
+  /**
+   * Runs a pipeline to export data from a Cloud Bigtable table to Parquet files in
+   * GCS.
+   *
+   * @param args arguments to the pipeline
+   */
+  public static void main(String[] args) {
+    Options options = PipelineOptionsFactory.fromArgs(args).withValidation().as(Options.class);
+
+    PipelineResult result = run(options);
+
+    // Wait for pipeline to finish only if it is not constructing a template.
+    if (options.as(DataflowPipelineOptions.class).getTemplateLocation() == null) {
+      result.waitUntilFinish();
+    }
+  }
+
+  public static String getTableSchema() {
+    String avroSchemaStr = "";
+    try {
+      avroSchemaStr = new String(Files.readAllBytes(Paths
+          .get("src/main/resources/schema/avro/bigtable.avsc")));
+    } catch (IOException e) {
+      LOG.error("failed to load schema");
+    }
+    return avroSchemaStr;
+  }
+
+  public static PipelineResult run(Options options) {
+    Pipeline pipeline = Pipeline.create(options);
+
+    BigtableIO.Read read =
+        BigtableIO.read()
+            .withProjectId(options.getBigtableProjectId())
+            .withInstanceId(options.getBigtableInstanceId())
+            .withTableId(options.getBigtableTableId());
+
+    // Do not validate input fields if it is running as a template.
+    if (options.as(DataflowPipelineOptions.class).getTemplateLocation() != null) {
+      read = read.withoutValidation();
+    }
+
+    String schemaStr = getTableSchema();
+    Schema schema = new Schema.Parser().parse(schemaStr);
+
+    
+
+    pipeline
+        .apply("Read from Bigtable", read)
+        .apply("Filtering & Transform to GenericRecord",ParDo.of(new BigtableToGenericRecord(schemaStr)))
+        .setCoder(AvroCoder.of(schema))
+        .apply("Windowing to 1 hour windows", Window.<GenericRecord>into(FixedWindows.of(Duration.standardHours(1))))
+        .apply(
+          "WriteToParquet",
+          FileIO.<String, GenericRecord>writeDynamic()
+                .by(record -> {
+                  Long firstCellTS = (Long)((List<GenericRecord>)(record.get("cells"))).get(0).get("timestamp");
+                  OffsetDateTime instant = Instant.ofEpochMilli(firstCellTS / 1000).atOffset(ZoneOffset.UTC);
+                  return instant.getYear() + "/" + instant.getMonthValue() + "/" + instant.getDayOfMonth() + "/" + instant.getHour() + "/";
+                })
+                .withDestinationCoder(StringUtf8Coder.of())
+                .via(ParquetIO.sink(schema))
+                .to(options.getOutputDirectory())
+                .withSharding(new PTransform<PCollection<GenericRecord>,PCollectionView<Integer>>("Window Sharding Transformation"){
+                  @Override
+                  public PCollectionView<Integer> expand(PCollection<GenericRecord> input) {
+                    return input
+                      .apply("Counting window size", Combine.globally(Count.<GenericRecord>combineFn()).withoutDefaults())
+                      .apply("GenerateShardCount", ParDo.of(new CalculateShardsFn()))
+                      .apply(View.asSingleton());
+                  }
+                })
+                .withNaming((entityName) -> FileIO.Write.defaultNaming(entityName, ".parquet"))
+        );
+
+
+    return pipeline.run();
+  }
+
+  static class CalculateShardsFn extends DoFn<Long, Integer> {
+    private final int maxShards = 100;
+    @ProcessElement
+    public void process(ProcessContext ctx) {
+      ctx.output(calculateShards(ctx.element()));
+    }
+ 
+    private int calculateShards(long totalRecords) {
+      if (totalRecords == 0) {
+        // WriteFiles out at least one shard, even if there is no input.
+        return 1;
+      }
+      
+      //1 shard per 1000000 record
+      int shards = (int) totalRecords/1000000;
+      if (shards == 0) {
+        return 1;
+      }
+
+      return Math.min(shards, maxShards);
+    }
+  }
+
+  
+
+  /** Translates Bigtable {@link Row} to Avro {@link BigtableRow}. */
+  static class BigtableToGenericRecord extends DoFn<Row, GenericRecord> {
+    private String schemaStr;
+    private Schema schema = null;
+
+    public BigtableToGenericRecord(String schemaStr) {      
+      this.schemaStr = schemaStr;
+    }
+
+    private Schema getSchema() {
+      if (this.schema == null) {
+        this.schema = new Schema.Parser().parse(schemaStr);
+      }
+      return this.schema;
+    }
+
+    @ProcessElement
+    public void processElement(@Element Row row, OutputReceiver<GenericRecord> out) {
+      Schema schema = this.getSchema();
+      Schema cellsSchema = schema.getField("cells").schema().getElementType();
+
+      GenericRecordBuilder recordBuilder = new GenericRecordBuilder(schema);
+      ByteBuffer key = ByteBuffer.wrap(BigtableToAvro.toByteArray(row.getKey()));
+      recordBuilder.set("key", key);
+
+      long timestamp = 0;
+      List<GenericRecord> cells = new ArrayList<>();
+      for (Family family : row.getFamiliesList()) {
+        String familyName = family.getName();
+        for (Column column : family.getColumnsList()) {
+          ByteBuffer qualifier = ByteBuffer.wrap(BigtableToAvro.toByteArray(column.getQualifier()));
+          for (Cell cell : column.getCellsList()) {
+            if (timestamp == 0) { //assign the timestamp tot he first cell of first column family
+              timestamp = cell.getTimestampMicros();
+            }
+            ByteBuffer value = ByteBuffer.wrap(BigtableToAvro.toByteArray(cell.getValue()));
+            GenericRecord genericCell = new GenericData.Record(cellsSchema);
+            genericCell.put("family", familyName);
+            genericCell.put("qualifier", qualifier);
+            genericCell.put("timestamp", timestamp);
+            genericCell.put("value", value);
+
+            cells.add(genericCell);
+          }
+        }
+      }
+      if (timestamp == 0) {
+        //no cell at all
+        return;
+      }
+
+      recordBuilder.set("cells", cells);
+      org.joda.time.Instant ins =  new org.joda.time.Instant(timestamp / 1000);
+      out.outputWithTimestamp(recordBuilder.build(), ins); //set ts for each element to effectively window them later
+    }
+  }
+}
---
+++ src/test/java/com/google/cloud/teleport/bigtable/BigtableToPartitionedParquetTest.java
@@ -0,0 +1,74 @@
+package com.google.cloud.teleport.bigtable;
+
+import static com.google.cloud.teleport.bigtable.TestUtils.createBigtableRow;
+import static com.google.cloud.teleport.bigtable.TestUtils.upsertBigtableCell;
+
+import java.nio.ByteBuffer;
+import java.util.List;
+
+import com.google.bigtable.v2.Row;
+import com.google.cloud.teleport.bigtable.BigtableToPartitionedParquet.BigtableToGenericRecord;
+import com.google.common.collect.ImmutableList;
+
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.generic.GenericRecordBuilder;
+import org.apache.beam.sdk.coders.AvroCoder;
+import org.apache.beam.sdk.testing.PAssert;
+import org.apache.beam.sdk.testing.TestPipeline;
+import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.values.PCollection;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Tests for BigtableToPartitionedParquetTest. */
+@RunWith(JUnit4.class)
+public final class BigtableToPartitionedParquetTest {
+  @Test
+  public void applyBigtableToGenericRecordFn() throws Exception {
+    Row bigtableRow1 = createBigtableRow("row1");
+    bigtableRow1 = upsertBigtableCell(bigtableRow1, "family1", "column1", 1L, "value1");
+    Row bigtableRow2 = createBigtableRow("row2");
+    bigtableRow2 = upsertBigtableCell(bigtableRow2, "family2", "column2", 1L, "value2");
+    final List<Row> bigtableRows = ImmutableList.of(bigtableRow1, bigtableRow2);
+
+
+    String schemaStr = BigtableToPartitionedParquet.getTableSchema();
+    Schema schema = new Schema.Parser().parse(schemaStr);
+    Schema cellsSchema = schema.getField("cells").schema().getElementType();
+    
+    GenericRecord cell1 = new GenericData.Record(cellsSchema);
+    cell1.put("family", "family1");
+    cell1.put("qualifier", ByteBuffer.wrap("column1".getBytes()));
+    cell1.put("timestamp", 1L);
+    cell1.put("value", ByteBuffer.wrap("value1".getBytes()));
+    GenericRecord record1 = new GenericRecordBuilder(schema)
+      .set("key", ByteBuffer.wrap("row1".getBytes()))
+      .set("cells", ImmutableList.of(cell1))
+      .build();
+
+    GenericRecord cell2 = new GenericData.Record(cellsSchema);
+    cell2.put("family", "family2");
+    cell2.put("qualifier", ByteBuffer.wrap("column2".getBytes()));
+    cell2.put("timestamp", 1L);
+    cell2.put("value", ByteBuffer.wrap("value2".getBytes()));
+    GenericRecord record2 = new GenericRecordBuilder(schema)
+      .set("key", ByteBuffer.wrap("row2".getBytes()))
+      .set("cells", ImmutableList.of(cell2))
+      .build();
+
+    final List<GenericRecord> expectedGenericRows = ImmutableList.of(record1, record2);
+
+
+    TestPipeline pipeline = TestPipeline.create();
+    PCollection<GenericRecord> outputGenericRows = pipeline
+        .apply("Read from Bigtable", Create.of(bigtableRows))
+        .apply("Filter & Transform to GenericRecord",ParDo.of(new BigtableToGenericRecord(schemaStr)))
+        .setCoder(AvroCoder.of(schema));
+    System.err.println("HERE:"+expectedGenericRows.toString());
+    PAssert.that(outputGenericRows).containsInAnyOrder(expectedGenericRows);
+  }
+}
