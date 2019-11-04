0d2402c52e848cf2b6a323d7f9aa3cabab172fbc
melburnerodrigues <melburne.rodrigues@quantiphi.com>
2019-11-04 19:19:48 +0530

    Adding FileFormatConversion flex template.

--- v2/common/src/main/java/com/google/cloud/teleport/v2/utils/SchemaUtils.java
+++ v2/common/src/main/java/com/google/cloud/teleport/v2/utils/SchemaUtils.java
@@ -15,8 +15,27 @@
  */
 package com.google.cloud.teleport.v2.utils;
 
+import static org.apache.beam.vendor.guava.v20_0.com.google.common.base.Preconditions.checkArgument;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.channels.Channels;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.apache.avro.Schema;
+import org.apache.beam.sdk.io.FileSystems;
+import org.apache.beam.sdk.io.fs.MatchResult;
+import org.apache.beam.sdk.io.fs.ResourceId;
+import org.apache.beam.vendor.guava.v20_0.com.google.common.io.CharStreams;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /** Schema utilities for KafkaToBigQuery pipeline. */
 public class SchemaUtils {
+
+  /* Logger for class. */
+  private static final Logger LOG = LoggerFactory.getLogger(SchemaUtils.class);
 
   public static final String DEADLETTER_SCHEMA =
       "{\n"
@@ -65,4 +84,50 @@ public class SchemaUtils {
           + "    }\n"
           + "  ]\n"
           + "}";
+
+  /**
+   * The {@link SchemaUtils#getGcsFileAsString(String)} reads a file from GCS and returns it as a
+   * string.
+   *
+   * @param filePath path to file in GCS
+   * @return contents of the file as a string
+   * @throws IOException thrown if not able to read file
+   */
+  public static String getGcsFileAsString(String filePath) {
+    MatchResult result;
+    try {
+      result = FileSystems.match(filePath);
+      checkArgument(
+          result.status() == MatchResult.Status.OK && !result.metadata().isEmpty(),
+          "Failed to match any files with the pattern: " + filePath);
+
+      List<ResourceId> rId =
+          result.metadata().stream()
+              .map(MatchResult.Metadata::resourceId)
+              .collect(Collectors.toList());
+
+      checkArgument(rId.size() == 1, "Expected exactly 1 file, but got " + rId.size() + " files.");
+
+      Reader reader =
+          Channels.newReader(FileSystems.open(rId.get(0)), StandardCharsets.UTF_8.name());
+
+      return CharStreams.toString(reader);
+
+    } catch (IOException ioe) {
+      LOG.error("File system i/o error: " + ioe.getMessage());
+      throw new RuntimeException(ioe);
+    }
+  }
+
+  /**
+   * The {@link SchemaUtils#getAvroSchema(String)} reads an Avro schema file from GCS, parses it and
+   * returns a new {@link Schema} object.
+   *
+   * @param schemaLocation Path to schema (e.g. gs://mybucket/path/).
+   * @return {@link Schema}
+   */
+  public static Schema getAvroSchema(String schemaLocation) {
+    String schemaFile = getGcsFileAsString(schemaLocation);
+    return new Schema.Parser().parse(schemaFile);
+  }
 }
