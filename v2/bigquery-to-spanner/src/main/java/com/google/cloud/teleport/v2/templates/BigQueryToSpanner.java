25e466da7c5d9048813a44685db7a119cfc8d046
Tim Sell <tsell@google.com>
2020-01-13 17:38:05 +0800

    cleanup unused code

--- v2/bigquery-to-spanner/src/main/java/com/google/cloud/teleport/v2/templates/BigQueryToSpanner.java
+++ v2/bigquery-to-spanner/src/main/java/com/google/cloud/teleport/v2/templates/BigQueryToSpanner.java
@@ -15,16 +15,9 @@
  */
 package com.google.cloud.teleport.v2.templates;
 
-import com.google.api.gax.rpc.InvalidArgumentException;
-import com.google.api.services.bigquery.model.TableReference;
-import com.google.cloud.bigquery.storage.v1beta1.BigQueryStorageClient;
 import com.google.cloud.bigquery.storage.v1beta1.ReadOptions.TableReadOptions;
-import com.google.cloud.bigquery.storage.v1beta1.Storage.CreateReadSessionRequest;
-import com.google.cloud.bigquery.storage.v1beta1.Storage.ReadSession;
-import com.google.cloud.bigquery.storage.v1beta1.TableReferenceProto;
 import com.google.cloud.spanner.Mutation.Op;
 import com.google.common.base.Preconditions;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import org.apache.beam.sdk.Pipeline;
@@ -235,65 +228,5 @@ public class BigQueryToSpanner {
     Op getSpannerOperation();
 
     void setSpannerOperation(Op spannerDatabase);
-  }
-
-
-  /**
-   * Factory to create BigQueryStorageClients.
-   */
-  static class BigQueryStorageClientFactory {
-
-    /**
-     * Creates BigQueryStorage client for use in extracting table schema.
-     *
-     * @return BigQueryStorageClient
-     */
-    static BigQueryStorageClient create() {
-      try {
-        return BigQueryStorageClient.create();
-      } catch (IOException e) {
-        LOG.error("Error connecting to BigQueryStorage API: " + e.getMessage());
-        throw new RuntimeException(e);
-      }
-    }
-  }
-
-  /**
-   * Factory to create ReadSessions.
-   */
-  static class ReadSessionFactory {
-
-    /**
-     * Creates ReadSession for schema extraction.
-     *
-     * @param client BigQueryStorage client used to create ReadSession.
-     * @param tableString String that represents table to export from.
-     * @param tableReadOptions TableReadOptions that specify any fields in the table to filter on.
-     * @return session ReadSession object that contains the schema for the export.
-     */
-    static ReadSession create(
-        BigQueryStorageClient client, String tableString, TableReadOptions tableReadOptions) {
-      TableReference tableReference = BigQueryHelpers.parseTableSpec(tableString);
-      String parentProjectId = "projects/" + tableReference.getProjectId();
-
-      TableReferenceProto.TableReference storageTableRef =
-          TableReferenceProto.TableReference.newBuilder()
-              .setProjectId(tableReference.getProjectId())
-              .setDatasetId(tableReference.getDatasetId())
-              .setTableId(tableReference.getTableId())
-              .build();
-
-      CreateReadSessionRequest.Builder builder =
-          CreateReadSessionRequest.newBuilder()
-              .setParent(parentProjectId)
-              .setReadOptions(tableReadOptions)
-              .setTableReference(storageTableRef);
-      try {
-        return client.createReadSession(builder.build());
-      } catch (InvalidArgumentException iae) {
-        LOG.error("Error creating ReadSession: " + iae.getMessage());
-        throw new RuntimeException(iae);
-      }
-    }
   }
 }
