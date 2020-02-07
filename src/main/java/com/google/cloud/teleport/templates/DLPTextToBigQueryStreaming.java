a1cd7ac3f1ce4147915f65d1b0ce5e32ac3cb003
Cloud Teleport <cloud-teleport@google.com>
2020-02-07 12:28:56 -0800

    bump beam version to 2.18
    PiperOrigin-RevId: 293870819

--- src/main/java/com/google/cloud/teleport/templates/DLPTextToBigQueryStreaming.java
+++ src/main/java/com/google/cloud/teleport/templates/DLPTextToBigQueryStreaming.java
@@ -72,6 +72,7 @@ import org.apache.beam.sdk.transforms.splittabledofn.OffsetRangeTracker;
 import org.apache.beam.sdk.transforms.splittabledofn.RestrictionTracker;
 import org.apache.beam.sdk.transforms.windowing.AfterProcessingTime;
 import org.apache.beam.sdk.transforms.windowing.FixedWindows;
+import org.apache.beam.sdk.transforms.windowing.Repeatedly;
 import org.apache.beam.sdk.transforms.windowing.Window;
 import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
@@ -174,7 +175,7 @@ public class DLPTextToBigQueryStreaming {
     Pipeline p = Pipeline.create(options);
     /*
      * Steps:
-     *   1) Read from the text source continuously based on default interval e.g. 300 seconds
+     *   1) Read from the text source continuously based on default interval e.g. 30 seconds
      *       - Setup a window for 30 secs to capture the list of files emited.
      *       - Group by file name as key and ReadableFile as a value.
      *   2) Create a side input for the window containing list of headers par file.
@@ -203,8 +204,8 @@ public class DLPTextToBigQueryStreaming {
             .apply(
                 "Fixed Window(30 Sec)",
                 Window.<KV<String, ReadableFile>>into(FixedWindows.of(WINDOW_INTERVAL))
-                    .triggering(
-                        AfterProcessingTime.pastFirstElementInPane().plusDelayOf(Duration.ZERO))
+                    .triggering(Repeatedly.forever(
+                        AfterProcessingTime.pastFirstElementInPane().plusDelayOf(Duration.ZERO)))
                     .discardingFiredPanes()
                     .withAllowedLateness(Duration.ZERO))
             .apply(GroupByKey.create());
