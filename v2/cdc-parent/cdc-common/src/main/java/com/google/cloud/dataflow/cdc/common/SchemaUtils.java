88cd165dca855099af95385715ba54d384b47810
Eugene Nikolayev <eugene.nikolayev@gmail.com>
2020-04-25 10:05:37 +0300

    CDC Template: add support for INT16 data type (MySQL TINYINT).

--- v2/cdc-parent/cdc-common/src/main/java/com/google/cloud/dataflow/cdc/common/SchemaUtils.java
+++ v2/cdc-parent/cdc-common/src/main/java/com/google/cloud/dataflow/cdc/common/SchemaUtils.java
@@ -40,6 +40,7 @@ public class SchemaUtils {
           .put("BOOL", org.apache.beam.sdk.schemas.Schema.TypeName.BOOLEAN)
           .put("BYTES", org.apache.beam.sdk.schemas.Schema.TypeName.BYTES)
           .put("DOUBLE", org.apache.beam.sdk.schemas.Schema.TypeName.DOUBLE)
+          .put("INT16", org.apache.beam.sdk.schemas.Schema.TypeName.INT16)
           .put("INT32", org.apache.beam.sdk.schemas.Schema.TypeName.INT32)
           .put("INT64", org.apache.beam.sdk.schemas.Schema.TypeName.INT64)
           .put("STRING", org.apache.beam.sdk.schemas.Schema.TypeName.STRING)
