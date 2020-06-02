46d79653195b2566e618d9d1ed3ee90f120b0ac9
Balázs Németh <nbali@users.noreply.github.com>
2020-06-03 00:09:20 +0200

    Fix for having no whitelisted tables at all in the CDC Example

--- v2/cdc-parent/cdc-embedded-connector/src/main/java/com/google/cloud/dataflow/cdc/connector/DebeziumMysqlToPubSubDataSender.java
+++ v2/cdc-parent/cdc-embedded-connector/src/main/java/com/google/cloud/dataflow/cdc/connector/DebeziumMysqlToPubSubDataSender.java
@@ -91,7 +91,6 @@ public class DebeziumMysqlToPubSubDataSender implements Runnable {
 
     this.whitelistedTables = whitelistedTables;
 
-
     // Prepare Debezium's table.whitelist property by removing
     // instance name from each of the whitelisted tables specified.
     String dbzWhitelistedTables = whitelistedTables.stream()
@@ -109,8 +108,12 @@ public class DebeziumMysqlToPubSubDataSender implements Runnable {
         .with("database.password", this.mysqlUserPassword)
         .with("database.server.name", mysqlDatabaseInstanceName)
         .with("decimal.handling.mode", "string")
-        .with(MySqlConnectorConfig.TABLE_WHITELIST, dbzWhitelistedTables)
         .with(MySqlConnectorConfig.DATABASE_HISTORY, MemoryDatabaseHistory.class.getName());
+
+    if (!whitelistedTables.isEmpty()) {
+      LOG.info("Whitelisting tables: {}", dbzWhitelistedTables);
+      configBuilder = configBuilder.with(MySqlConnectorConfig.TABLE_WHITELIST, dbzWhitelistedTables);
+    }
 
     if (this.inMemoryOffsetStorage) {
       LOG.info("Setting up in memory offset storage.");
