f03c19cf7f4f9e333beebe4a5700e2b4d2168a02
parvathareddy <parvathareddy@google.com>
2020-11-30 10:03:13 -0800

    Fixed formatting and license header
    PiperOrigin-RevId: 344834222

--- v2/kafka-to-pubsub/src/main/java/com/google/cloud/teleport/v2/transforms/FormatTransform.java
+++ v2/kafka-to-pubsub/src/main/java/com/google/cloud/teleport/v2/transforms/FormatTransform.java
@@ -1,17 +1,17 @@
 /*
  * Copyright (C) 2020 Google Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.google.cloud.teleport.v2.transforms;
 
@@ -34,81 +34,76 @@ import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PCollectionTuple;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
-/**
- * Different transformations over the processed data in the pipeline.
- */
+/** Different transformations over the processed data in the pipeline. */
 public class FormatTransform {
 
-    /**
-     * Configures Kafka consumer.
-     *
-     * @param bootstrapServers Kafka servers to read from
-     * @param topicsList       Kafka topics to read from
-     * @param config           configuration for the Kafka consumer
-     * @return PCollection of Kafka Key & Value Pair deserialized in string format
-     */
-    public static PTransform<PBegin, PCollection<KV<String, String>>> readFromKafka(
-            String bootstrapServers,
-            List<String> topicsList,
-            Map<String, Object> config,
-            @Nullable Map<String, String> sslConfig
-    ) {
-        KafkaIO.Read<String, String> kafkaRecords = KafkaIO.<String, String>read()
-                .withBootstrapServers(bootstrapServers)
-                .withTopics(topicsList)
-                .withKeyDeserializerAndCoder(
-                        StringDeserializer.class, NullableCoder.of(StringUtf8Coder.of()))
-                .withValueDeserializerAndCoder(
-                        StringDeserializer.class, NullableCoder.of(StringUtf8Coder.of()))
-                .withConsumerConfigUpdates(config);
-        if (sslConfig != null) {
-            kafkaRecords = kafkaRecords.withConsumerFactoryFn(new SslConsumerFactoryFn(sslConfig));
-        }
-        return kafkaRecords.withoutMetadata();
+  /**
+   * Configures Kafka consumer.
+   *
+   * @param bootstrapServers Kafka servers to read from
+   * @param topicsList Kafka topics to read from
+   * @param config configuration for the Kafka consumer
+   * @return PCollection of Kafka Key & Value Pair deserialized in string format
+   */
+  public static PTransform<PBegin, PCollection<KV<String, String>>> readFromKafka(
+      String bootstrapServers,
+      List<String> topicsList,
+      Map<String, Object> config,
+      @Nullable Map<String, String> sslConfig) {
+    KafkaIO.Read<String, String> kafkaRecords =
+        KafkaIO.<String, String>read()
+            .withBootstrapServers(bootstrapServers)
+            .withTopics(topicsList)
+            .withKeyDeserializerAndCoder(
+                StringDeserializer.class, NullableCoder.of(StringUtf8Coder.of()))
+            .withValueDeserializerAndCoder(
+                StringDeserializer.class, NullableCoder.of(StringUtf8Coder.of()))
+            .withConsumerConfigUpdates(config);
+    if (sslConfig != null) {
+      kafkaRecords = kafkaRecords.withConsumerFactoryFn(new SslConsumerFactoryFn(sslConfig));
+    }
+    return kafkaRecords.withoutMetadata();
+  }
 
+  /**
+   * The {@link MessageToFailsafeElementFn} wraps an Kafka Message with the {@link FailsafeElement}
+   * class so errors can be recovered from and the original message can be output to a error records
+   * table.
+   */
+  static class MessageToFailsafeElementFn
+      extends DoFn<KV<String, String>, FailsafeElement<KV<String, String>, String>> {
+
+    @ProcessElement
+    public void processElement(ProcessContext context) {
+      KV<String, String> message = context.element();
+      context.output(FailsafeElement.of(message, message.getValue()));
+    }
+  }
+
+  /** The {@link UdfProcess} enables usage of the User Defined Functions in the pipeline. */
+  public static class UdfProcess
+      extends PTransform<PCollection<KV<String, String>>, PCollectionTuple> {
+
+    private final KafkaToPubsubOptions options;
+
+    public UdfProcess(KafkaToPubsubOptions options) {
+      this.options = options;
     }
 
-    /**
-     * The {@link MessageToFailsafeElementFn} wraps an Kafka Message with the {@link FailsafeElement}
-     * class so errors can be recovered from and the original message can be output to a error records
-     * table.
-     */
-    static class MessageToFailsafeElementFn
-            extends DoFn<KV<String, String>, FailsafeElement<KV<String, String>, String>> {
-
-        @ProcessElement
-        public void processElement(ProcessContext context) {
-            KV<String, String> message = context.element();
-            context.output(FailsafeElement.of(message, message.getValue()));
-        }
+    @Override
+    public PCollectionTuple expand(PCollection<KV<String, String>> input) {
+      return input
+          // Map the incoming messages into FailsafeElements so we can recover from failures
+          // across multiple transforms.
+          .apply("mapToRecord", ParDo.of(new MessageToFailsafeElementFn()))
+          .apply(
+              "invokeUDF",
+              JavascriptTextTransformer.FailsafeJavascriptUdf.<KV<String, String>>newBuilder()
+                  .setFileSystemPath(options.getJavascriptTextTransformGcsPath())
+                  .setFunctionName(options.getJavascriptTextTransformFunctionName())
+                  .setSuccessTag(KafkaPubsubConstants.UDF_OUT)
+                  .setFailureTag(KafkaPubsubConstants.UDF_DEADLETTER_OUT)
+                  .build());
     }
-
-    /**
-     * The {@link UdfProcess} enables usage of the User Defined Functions in the pipeline.
-     */
-    public static class UdfProcess
-            extends PTransform<PCollection<KV<String, String>>, PCollectionTuple> {
-
-        private final KafkaToPubsubOptions options;
-
-        public UdfProcess(KafkaToPubsubOptions options) {
-            this.options = options;
-        }
-
-        @Override
-        public PCollectionTuple expand(PCollection<KV<String, String>> input) {
-            return input
-                    // Map the incoming messages into FailsafeElements so we can recover from failures
-                    // across multiple transforms.
-                    .apply("mapToRecord", ParDo.of(new MessageToFailsafeElementFn()))
-                    .apply(
-                            "invokeUDF",
-                            JavascriptTextTransformer.FailsafeJavascriptUdf.<KV<String, String>>newBuilder()
-                                    .setFileSystemPath(options.getJavascriptTextTransformGcsPath())
-                                    .setFunctionName(options.getJavascriptTextTransformFunctionName())
-                                    .setSuccessTag(KafkaPubsubConstants.UDF_OUT)
-                                    .setFailureTag(KafkaPubsubConstants.UDF_DEADLETTER_OUT)
-                                    .build());
-        }
-    }
+  }
 }
