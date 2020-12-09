73c03fa0f62c42a6837d739f86fa7300ebfc6287
Ilya Kozyrev <ilya.kozyrev@akvelon.com>
2020-12-09 09:09:07 +0300

    added licence

--- v2/protegrity-data-tokenization/src/main/java/com/google/cloud/teleport/v2/transforms/io/BigTableIO.java
+++ v2/protegrity-data-tokenization/src/main/java/com/google/cloud/teleport/v2/transforms/io/BigTableIO.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2020 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.google.cloud.teleport.v2.transforms.io;
 
 import com.google.bigtable.v2.Mutation;
@@ -15,11 +30,15 @@ import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PDone;
 import org.apache.beam.sdk.values.Row;
 import org.apache.commons.lang3.tuple.Pair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * The {@link BigTableIO} class for writing data from template to BigTable.
  */
 public class BigTableIO {
+    private static final Logger LOG = LoggerFactory.getLogger(BigTableIO.class);
+
     private final ProtegrityDataTokenizationOptions options;
 
     public BigTableIO(ProtegrityDataTokenizationOptions options) {
@@ -27,14 +46,14 @@ public class BigTableIO {
     }
 
     public PDone write(
-            PCollection<Row> input,
+                PCollection<Row> input,
             Schema schema
     ) {
         return input.apply("convertToBigTableFormat", ParDo.of(new TransformToBigTableFormat(schema)))
                 .apply("writeToBigTable", BigtableIO.write()
                         .withProjectId(options.getBigTableProjectId())
                         .withInstanceId(options.getBigTableInstanceId())
-                        .withTableId(options.getBigTableTableId()));
+                        .withTableId(options.getBigTableTableId())) ;
     }
 
     static class TransformToBigTableFormat extends DoFn<Row, KV<ByteString, Iterable<Mutation>>> {
