66d049c83be2e81334df81ea668eb1e8ea1fb72e
Ilya Kozyrev <ilya.kozyrev@akvelon.com>
2020-12-11 17:15:59 +0300

    rollback changes for DataStreamBigQueryMerger.java

--- v2/common/src/main/java/com/google/cloud/teleport/v2/cdc/merge/DataStreamBigQueryMerger.java
+++ v2/common/src/main/java/com/google/cloud/teleport/v2/cdc/merge/DataStreamBigQueryMerger.java
@@ -15,103 +15,101 @@
  */
 package com.google.cloud.teleport.v2.cdc.merge;
 
-//import com.google.api.services.bigquery.model.TableRow;
-//import com.google.cloud.bigquery.BigQuery;
-//import com.google.cloud.bigquery.TableId;
-//import com.google.cloud.teleport.v2.cdc.mappers.MergeInfoMapper;
-//import com.google.cloud.teleport.v2.utils.DataStreamClient;
-//import java.io.IOException;
-//import org.apache.beam.sdk.extensions.gcp.options.GcpOptions;
-//import org.apache.beam.sdk.transforms.MapElements;
-//import org.apache.beam.sdk.transforms.PTransform;
-//import org.apache.beam.sdk.transforms.Values;
-//import org.apache.beam.sdk.values.KV;
-//import org.apache.beam.sdk.values.PCollection;
-//import org.apache.beam.sdk.values.TypeDescriptor;
-//import org.apache.beam.sdk.values.TypeDescriptors;
-//import org.joda.time.Duration;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
+import com.google.api.services.bigquery.model.TableRow;
+import com.google.cloud.bigquery.BigQuery;
+import com.google.cloud.bigquery.TableId;
+import com.google.cloud.teleport.v2.cdc.mappers.MergeInfoMapper;
+import com.google.cloud.teleport.v2.utils.DataStreamClient;
+import java.io.IOException;
+import org.apache.beam.sdk.extensions.gcp.options.GcpOptions;
+import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.transforms.PTransform;
+import org.apache.beam.sdk.transforms.Values;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.TypeDescriptor;
+import org.apache.beam.sdk.values.TypeDescriptors;
+import org.joda.time.Duration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Class BigQueryMerger.
  */
-public class DataStreamBigQueryMerger
-// extends PTransform<PCollection<KV<TableId, TableRow>>, PCollection<Void>>
-{
+public class DataStreamBigQueryMerger extends PTransform<PCollection<KV<TableId, TableRow>>, PCollection<Void>> {
 
-// private static final Logger LOG = LoggerFactory.getLogger(DataStreamBigQueryMerger.class);
-//
-// private DataStreamClient dataStreamClient;
-// private String stagingDataset;
-// private String stagingTable;
-// private String replicaDataset;
-// private String replicaTable;
-// private Duration windowDuration;
-// private BigQuery bigQueryClient;
-// private MergeConfiguration mergeConfiguration;
-//
-// public DataStreamBigQueryMerger(
-// GcpOptions options,
-// String stagingDataset, String stagingTable,
-// String replicaDataset, String replicaTable,
-// Duration windowDuration, BigQuery bigQueryClient, MergeConfiguration mergeConfiguration) {
-// this.stagingDataset = stagingDataset;
-// this.stagingTable = stagingTable;
-//
-// this.replicaDataset = replicaDataset;
-// this.replicaTable = replicaTable;
-//
-// this.windowDuration = windowDuration;
-// this.bigQueryClient = bigQueryClient;
-// this.mergeConfiguration = mergeConfiguration;
-//
-// try {
-// this.dataStreamClient = new DataStreamClient(options.getGcpCredential());
-// } catch (IOException e) {
-// LOG.error("IOException Occurred: DataStreamClient failed initialization.");
-// this.dataStreamClient = null;
-// }
-// }
-//
-// public DataStreamBigQueryMerger withDataStreamRootUrl(String url) {
-// if (this.dataStreamClient != null) {
-// this.dataStreamClient.setRootUrl(url);
-// }
-//
-// return this;
-// }
-//
-// @Override
-// public PCollection<Void> expand(PCollection<KV<TableId, TableRow>> input) {
-// final MergeStatementBuilder mergeBuilder = new MergeStatementBuilder(mergeConfiguration);
-//
-// // Group each batch of rows into a single table object for merge
-// PCollection<KV<TableId, TableRow>> groupedByTable = input
-// .apply(
-// MapElements.into(
-// TypeDescriptors.kvs(
-// TypeDescriptors.strings(),
-// TypeDescriptors.kvs(
-// TypeDescriptor.of(TableId.class),
-// TypeDescriptor.of(TableRow.class))))
-// .via(tableInfo -> KV.of(tableInfo.getKey().toString(), tableInfo)))
-// .apply(
-// new BigQueryMerger.TriggerPerKeyOnFixedIntervals<String, KV<TableId, TableRow>>(windowDuration))
-// .apply(Values.create());
-//
-// // Create MergeInfo objects using DataStream APIs
-// PCollection<MergeInfo> mergeInfoRecords =
-// groupedByTable.apply(
-// "Create MergeInfo Objects",
-// new MergeInfoMapper(
-// this.dataStreamClient,
-// this.stagingDataset,
-// this.stagingTable,
-// this.replicaDataset,
-// this.replicaTable));
-//
-// // Excute Merge Statement
-// return BigQueryMerger.expandExecuteMerge(mergeInfoRecords, mergeConfiguration, bigQueryClient);
-// }
+    private static final Logger LOG = LoggerFactory.getLogger(DataStreamBigQueryMerger.class);
+
+    private DataStreamClient dataStreamClient;
+    private String stagingDataset;
+    private String stagingTable;
+    private String replicaDataset;
+    private String replicaTable;
+    private Duration windowDuration;
+    private BigQuery bigQueryClient;
+    private MergeConfiguration mergeConfiguration;
+
+    public DataStreamBigQueryMerger(
+            GcpOptions options,
+            String stagingDataset, String stagingTable,
+            String replicaDataset, String replicaTable,
+            Duration windowDuration, BigQuery bigQueryClient, MergeConfiguration mergeConfiguration) {
+        this.stagingDataset = stagingDataset;
+        this.stagingTable = stagingTable;
+
+        this.replicaDataset = replicaDataset;
+        this.replicaTable = replicaTable;
+
+        this.windowDuration = windowDuration;
+        this.bigQueryClient = bigQueryClient;
+        this.mergeConfiguration = mergeConfiguration;
+
+        try {
+            this.dataStreamClient = new DataStreamClient(options.getGcpCredential());
+        } catch (IOException e) {
+            LOG.error("IOException Occurred: DataStreamClient failed initialization.");
+            this.dataStreamClient = null;
+        }
+    }
+
+    public DataStreamBigQueryMerger withDataStreamRootUrl(String url) {
+        if (this.dataStreamClient != null) {
+            this.dataStreamClient.setRootUrl(url);
+        }
+
+        return this;
+    }
+
+    @Override
+    public PCollection<Void> expand(PCollection<KV<TableId, TableRow>> input) {
+        final MergeStatementBuilder mergeBuilder = new MergeStatementBuilder(mergeConfiguration);
+
+        // Group each batch of rows into a single table object for merge
+        PCollection<KV<TableId, TableRow>> groupedByTable = input
+                .apply(
+                        MapElements.into(
+                                TypeDescriptors.kvs(
+                                        TypeDescriptors.strings(),
+                                        TypeDescriptors.kvs(
+                                                TypeDescriptor.of(TableId.class),
+                                                TypeDescriptor.of(TableRow.class))))
+                                .via(tableInfo -> KV.of(tableInfo.getKey().toString(), tableInfo)))
+                .apply(
+                        new BigQueryMerger.TriggerPerKeyOnFixedIntervals<String, KV<TableId, TableRow>>(windowDuration))
+                .apply(Values.create());
+
+        // Create MergeInfo objects using DataStream APIs
+        PCollection<MergeInfo> mergeInfoRecords =
+                groupedByTable.apply(
+                        "Create MergeInfo Objects",
+                        new MergeInfoMapper(
+                                this.dataStreamClient,
+                                this.stagingDataset,
+                                this.stagingTable,
+                                this.replicaDataset,
+                                this.replicaTable));
+
+        // Excute Merge Statement
+        return BigQueryMerger.expandExecuteMerge(mergeInfoRecords, mergeConfiguration, bigQueryClient);
+    }
 }
