dcebfb999833a7f84d0f6858c00ad932f3ea8109
MikhailMedvedevAkvelon <78736905+MikhailMedvedevAkvelon@users.noreply.github.com>
2021-05-07 15:36:27 +0400

    Add beamSchemaToJson functionality (#19)

--- v2/common/src/main/java/com/google/cloud/teleport/v2/utils/BeamSchemaUtils.java
+++ v2/common/src/main/java/com/google/cloud/teleport/v2/utils/BeamSchemaUtils.java
@@ -1,5 +1,53 @@
+/*
+ * Copyright (C) 2021 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.google.cloud.teleport.v2.utils;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.beam.sdk.schemas.Schema;
+import org.apache.beam.sdk.schemas.Schema.Field;
+
+/**
+ * BeamSchemaUtils has utilities scope for convert {@link Schema} into/from various formats
+ */
 public class BeamSchemaUtils {
+
+  public static final String FIELD_NAME = "name";
+  public static final String FIELD_TYPE = "type";
+
+  /**
+   * Convert {@link Schema} into json string
+   *
+   * @param beamSchema {@link Schema}
+   * @return json string as {@link String}
+   */
+  public static String beamSchemaToJson(Schema beamSchema) {
+    ObjectMapper mapper = new ObjectMapper();
+    ArrayNode beamSchemaJsonNode = mapper.createArrayNode();
+
+    for (Field field : beamSchema.getFields()) {
+      ObjectNode fieldJsonNode = mapper.createObjectNode();
+      fieldJsonNode.put(FIELD_NAME, field.getName());
+      fieldJsonNode.put(FIELD_TYPE, field.getType().getTypeName().toString());
+
+      beamSchemaJsonNode.add(fieldJsonNode);
+    }
+
+    return beamSchemaJsonNode.toString();
+  }
 
 }
