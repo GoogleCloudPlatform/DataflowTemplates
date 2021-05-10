c7d0979114efb931f3811e1521b01433ec56f4f3
RaphaelSanamyan <61141424+RaphaelSanamyan@users.noreply.github.com>
2021-05-10 20:20:18 +0300

    Parse Json to beam schema (#18)
    * create schema from json
    
    * fix test
    
    * add license and docstrings
    
    * add extra check and update test
    
    * Merge branch and resolve conflicts

--- v2/common/src/main/java/com/google/cloud/teleport/v2/utils/BeamSchemaUtils.java
+++ v2/common/src/main/java/com/google/cloud/teleport/v2/utils/BeamSchemaUtils.java
@@ -18,12 +18,26 @@ package com.google.cloud.teleport.v2.utils;
 import static org.apache.beam.sdk.io.gcp.bigquery.BigQueryUtils.fromTableSchema;
 import static org.apache.beam.sdk.io.gcp.bigquery.BigQueryUtils.toTableSchema;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import static org.apache.beam.vendor.guava.v20_0.com.google.common.base.Preconditions.checkNotNull;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Enums;
+import com.google.common.base.Optional;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.LinkedList;
+import java.util.List;
 import com.google.api.services.bigquery.model.TableSchema;
 import org.apache.beam.sdk.schemas.Schema;
 import org.apache.beam.sdk.schemas.Schema.Field;
+import org.apache.beam.sdk.schemas.Schema.FieldType;
+import org.apache.beam.sdk.schemas.Schema.TypeName;
+
+
 
 /**
  * BeamSchemaUtils has utilities scope for convert {@link Schema} into/from various formats
@@ -32,6 +46,8 @@ public class BeamSchemaUtils {
 
   public static final String FIELD_NAME = "name";
   public static final String FIELD_TYPE = "type";
+  static final JsonFactory FACTORY = new JsonFactory();
+  static final ObjectMapper MAPPER = new ObjectMapper(FACTORY);
 
   /**
    * Convert {@link Schema} into json string
@@ -40,11 +56,10 @@ public class BeamSchemaUtils {
    * @return json string as {@link String}
    */
   public static String beamSchemaToJson(Schema beamSchema) {
-    ObjectMapper mapper = new ObjectMapper();
-    ArrayNode beamSchemaJsonNode = mapper.createArrayNode();
+    ArrayNode beamSchemaJsonNode = MAPPER.createArrayNode();
 
     for (Field field : beamSchema.getFields()) {
-      ObjectNode fieldJsonNode = mapper.createObjectNode();
+      ObjectNode fieldJsonNode = MAPPER.createObjectNode();
       fieldJsonNode.put(FIELD_NAME, field.getName());
       fieldJsonNode.put(FIELD_TYPE, field.getType().getTypeName().toString());
 
@@ -54,6 +69,75 @@ public class BeamSchemaUtils {
     return beamSchemaJsonNode.toString();
   }
 
+  /**
+   * Parse provided json string to {@link Schema}.
+   *
+   * <p>Json string should be in "[{"type": "INT32", "name": "fieldName"}, ...]" format
+   *
+   * @param jsonString json compatible string
+   * @return converted {@link Schema}
+   */
+  public static Schema fromJson(String jsonString) throws SchemaParseException, IOException {
+    try (JsonParser jsonParser = FACTORY.createParser(new StringReader(jsonString))) {
+      return fromJson(jsonParser);
+    }
+  }
+
+
+  private static Schema fromJson(JsonParser jsonParser) throws IOException, SchemaParseException {
+    JsonNode jsonNodes = MAPPER.readTree(jsonParser);
+    if (!jsonNodes.isArray()) {
+      throw new SchemaParseException(
+          "Provided schema must be in \"[{\"type\": \"fieldTypeName\", \"name\": \"fieldName\"}, ...]\" format");
+    }
+    return new Schema(getFieldsfromJsonNode(jsonNodes));
+
+  }
+
+
+  private static List<Field> getFieldsfromJsonNode(JsonNode jsonNode)
+      throws SchemaParseException {
+    List<Field> fields = new LinkedList<>();
+    for (JsonNode node : jsonNode) {
+      if (!node.isObject()) {
+        throw new SchemaParseException("Node must be object: " + node.toString());
+      }
+      String type = getText(node, FIELD_TYPE, "type is missed");
+      String name = getText(node, FIELD_NAME, "name is missed");
+      fields.add(Field.of(name, stringToFieldType(type)));
+    }
+    return fields;
+  }
+
+  private static FieldType stringToFieldType(String string) throws SchemaParseException {
+    Optional<TypeName> typeName = Enums.getIfPresent(TypeName.class, string);
+    if (!typeName.isPresent()) {
+      throw new SchemaParseException(String.format("Provided type \"%s\" does not exist", string));
+    }
+    return FieldType.of(typeName.get());
+  }
+
+  private static String getOptionalText(JsonNode node, String key) {
+    JsonNode jsonNode = node.get(key);
+    return jsonNode != null ? jsonNode.textValue() : null;
+  }
+
+  private static String getText(JsonNode node, String key, String errorMessage) {
+    String text = getOptionalText(node, key);
+    checkNotNull(text, errorMessage + ": " + node);
+    return text;
+  }
+
+  public static class SchemaParseException extends Exception {
+
+    public SchemaParseException(Throwable cause) {
+      super(cause);
+    }
+
+    public SchemaParseException(String message) {
+      super(message);
+    }
+  }
   /**
    * Convert a BigQuery {@link TableSchema} to a Beam {@link Schema}.
    */
--- v2/common/src/test/java/com/google/cloud/teleport/v2/utils/BeamSchemaUtilsTest.java
+++ v2/common/src/test/java/com/google/cloud/teleport/v2/utils/BeamSchemaUtilsTest.java
@@ -15,8 +15,12 @@
  */
 package com.google.cloud.teleport.v2.utils;
 
+import static org.junit.Assert.assertEquals;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.cloud.teleport.v2.utils.BeamSchemaUtils.SchemaParseException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -26,21 +30,154 @@ import org.apache.beam.sdk.schemas.Schema;
 import org.apache.beam.sdk.schemas.Schema.FieldType;
 import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 /**
  * Tests for {@link BeamSchemaUtils}
  */
 public class BeamSchemaUtilsTest {
 
+  @Rule
+  public ExpectedException exceptionRule = ExpectedException.none();
   static final String EXPECTED_FIELD_NAME = "expectedFieldName";
   static ObjectMapper mapper;
+  public static String jsonSchema = "[\n"
+      + "  {\n"
+      + "    \"name\": \"byte\",\n"
+      + "    \"type\": \"BYTE\"\n"
+      + "  },\n"
+      + "  {\n"
+      + "    \"name\": \"int16\",\n"
+      + "    \"type\": \"INT16\"\n"
+      + "  },\n"
+      + "  {\n"
+      + "    \"name\": \"int32\",\n"
+      + "    \"type\": \"INT32\"\n"
+      + "  },\n"
+      + "  {\n"
+      + "    \"name\": \"int64\",\n"
+      + "    \"type\": \"INT64\"\n"
+      + "  },\n"
+      + "  {\n"
+      + "    \"name\": \"decimal\",\n"
+      + "    \"type\": \"DECIMAL\"\n"
+      + "  },\n"
+      + "  {\n"
+      + "    \"name\": \"float\",\n"
+      + "    \"type\": \"FLOAT\"\n"
+      + "  },\n"
+      + "  {\n"
+      + "    \"name\": \"double\",\n"
+      + "    \"type\": \"DOUBLE\"\n"
+      + "  },\n"
+      + "  {\n"
+      + "    \"name\": \"string\",\n"
+      + "    \"type\": \"STRING\"\n"
+      + "  },\n"
+      + "  {\n"
+      + "    \"name\": \"datetime\",\n"
+      + "    \"type\": \"DATETIME\"\n"
+      + "  },\n"
+      + "  {\n"
+      + "    \"name\": \"boolean\",\n"
+      + "    \"type\": \"BOOLEAN\"\n"
+      + "  },\n"
+      + "  {\n"
+      + "    \"name\": \"bytes\",\n"
+      + "    \"type\": \"BYTES\"\n"
+      + "  }\n"
+      + "]\n";
 
   @BeforeClass
   public static void before() {
     mapper = new ObjectMapper();
   }
 
+
+  @Test
+  public void testFromJson() throws SchemaParseException, IOException {
+    Schema schema = BeamSchemaUtils.fromJson(jsonSchema);
+    assertEquals(11, schema.getFieldCount());
+
+    assertEquals(0, schema.indexOf("byte"));
+    assertEquals("byte", schema.getField(0).getName());
+    assertEquals(FieldType.BYTE, schema.getField(0).getType());
+
+    assertEquals(1, schema.indexOf("int16"));
+    assertEquals("int16", schema.getField(1).getName());
+    assertEquals(FieldType.INT16, schema.getField(1).getType());
+
+    assertEquals(2, schema.indexOf("int32"));
+    assertEquals("int32", schema.getField(2).getName());
+    assertEquals(FieldType.INT32, schema.getField(2).getType());
+
+    assertEquals(3, schema.indexOf("int64"));
+    assertEquals("int64", schema.getField(3).getName());
+    assertEquals(FieldType.INT64, schema.getField(3).getType());
+
+    assertEquals(4, schema.indexOf("decimal"));
+    assertEquals("decimal", schema.getField(4).getName());
+    assertEquals(FieldType.DECIMAL, schema.getField(4).getType());
+
+    assertEquals(5, schema.indexOf("float"));
+    assertEquals("float", schema.getField(5).getName());
+    assertEquals(FieldType.FLOAT, schema.getField(5).getType());
+
+    assertEquals(6, schema.indexOf("double"));
+    assertEquals("double", schema.getField(6).getName());
+    assertEquals(FieldType.DOUBLE, schema.getField(6).getType());
+
+    assertEquals(7, schema.indexOf("string"));
+    assertEquals("string", schema.getField(7).getName());
+    assertEquals(FieldType.STRING, schema.getField(7).getType());
+
+    assertEquals(8, schema.indexOf("datetime"));
+    assertEquals("datetime", schema.getField(8).getName());
+    assertEquals(FieldType.DATETIME, schema.getField(8).getType());
+
+    assertEquals(9, schema.indexOf("boolean"));
+    assertEquals("boolean", schema.getField(9).getName());
+    assertEquals(FieldType.BOOLEAN, schema.getField(9).getType());
+
+    assertEquals(10, schema.indexOf("bytes"));
+    assertEquals("bytes", schema.getField(10).getName());
+    assertEquals(FieldType.BYTES, schema.getField(10).getType());
+  }
+
+  @Test
+  public void testMissedField() throws SchemaParseException, IOException {
+    exceptionRule.expect(NullPointerException.class);
+    exceptionRule.expectMessage("type is missed: {\"name\":\"testName\"}");
+    BeamSchemaUtils.fromJson("[{\"name\": \"testName\"}]");
+
+    exceptionRule.expect(NullPointerException.class);
+    exceptionRule.expectMessage("name is missed: {\"type\":\"testType\"}");
+    BeamSchemaUtils.fromJson("[{\"type\": \"testType\"}]");
+  }
+
+  @Test
+  public void testInvalidFormat() throws SchemaParseException, IOException {
+    exceptionRule.expect(SchemaParseException.class);
+    exceptionRule.expectMessage(
+        "Provided schema must be in \"[{\"type\": \"fieldTypeName\", \"name\": \"fieldName\"}, ...]\" format");
+    BeamSchemaUtils.fromJson("{\"name\": \"bytes\",\"type\": \"BYTES\"}");
+  }
+
+  @Test
+  public void testInvalidType() throws SchemaParseException, IOException {
+    exceptionRule.expect(SchemaParseException.class);
+    exceptionRule.expectMessage("Provided type \"INVALID\" does not exist");
+    BeamSchemaUtils.fromJson("[{\"name\": \"bytes\",\"type\": \"INVALID\"}]");
+  }
+
+  @Test
+  public void testInvalidNodeFormat() throws SchemaParseException, IOException {
+    exceptionRule.expect(SchemaParseException.class);
+    exceptionRule.expectMessage("Node must be object: [{\"name\":\"bytes\",\"type\":\"BYTES\"}]");
+    BeamSchemaUtils.fromJson("[[{\"name\": \"bytes\",\"type\": \"BYTES\"}]]");
+  }
 
   @Test
   public void givenBeamSchema_whenBYTEField_thenCorrectJsonString() throws JsonProcessingException {
