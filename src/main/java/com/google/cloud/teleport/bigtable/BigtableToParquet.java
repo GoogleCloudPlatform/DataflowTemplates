b5b46eb36985abafb333bee526b41770043b728b
Didi Kohen <kohen.d@gmail.com>
2021-07-19 11:05:44 +0300

    Test fix

--- src/main/java/com/google/cloud/teleport/bigtable/BigtableToParquet.java
+++ src/main/java/com/google/cloud/teleport/bigtable/BigtableToParquet.java
@@ -100,7 +100,7 @@ public class BigtableToParquet {
     ValueProvider<Integer> getNumVersions();
 
     @SuppressWarnings("unused")
-    void setNumVersions(ValueProvider<Integer> NumVersions);
+    void setNumVersions(ValueProvider<Integer> numVersions);
   }
 
   /**
