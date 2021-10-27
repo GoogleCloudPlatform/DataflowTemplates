57cef94378fc241213a9996a950b8f7176a3f148
Cloud Teleport <cloud-teleport@google.com>
2021-10-27 16:42:24 -0700

    Fix handling nulls when converting from JSONs
    The CL updates how the JSON files are being converted to GenericRecord instances.
    The original method that used the built in Avro converter didn't handle nullable
    value from Dataplex json files. (The builtin JSON decoder is expecting the nullable values that are not null to be "annotated" with a type, while Dataplex's JSONs, as expected, just contain the values.)
    
    PiperOrigin-RevId: 406010522

--- v2/common/src/main/java/com/google/cloud/teleport/v2/transforms/JsonConverters.java
+++ v2/common/src/main/java/com/google/cloud/teleport/v2/transforms/JsonConverters.java
@@ -15,15 +15,20 @@
  */
 package com.google.cloud.teleport.v2.transforms;
 
-import static org.apache.beam.vendor.guava.v26_0_jre.com.google.common.base.Preconditions.checkArgument;
-
 import com.google.auto.value.AutoValue;
 import com.google.cloud.teleport.v2.utils.SchemaUtils;
-import java.io.IOException;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import javax.annotation.Nullable;
 import org.apache.avro.Schema;
-import org.apache.avro.generic.GenericDatumReader;
+import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericRecord;
-import org.apache.avro.io.DecoderFactory;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.PTransform;
@@ -66,7 +71,7 @@ public final class JsonConverters {
 
       public JsonConverters.ReadJson build() {
         JsonConverters.ReadJson readJson = autoBuild();
-        checkArgument(readJson.inputFileSpec() != null, "Input file spec must be provided.");
+        Objects.requireNonNull(readJson.inputFileSpec(), "Input file spec must be provided.");
         return readJson;
       }
     }
@@ -92,20 +97,128 @@ public final class JsonConverters {
 
     @ProcessElement
     public void processElement(ProcessContext context) throws IllegalArgumentException {
-      GenericDatumReader<GenericRecord> reader = new GenericDatumReader<>(schema);
-      String json = context.element();
+      String jsonRecord =
+          Objects.requireNonNull(context.element(), "The Json record can't be null");
+      Object result;
       try {
-        context.output(reader.read(null, DecoderFactory.get().jsonDecoder(schema, json)));
-      } catch (IOException e) {
+        result = jsonElementToAvro(JsonParser.parseString(jsonRecord), schema);
+      } catch (JsonSchemaMismatchException e) {
         String message =
-            "Error while converting json record `"
-                + json
-                + "` according to schema `"
-                + schema
-                + "`.";
-        LOG.error(message, e);
-        throw new RuntimeException(message, e);
+            String.format("Json record `%s` doesn't match Avro schema `%s`.", jsonRecord, schema);
+        LOG.error(message);
+        throw new IllegalArgumentException(message, e);
       }
+      if (!(result instanceof GenericRecord)) {
+        throw new IllegalArgumentException(
+            String.format(
+                "The Json record `%s` must convert to GenericRecord instance with Avro schema"
+                    + " `%s`.",
+                jsonRecord, schema));
+      }
+      context.output((GenericRecord) result);
+    }
+  }
+
+  @Nullable
+  private static Object jsonElementToAvro(@Nullable JsonElement element, Schema schema)
+      throws JsonSchemaMismatchException {
+    boolean schemaIsNullable = isNullable(schema);
+    if (schemaIsNullable) {
+      schema = typeFromNullable(schema);
+    }
+    if (element == null || element.isJsonNull()) {
+      if (!schemaIsNullable) {
+        throw new JsonSchemaMismatchException("The element is not nullable in Avro schema.");
+      }
+      return null;
+    } else if (element.isJsonObject()) {
+      if (schema.getType() != Schema.Type.RECORD) {
+        throw new JsonSchemaMismatchException(
+            String.format("The element `%s` doesn't match Avro type RECORD", element));
+      }
+      return jsonObjectToAvro(element.getAsJsonObject(), schema);
+    } else if (element.isJsonArray()) {
+      if (schema.getType() != Schema.Type.ARRAY) {
+        throw new JsonSchemaMismatchException(
+            String.format("The element `%s` doesn't match Avro type ARRAY", element));
+      }
+      JsonArray jsonArray = element.getAsJsonArray();
+      List<Object> avroArray = new ArrayList<>(jsonArray.size());
+      for (JsonElement e : element.getAsJsonArray()) {
+        avroArray.add(jsonElementToAvro(e, schema.getElementType()));
+      }
+      return avroArray;
+    } else if (element.isJsonPrimitive()) {
+      return jsonPrimitiveToAvro(element.getAsJsonPrimitive(), schema);
+    } else {
+      throw new JsonSchemaMismatchException(
+          String.format(
+              "The Json element `%s` is of an unknown class %s", element, element.getClass()));
+    }
+  }
+
+  private static GenericRecord jsonObjectToAvro(JsonObject jsonObject, Schema schema)
+      throws JsonSchemaMismatchException {
+    GenericRecord avroRecord = new GenericData.Record(schema);
+    for (Schema.Field field : schema.getFields()) {
+      avroRecord.put(field.name(), jsonElementToAvro(jsonObject.get(field.name()), field.schema()));
+    }
+    return avroRecord;
+  }
+
+  private static Object jsonPrimitiveToAvro(JsonPrimitive primitive, Schema schema)
+      throws JsonSchemaMismatchException {
+    switch (schema.getType()) {
+      case NULL:
+        return null;
+      case STRING:
+        return primitive.getAsString();
+      case BOOLEAN:
+        return primitive.getAsBoolean();
+      case INT:
+        return primitive.getAsInt();
+      case LONG:
+        return primitive.getAsLong();
+      case FLOAT:
+        return primitive.getAsFloat();
+      case DOUBLE:
+        return primitive.getAsDouble();
+      default:
+        throw new JsonSchemaMismatchException(
+            String.format("The Avro type %s is not supported", schema.getType()));
+    }
+  }
+
+  /**
+   * This method is equivalent to {@code Schema.isNullable()} of Avro 1.9.
+   *
+   * <p>Normally nullables in Avro are represented as a `union`: `"type": ["null", "some type"]}`.
+   * But the implementation of {@code Schema.isNullable()} in Avro 1.9 suggests that the unions
+   * could potentially be nested, so this implementation also handles that.
+   */
+  private static boolean isNullable(Schema type) {
+    return type.getType() == Schema.Type.NULL
+        || type.getType() == Schema.Type.UNION
+            && type.getTypes().stream().anyMatch(JsonConverters::isNullable);
+  }
+
+  private static Schema typeFromNullable(Schema type) {
+    if (type.getType() == Schema.Type.UNION) {
+      return typeFromNullable(
+          type.getTypes().stream()
+              .filter(t -> t.getType() != Schema.Type.NULL)
+              .findFirst()
+              .orElseThrow(
+                  () ->
+                      new IllegalStateException(
+                          String.format("Type `%s` should have a non null subtype", type))));
+    }
+    return type;
+  }
+
+  private static class JsonSchemaMismatchException extends Exception {
+    JsonSchemaMismatchException(String message) {
+      super(message);
     }
   }
 }
