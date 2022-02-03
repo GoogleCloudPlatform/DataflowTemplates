64a26909e1d4903f411cc348c591c658d3947f06
zhoufek <zhoufek@google.com>
2022-02-03 15:06:14 -0500

    Add command for running checkstyle

---
+++ cicd/cmd/run-checkstyle/main.go
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/GoogleCloudPlatform/DataflowTemplates/cicd/internal/flags"
+	"github.com/GoogleCloudPlatform/DataflowTemplates/cicd/internal/op"
+	"github.com/GoogleCloudPlatform/DataflowTemplates/cicd/internal/repo"
+)
+
+const (
+	CheckstyleCommand = "checkstyle:check"
+)
+
+func main() {
+	flags.RegisterCommonFlags()
+	flag.Parse()
+
+	changed := flags.ChangedFiles()
+	if len(changed) == 0 {
+		return
+	}
+
+	var fullErr error
+	for root, children := range repo.GetModulesForPaths(changed) {
+		var err error
+		if len(children) == 0 {
+			err = op.RunMavenOnPom(root, CheckstyleCommand)
+		} else {
+			err = op.RunMavenOnModule(root, CheckstyleCommand, strings.Join(children, ","))
+		}
+
+		if err != nil && fullErr == nil {
+			fullErr = err
+		} else {
+			fullErr = fmt.Errorf("%w\n%v", fullErr, err)
+		}
+	}
+
+	if fullErr != nil {
+		log.Fatal("Error running checkstyle. See output for more details.")
+	}
+}
