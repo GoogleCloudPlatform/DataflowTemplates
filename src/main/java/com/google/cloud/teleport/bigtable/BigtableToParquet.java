cf452a5c1e2f3425a1fc36bca2d8f216a88ae9e1
Yefim Matskin <yafim@cycognito.com>
2022-03-02 16:38:55 +0200

    revert : add disk config

--- src/main/java/com/google/cloud/teleport/bigtable/BigtableToParquet.java
+++ src/main/java/com/google/cloud/teleport/bigtable/BigtableToParquet.java
@@ -21,7 +21,6 @@ import com.google.bigtable.v2.Cell;
 import com.google.bigtable.v2.Column;
 import com.google.bigtable.v2.Family;
 import com.google.bigtable.v2.Row;
-import com.google.bigtable.v2.RowFilter;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
@@ -93,19 +92,6 @@ public class BigtableToParquet {
 
     @SuppressWarnings("unused")
     void setNumShards(ValueProvider<Integer> numShards);
-
-    @Description("Disk size, GB")
-    @Default.Integer(25)
-    ValueProvider<Integer> getDiskSize();
-
-    @SuppressWarnings("unused")
-    void setDiskSize(ValueProvider<Integer> sizeGB);
-
-    @Description("Worker disk type")
-    String getWorkerDiskType();
-
-    @SuppressWarnings("unused")
-    void setWorkerDiskType(String type);
   }
 
   /**
@@ -116,7 +102,7 @@ public class BigtableToParquet {
   public static void main(String[] args) {
     Options options = PipelineOptionsFactory.fromArgs(args).withValidation().as(Options.class);
 
-    PipelineResult result = run(options, null);
+    PipelineResult result = run(options);
 
     // Wait for pipeline to finish only if it is not constructing a template.
     if (options.as(DataflowPipelineOptions.class).getTemplateLocation() == null) {
@@ -128,16 +114,8 @@ public class BigtableToParquet {
    * Runs a pipeline to export data from a Cloud Bigtable table to Parquet file(s) in GCS.
    *
    * @param options arguments to the pipeline
-   * @param filter  filter to apply, optional
    */
-  public static PipelineResult run(Options options, RowFilter filter) {
-    DataflowPipelineOptions dataflowOpts = options.as(DataflowPipelineOptions.class);
-    if (options.getDiskSize().isAccessible()) {
-      dataflowOpts.setDiskSizeGb(options.getDiskSize().get());
-    }
-    if (options.getWorkerDiskType() != null) {
-      dataflowOpts.setWorkerDiskType(options.getWorkerDiskType());
-    }
+  public static PipelineResult run(Options options) {
     Pipeline pipeline = Pipeline.create(PipelineUtils.tweakPipelineOptions(options));
     BigtableIO.Read read =
         BigtableIO.read()
@@ -145,9 +123,6 @@ public class BigtableToParquet {
             .withInstanceId(options.getBigtableInstanceId())
             .withTableId(options.getBigtableTableId());
 
-    if (filter != null) {
-      read.withRowFilter(filter);
-    }
     // Do not validate input fields if it is running as a template.
     if (options.as(DataflowPipelineOptions.class).getTemplateLocation() != null) {
       read = read.withoutValidation();
