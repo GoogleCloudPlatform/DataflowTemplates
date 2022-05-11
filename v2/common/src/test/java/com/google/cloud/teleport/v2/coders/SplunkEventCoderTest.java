0c7a2a3e46a524e82d7c47633cc1656f46350fbd
Andrei Gurau <andreigurau@google.com>
2022-05-11 11:28:09 -0400

    brought unit tests for SplunkEventCoder and SplunkCoverters

---
+++ v2/common/src/test/java/com/google/cloud/teleport/v2/coders/SplunkEventCoderTest.java
@@ -0,0 +1,175 @@
+/*
+ * Copyright (C) 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.cloud.teleport.v2.coders;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import org.apache.beam.sdk.io.splunk.SplunkEvent;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+import org.junit.Test;
+
+/** Unit tests for {@link com.google.cloud.teleport.v2.coders.SplunkEventCoder} class. */
+public class SplunkEventCoderTest {
+
+  /**
+   * Test whether {@link SplunkEventCoder} is able to encode/decode a {@link SplunkEvent} correctly.
+   *
+   * @throws IOException
+   */
+  @Test
+  public void testEncodeDecode() throws IOException {
+
+    String event = "test-event";
+    String host = "test-host";
+    String index = "test-index";
+    String source = "test-source";
+    String sourceType = "test-source-type";
+    Long time = 123456789L;
+
+    SplunkEvent actualEvent =
+        SplunkEvent.newBuilder()
+            .withEvent(event)
+            .withHost(host)
+            .withIndex(index)
+            .withSource(source)
+            .withSourceType(sourceType)
+            .withTime(time)
+            .create();
+
+    SplunkEventCoder coder = SplunkEventCoder.of();
+    try (ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
+      coder.encode(actualEvent, bos);
+      try (ByteArrayInputStream bin = new ByteArrayInputStream(bos.toByteArray())) {
+        SplunkEvent decodedEvent = coder.decode(bin);
+        assertThat(decodedEvent, is(equalTo(actualEvent)));
+      }
+    }
+  }
+
+  /**
+   * Tests whether {@link SplunkEventCoder} is able to decode a {@link SplunkEvent} encoded using
+   * the older coder version 1 (commit f0ff6cc).
+   */
+  @Test
+  public void testBackwardsCompatibility_canDecodeVersion1() throws IOException, DecoderException {
+
+    SplunkEvent expectedEvent =
+        SplunkEvent.newBuilder()
+            .withEvent("e")
+            .withHost("h")
+            .withIndex("i")
+            .withSource("s")
+            .withSourceType("st")
+            .withTime(1234L)
+            .create();
+
+    String hex = "0100000000000004d2010168010173010273740101690165";
+    SplunkEvent actualEvent = SplunkEventCoder.of().decode(fromHex(hex));
+
+    assertThat(actualEvent, is(equalTo(expectedEvent)));
+  }
+
+  /**
+   * Tests whether {@link SplunkEventCoder} is able to decode a {@link SplunkEvent} encoded using
+   * the older coder version 1 (commit f0ff6cc) and having an empty "event" field.
+   *
+   * <p>An empty field is encoded as <code>00</code>, which may look like the present/not present
+   * marker for the "fields" field in V2.
+   */
+  @Test
+  public void testBackwardsCompatibility_canDecodeVersion1withEmptyEvent()
+      throws IOException, DecoderException {
+
+    SplunkEvent expectedEvent =
+        SplunkEvent.newBuilder()
+            .withEvent("")
+            .withHost("h")
+            .withIndex("i")
+            .withSource("s")
+            .withSourceType("st")
+            .withTime(1234L)
+            .create();
+
+    String hex = "0100000000000004d20101680101730102737401016900";
+    SplunkEvent actualEvent = SplunkEventCoder.of().decode(fromHex(hex));
+
+    assertThat(actualEvent, is(equalTo(expectedEvent)));
+  }
+
+  /**
+   * Tests whether {@link SplunkEventCoder} is able to decode a {@link SplunkEvent} encoded using
+   * the older coder version 1 (commit f0ff6cc) and having the "event" field of length 1.
+   *
+   * <p>This is a special case when "event" is of length 1 and the first character code is 00. This
+   * is encoded as byte sequence 01 00 by V1 coder, which can be treated as an empty "fields" field
+   * by V2 decoder.
+   */
+  @Test
+  public void testBackwardsCompatibility_canDecodeVersion1withEventLength1()
+      throws IOException, DecoderException {
+
+    SplunkEvent expectedEvent =
+        SplunkEvent.newBuilder()
+            .withEvent(new String(new byte[] {0}, StandardCharsets.UTF_8))
+            .withHost("h")
+            .withIndex("i")
+            .withSource("s")
+            .withSourceType("st")
+            .withTime(1234L)
+            .create();
+
+    String hex = "0100000000000004d2010168010173010273740101690100";
+    SplunkEvent actualEvent = SplunkEventCoder.of().decode(fromHex(hex));
+
+    assertThat(actualEvent, is(equalTo(expectedEvent)));
+  }
+
+  /**
+   * Tests whether {@link SplunkEventCoder} is able to decode a {@link SplunkEvent} encoded using
+   * the older coder version 2 (commit 5e53040), without the newly added "fields" field.
+   */
+  @Test
+  public void testBackwardsCompatibility_canDecodeVersion2() throws IOException, DecoderException {
+
+    SplunkEvent expectedEvent =
+        SplunkEvent.newBuilder()
+            .withEvent("e")
+            .withHost("h")
+            .withIndex("i")
+            .withSource("s")
+            .withSourceType("st")
+            .withTime(1234L)
+            .create();
+
+    String hex = "0100000000000004d201016801017301027374010169000165";
+    SplunkEvent actualEvent = SplunkEventCoder.of().decode(fromHex(hex));
+
+    assertThat(actualEvent, is(equalTo(expectedEvent)));
+  }
+
+  private static InputStream fromHex(String hex) throws DecoderException {
+    byte[] b = Hex.decodeHex(hex);
+    return new ByteArrayInputStream(b);
+  }
+}
---
+++ v2/common/src/test/java/com/google/cloud/teleport/v2/transforms/SplunkConvertersTest.java
@@ -0,0 +1,266 @@
+/*
+ * Copyright (C) 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.cloud.teleport.v2.transforms;
+
+import com.google.api.client.util.DateTime;
+import com.google.cloud.teleport.v2.coders.FailsafeElementCoder;
+import com.google.cloud.teleport.v2.coders.SplunkEventCoder;
+import com.google.cloud.teleport.v2.values.FailsafeElement;
+import com.google.gson.Gson;
+import org.apache.beam.sdk.coders.StringUtf8Coder;
+import org.apache.beam.sdk.io.splunk.SplunkEvent;
+import org.apache.beam.sdk.testing.NeedsRunner;
+import org.apache.beam.sdk.testing.PAssert;
+import org.apache.beam.sdk.testing.TestPipeline;
+import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.values.PCollectionTuple;
+import org.apache.beam.sdk.values.TupleTag;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+/** Unit tests for {@link com.google.cloud.teleport.v2.transforms.SplunkConverters} class. */
+public class SplunkConvertersTest {
+
+  private static final TupleTag<SplunkEvent> SPLUNK_EVENT_OUT = new TupleTag<SplunkEvent>() {};
+  private static final TupleTag<FailsafeElement<String, String>> SPLUNK_EVENT_DEADLETTER_OUT =
+      new TupleTag<FailsafeElement<String, String>>() {};
+  @Rule public final transient TestPipeline pipeline = TestPipeline.create();
+
+  private static final Gson GSON = new Gson();
+
+  /** Test successful conversion of simple String payloads. */
+  @Test
+  @Category(NeedsRunner.class)
+  public void testFailsafeStringToSplunkEventSimpleStrings() {
+
+    FailsafeElement<String, String> hello = FailsafeElement.of("hello", "hello");
+    FailsafeElement<String, String> world = FailsafeElement.of("world", "world");
+
+    pipeline.getCoderRegistry().registerCoderForClass(SplunkEvent.class, SplunkEventCoder.of());
+
+    PCollectionTuple tuple =
+        pipeline
+            .apply(
+                Create.of(hello, world)
+                    .withCoder(FailsafeElementCoder.of(StringUtf8Coder.of(), StringUtf8Coder.of())))
+            .apply(
+                SplunkConverters.failsafeStringToSplunkEvent(
+                    SPLUNK_EVENT_OUT, SPLUNK_EVENT_DEADLETTER_OUT));
+
+    PAssert.that(tuple.get(SPLUNK_EVENT_DEADLETTER_OUT)).empty();
+    PAssert.that(tuple.get(SPLUNK_EVENT_OUT))
+        .containsInAnyOrder(
+            SplunkEvent.newBuilder().withEvent("hello").create(),
+            SplunkEvent.newBuilder().withEvent("world").create());
+
+    pipeline.run();
+  }
+
+  /** Test successful conversion of invalid JSON messages. */
+  @Test
+  @Category(NeedsRunner.class)
+  public void testFailsafeStringToSplunkEventInvalidJSON() {
+
+    FailsafeElement<String, String> input =
+        FailsafeElement.of(
+            "" + "\t\"name\": \"Jim\",\n" + "}", "{\n" + "\t\"name\": \"Jim\",\n" + "}");
+
+    SplunkEvent expectedSplunkEvent =
+        SplunkEvent.newBuilder().withEvent("{\n" + "\t\"name\": \"Jim\",\n" + "}").create();
+
+    matchesSplunkEvent(input, expectedSplunkEvent);
+  }
+
+  /** Test successful conversion of JSON messages. */
+  @Test
+  @Category(NeedsRunner.class)
+  public void testFailsafeStringToSplunkEventValidJSON() {
+
+    FailsafeElement<String, String> input =
+        FailsafeElement.of(
+            "" + "\t\"name\": \"Jim\",\n" + "}", "{\n" + "\t\"name\": \"Jim\"\n" + "}");
+
+    SplunkEvent expectedSplunkEvent =
+        SplunkEvent.newBuilder().withEvent("{\n" + "\t\"name\": \"Jim\"\n" + "}").create();
+
+    matchesSplunkEvent(input, expectedSplunkEvent);
+  }
+
+  /** Test successful conversion of JSON messages with a valid timestamp. */
+  @Test
+  @Category(NeedsRunner.class)
+  public void testFailsafeStringToSplunkEventValidTimestamp() {
+
+    FailsafeElement<String, String> input =
+        FailsafeElement.of(
+            "",
+            "{\n"
+                + "\t\"name\": \"Jim\",\n"
+                + "\t\"logName\": \"test-log-name\",\n"
+                + "\t\"timestamp\": \"2019-10-15T11:32:26.553Z\"\n"
+                + "}");
+
+    SplunkEvent expectedSplunkEvent =
+        SplunkEvent.newBuilder()
+            .withEvent(
+                "{\n"
+                    + "\t\"name\": \"Jim\",\n"
+                    + "\t\"logName\": \"test-log-name\",\n"
+                    + "\t\"timestamp\": \"2019-10-15T11:32:26.553Z\"\n"
+                    + "}")
+            .withTime(DateTime.parseRfc3339("2019-10-15T11:32:26.553Z").getValue())
+            .create();
+
+    matchesSplunkEvent(input, expectedSplunkEvent);
+  }
+
+  /** Test successful conversion of JSON messages with an invalid timestamp. */
+  @Test
+  @Category(NeedsRunner.class)
+  public void testFailsafeStringToSplunkEventInValidTimestamp() {
+
+    FailsafeElement<String, String> input =
+        FailsafeElement.of(
+            "",
+            "{\n"
+                + "\t\"name\": \"Jim\",\n"
+                + "\t\"logName\": \"test-log-name\",\n"
+                + "\t\"timestamp\": \"2019-1011:32:26.553Z\"\n"
+                + "}");
+
+    SplunkEvent expectedSplunkEvent =
+        SplunkEvent.newBuilder()
+            .withEvent(
+                "{\n"
+                    + "\t\"name\": \"Jim\",\n"
+                    + "\t\"logName\": \"test-log-name\",\n"
+                    + "\t\"timestamp\": \"2019-1011:32:26.553Z\"\n"
+                    + "}")
+            .create();
+
+    matchesSplunkEvent(input, expectedSplunkEvent);
+  }
+
+  /** Test successful conversion of JSON messages with a user provided _metadata. */
+  @Test
+  @Category(NeedsRunner.class)
+  public void testFailsafeStringToSplunkEventValidSource() {
+
+    FailsafeElement<String, String> input =
+        FailsafeElement.of(
+            "",
+            "{\n"
+                + "\t\"name\": \"Jim\",\n"
+                + "\t\"_metadata\": {\"source\": \"test-log-name\"}\n"
+                + "}");
+
+    SplunkEvent expectedSplunkEvent =
+        SplunkEvent.newBuilder()
+            .withEvent("{\"name\":\"Jim\"}")
+            .withSource("test-log-name")
+            .create();
+
+    matchesSplunkEvent(input, expectedSplunkEvent);
+  }
+
+  /** Test successful conversion of JSON messages with a user provided host. */
+  @Test
+  @Category(NeedsRunner.class)
+  public void testFailsafeStringToSplunkEventValidHost() {
+
+    FailsafeElement<String, String> input =
+        FailsafeElement.of(
+            "",
+            "{\n"
+                + "\t\"name\": \"Jim\",\n"
+                + "\t\"_metadata\": {\"host\": \"test-host\"}\n"
+                + "}");
+
+    SplunkEvent expectedSplunkEvent =
+        SplunkEvent.newBuilder().withEvent("{\"name\":\"Jim\"}").withHost("test-host").create();
+
+    matchesSplunkEvent(input, expectedSplunkEvent);
+  }
+
+  /** Test successful conversion of JSON messages with a user provided index. */
+  @Test
+  @Category(NeedsRunner.class)
+  public void testFailsafeStringToSplunkEventValidIndex() {
+
+    FailsafeElement<String, String> input =
+        FailsafeElement.of(
+            "",
+            "{\n"
+                + "\t\"name\": \"Jim\",\n"
+                + "\t\"_metadata\": {\"host\": \"test-host\","
+                + "\"index\":\"test-index\"}\n"
+                + "}");
+
+    SplunkEvent expectedSplunkEvent =
+        SplunkEvent.newBuilder()
+            .withEvent("{\"name\":\"Jim\"}")
+            .withHost("test-host")
+            .withIndex("test-index")
+            .create();
+
+    matchesSplunkEvent(input, expectedSplunkEvent);
+  }
+
+  /** Test successful conversion of JSON messages with provided overrides for time and source. */
+  @Test
+  @Category(NeedsRunner.class)
+  public void testFailsafeStringToSplunkEventValidTimeOverride() {
+
+    FailsafeElement<String, String> input =
+        FailsafeElement.of(
+            "",
+            "{\n"
+                + "\t\"timestamp\": \"2019-10-15T11:32:26.553Z\",\n"
+                + "\t\"_metadata\": {\"time\": \"2019-11-22T11:32:26.553Z\", "
+                + "\"source\": \"test-source-name\"}\n"
+                + "}");
+
+    SplunkEvent expectedSplunkEvent =
+        SplunkEvent.newBuilder()
+            .withEvent("{" + "\"timestamp\":\"2019-10-15T11:32:26.553Z\"" + "}")
+            .withSource("test-source-name")
+            .withTime(DateTime.parseRfc3339("2019-11-22T11:32:26.553Z").getValue())
+            .create();
+
+    matchesSplunkEvent(input, expectedSplunkEvent);
+  }
+
+  @Category(NeedsRunner.class)
+  private void matchesSplunkEvent(
+      FailsafeElement<String, String> input, SplunkEvent expectedSplunkEvent) {
+    pipeline.getCoderRegistry().registerCoderForClass(SplunkEvent.class, SplunkEventCoder.of());
+
+    PCollectionTuple tuple =
+        pipeline
+            .apply(
+                Create.of(input)
+                    .withCoder(FailsafeElementCoder.of(StringUtf8Coder.of(), StringUtf8Coder.of())))
+            .apply(
+                SplunkConverters.failsafeStringToSplunkEvent(
+                    SPLUNK_EVENT_OUT, SPLUNK_EVENT_DEADLETTER_OUT));
+
+    PAssert.that(tuple.get(SPLUNK_EVENT_DEADLETTER_OUT)).empty();
+    PAssert.that(tuple.get(SPLUNK_EVENT_OUT)).containsInAnyOrder(expectedSplunkEvent);
+
+    pipeline.run();
+  }
+}
