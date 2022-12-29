a7d9d85e5a6550b68c284416a6a6f701f8c79b11
Alexey Kuznetsov <alexeyku@google.com>
2022-12-29 20:37:35 +0000

    Refreshed options and template class to latest annotations

--- v2/googlecloud-to-googlecloud/src/main/java/com/google/cloud/teleport/v2/options/BigtableChangeStreamsToBigQueryOptions.java
+++ v2/googlecloud-to-googlecloud/src/main/java/com/google/cloud/teleport/v2/options/BigtableChangeStreamsToBigQueryOptions.java
@@ -15,10 +15,9 @@
  */
 package com.google.cloud.teleport.v2.options;
 
-//import com.google.cloud.teleport.metadata.TemplateParameter;
+import com.google.cloud.teleport.metadata.TemplateParameter;
 import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
 import org.apache.beam.sdk.options.Default;
-import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.Validation;
 
 /**
@@ -27,262 +26,238 @@ import org.apache.beam.sdk.options.Validation;
  */
 public interface BigtableChangeStreamsToBigQueryOptions extends DataflowPipelineOptions {
 
-  @Description("REMOVE LATER")
-  // @TemplateParameter.Text(
-  //     order = 1,
-  //     description = "Cloud Bigtable instance ID",
-  //     helpText = "The Cloud Bigtable instance to read change streams from.")
+  @TemplateParameter.Text(
+      order = 1,
+      description = "Cloud Bigtable instance ID",
+      helpText = "The Cloud Bigtable instance to read change streams from.")
   @Validation.Required
   String getBigtableInstanceId();
 
   void setBigtableInstanceId(String value);
 
-  @Description("REMOVE LATER")
-  // @TemplateParameter.Text(
-  //     order = 2,
-  //     description = "Cloud Bigtable table ID",
-  //     helpText = "The Cloud Bigtable table to read change streams from.")
+  @TemplateParameter.Text(
+      order = 2,
+      description = "Cloud Bigtable table ID",
+      helpText = "The Cloud Bigtable table to read change streams from.")
   @Validation.Required
   String getBigtableTableId();
 
   void setBigtableTableId(String value);
 
-  @Description("REMOVE LATER")
-  // @TemplateParameter.Text(
-  //     order = 3,
-  //     description = "BigQuery dataset",
-  //     helpText = "The BigQuery dataset for change streams output.")
+  @TemplateParameter.Text(
+      order = 3,
+      description = "BigQuery dataset",
+      helpText = "The BigQuery dataset for change streams output.")
   @Validation.Required
   String getBigQueryDataset();
 
   void setBigQueryDataset(String value);
 
-  @Description("REMOVE LATER")
-  // @TemplateParameter.Text(
-  //     order = 4,
-  //     description = "Cloud Bigtable application profile name",
-  //     helpText = "The application profile is used to distinguish workload in Cloud Bigtable")
+  @TemplateParameter.Text(
+      order = 4,
+      description = "Cloud Bigtable application profile name",
+      helpText = "The application profile is used to distinguish workload in Cloud Bigtable")
   @Validation.Required
   String getBigtableAppProfile();
 
   void setBigtableAppProfile(String value);
 
-  @Description("REMOVE LATER")
-  // @TemplateParameter.ProjectId(
-  //     order = 5,
-  //     optional = true,
-  //     description = "Cloud Bigtable Project ID",
-  //     helpText =
-  //         "Project to read change streams from. The default for this parameter is the project "
-  //             + "where the Dataflow pipeline is running.")
+  @TemplateParameter.ProjectId(
+      order = 5,
+      optional = true,
+      description = "Cloud Bigtable Project ID",
+      helpText =
+          "Project to read change streams from. The default for this parameter is the project "
+              + "where the Dataflow pipeline is running.")
   @Default.String("")
   String getBigtableProjectId();
 
   void setBigtableProjectId(String projectId);
 
-
-  @Description("REMOVE LATER")
-  // @TemplateParameter.Text(
-  //     order = 6,
-  //     optional = true,
-  //     description = "Cloud Bigtable metadata instance ID",
-  //     helpText = "The Cloud Bigtable instance to use for the change streams connector metadata table.")
+  @TemplateParameter.Text(
+      order = 6,
+      optional = true,
+      description = "Cloud Bigtable metadata instance ID",
+      helpText = "The Cloud Bigtable instance to use for the change streams connector metadata table.")
   @Default.String("")
   String getBigtableMetadataInstanceId();
 
   void setBigtableMetadataInstanceId(String value);
 
-  @Description("REMOVE LATER")
-  // @TemplateParameter.Text(
-  //     order = 7,
-  //     optional = true,
-  //     description = "Cloud Bigtable metadata table ID",
-  //     helpText =
-  //         "The Cloud Bigtable change streams connector metadata table ID to use. If not "
-  //             + "provided, a Cloud Bigtable change streams connector metadata table will automatically be "
-  //             + "created during the pipeline flow. This parameter must be provided when updating an "
-  //             + "existing pipeline and should not be provided otherwise.")
+  @TemplateParameter.Text(
+      order = 7,
+      optional = true,
+      description = "Cloud Bigtable metadata table ID",
+      helpText =
+          "The Cloud Bigtable change streams connector metadata table ID to use. If not "
+              + "provided, a Cloud Bigtable change streams connector metadata table will automatically be "
+              + "created during the pipeline flow. This parameter must be provided when updating an "
+              + "existing pipeline and should not be provided otherwise.")
   @Default.String("__change_stream_md_table")
   String getBigtableMetadataTableTableId();
 
   void setBigtableMetadataTableTableId(String value);
 
-  @Description("REMOVE LATER")
-  // @TemplateParameter.Text(
-  //     order = 8,
-  //     optional = true,
-  //     description = "Cloud Bigtable column families to ignore",
-  //     helpText = "A comma-separated list of column family names changes to which won't be captured")
+  @TemplateParameter.Text(
+      order = 8,
+      optional = true,
+      description = "Cloud Bigtable column families to ignore",
+      helpText = "A comma-separated list of column family names changes to which won't be captured")
   @Default.String("")
   String getIgnoreColumnFamilies();
 
   void setIgnoreColumnFamilies(String value);
 
-  @Description("REMOVE LATER")
-  // @TemplateParameter.Text(
-  //     order = 9,
-  //     optional = true,
-  //     description = "Cloud Bigtable columns to ignore",
-  //     helpText = "A comma-separated list of column names changes to which won't be captured")
+  @TemplateParameter.Text(
+      order = 9,
+      optional = true,
+      description = "Cloud Bigtable columns to ignore",
+      helpText = "A comma-separated list of column names changes to which won't be captured")
   @Default.String("")
   String getIgnoreColumns();
 
   void setIgnoreColumns(String value);
 
-
-  @Description("REMOVE LATER")
-  // @TemplateParameter.DateTime(
-  //     order = 10,
-  //     optional = true,
-  //     description = "The timestamp to read change streams from",
-  //     helpText =
-  //         "The starting DateTime, inclusive, to use for reading change streams "
-  //             + "(https://tools.ietf.org/html/rfc3339). For example, 2022-05-05T07:59:59Z. Defaults to the "
-  //             + "timestamp when the pipeline starts.")
+  @TemplateParameter.DateTime(
+      order = 10,
+      optional = true,
+      description = "The timestamp to read change streams from",
+      helpText =
+          "The starting DateTime, inclusive, to use for reading change streams "
+              + "(https://tools.ietf.org/html/rfc3339). For example, 2022-05-05T07:59:59Z. Defaults to the "
+              + "timestamp when the pipeline starts.")
   @Default.String("")
   String getStartTimestamp();
 
   void setStartTimestamp(String startTimestamp);
 
-  @Description("REMOVE LATER")
-  // @TemplateParameter.DateTime(
-  //     order = 11,
-  //     optional = true,
-  //     description = "The timestamp to read change streams to",
-  //     helpText =
-  //         "The ending DateTime, inclusive, to use for reading change streams "
-  //             + "(https://tools.ietf.org/html/rfc3339). Ex-2022-05-05T07:59:59Z. Defaults to an infinite "
-  //             + "time in the future.")
+  @TemplateParameter.DateTime(
+      order = 11,
+      optional = true,
+      description = "The timestamp to read change streams to",
+      helpText =
+          "The ending DateTime, inclusive, to use for reading change streams "
+              + "(https://tools.ietf.org/html/rfc3339). Ex-2022-05-05T07:59:59Z. Defaults to an infinite "
+              + "time in the future.")
   @Default.String("")
   String getEndTimestamp();
 
   void setEndTimestamp(String startTimestamp);
 
-  @Description("REMOVE LATER")
-  // @TemplateParameter.Boolean(
-  //     order = 12,
-  //     optional = true,
-  //     description = "Write values as BigQuery BYTES",
-  //     helpText = "When set true rowkeys are written to BYTES column, otherwise to STRING column. " +
-  //         "Defaults to false.")
+  @TemplateParameter.Boolean(
+      order = 12,
+      optional = true,
+      description = "Write values as BigQuery BYTES",
+      helpText = "When set true rowkeys are written to BYTES column, otherwise to STRING column. " +
+          "Defaults to false.")
   @Default.Boolean(false)
   Boolean getWriteRowkeyAsBytes();
 
   void setWriteRowkeyAsBytes(Boolean value);
 
-  @Description("REMOVE LATER")
-  // @TemplateParameter.Boolean(
-  //     order = 13,
-  //     optional = true,
-  //     description = "Write values as BigQuery BYTES",
-  //     helpText = "When set true values are written to BYTES column, otherwise to STRING column. " +
-  //         "Defaults to false.")
+  @TemplateParameter.Boolean(
+      order = 13,
+      optional = true,
+      description = "Write values as BigQuery BYTES",
+      helpText = "When set true values are written to BYTES column, otherwise to STRING column. " +
+          "Defaults to false.")
   @Default.Boolean(false)
   Boolean getWriteValuesAsBytes();
 
   void setWriteValuesAsBytes(Boolean value);
 
-  @Description("REMOVE LATER")
-  // @TemplateParameter.Boolean(
-  //     order = 14,
-  //     optional = true,
-  //     description = "Write Bigtable timestamp as BigQuery INT",
-  //     helpText = "When set true values are written to INT column, otherwise to TIMESTAMP column. " +
-  //         "Columns affected: `timestamp`, `timestamp_from`, `timestamp_to`. " +
-  //         "Defaults to false.")
+  @TemplateParameter.Boolean(
+      order = 14,
+      optional = true,
+      description = "Write Bigtable timestamp as BigQuery INT",
+      helpText = "When set true values are written to INT column, otherwise to TIMESTAMP column. " +
+          "Columns affected: `timestamp`, `timestamp_from`, `timestamp_to`. " +
+          "Defaults to false.")
   @Default.Boolean(false)
   Boolean getWriteNumericTimestamps();
 
   void setWriteNumericTimestamps(Boolean value);
 
-  @Description("REMOVE LATER")
-  // @TemplateParameter.ProjectId(
-  //     order = 15,
-  //     optional = true,
-  //     description = "BigQuery charset name when reading values and column qualifiers",
-  //     helpText = "BigQuery charset name when reading values and column qualifiers. " +
-  //         "Default is UTF-8")
+  @TemplateParameter.ProjectId(
+      order = 15,
+      optional = true,
+      description = "BigQuery charset name when reading values and column qualifiers",
+      helpText = "BigQuery charset name when reading values and column qualifiers. " +
+          "Default is UTF-8")
   @Default.String("UTF-8")
   String getBigtableCharset();
 
   void setBigtableCharset(String value);
 
-  @Description("REMOVE LATER")
-  // @TemplateParameter.ProjectId(
-  //     order = 16,
-  //     optional = true,
-  //     description = "BigQuery project ID",
-  //     helpText = "The BigQuery Project. Default is the project for the Dataflow job.")
+  @TemplateParameter.ProjectId(
+      order = 16,
+      optional = true,
+      description = "BigQuery project ID",
+      helpText = "The BigQuery Project. Default is the project for the Dataflow job.")
   @Default.String("")
   String getBigQueryProjectId();
 
   void setBigQueryProjectId(String value);
 
-  @Description("REMOVE LATER")
-  // @TemplateParameter.Text(
-  //     order = 17,
-  //     optional = true,
-  //     description = "BigQuery table name",
-  //     helpText = "The BigQuery table name that contains the change log. Default: {bigtableTableId}_changelog")
+  @TemplateParameter.Text(
+      order = 17,
+      optional = true,
+      description = "BigQuery table name",
+      helpText = "The BigQuery table name that contains the change log. Default: {bigtableTableId}_changelog")
   @Default.String("")
   String getBigQueryChangelogTableName();
 
   void setBigQueryChangelogTableName(String value);
 
-  @Description("REMOVE LATER")
-  // @TemplateParameter.Text(
-  //     order = 18,
-  //     optional = true,
-  //     description = "Changelog table will be partitioned at specified granularity",
-  //     helpText = "When set, table partitioning will be in effect. Accepted values: `daily`, " +
-  //                "`monthly`, `yearly`. Default is no partitioning.")
+  @TemplateParameter.Text(
+      order = 18,
+      optional = true,
+      description = "Changelog table will be partitioned at specified granularity",
+      helpText = "When set, table partitioning will be in effect. Accepted values: `HOUR`, " +
+          "`DAY`, `MONTH`, `YEAR`. Default is no partitioning.")
   @Default.String("")
   String getBigQueryChangelogTablePartitionGranularity();
 
   void setBigQueryChangelogTablePartitionGranularity(String value);
 
-  @Description("REMOVE LATER")
-  // @TemplateParameter.Long(
-  //     order = 19,
-  //     optional = true,
-  //     description = "Sets partition expiration time in milliseconds",
-  //     helpText = "When set true partitions older than specified number of milliseconds will be " +
-  //         "deleted. Default is no expiration.")
+  @TemplateParameter.Long(
+      order = 19,
+      optional = true,
+      description = "Sets partition expiration time in milliseconds",
+      helpText = "When set true partitions older than specified number of milliseconds will be " +
+           "deleted. Default is no expiration.")
   Long getBigQueryChangelogTablePartitionExpirationMs();
 
   void setBigQueryChangelogTablePartitionExpirationMs(Long value);
 
-  @Description("REMOVE LATER")
-  // @TemplateParameter.Text(
-  //     order = 20,
-  //     optional = true,
-  //     description = "Optional changelog table columns to be disabled",
-  //     helpText = "A comma-separated list of the changelog columns which will not be created and " +
-  //         "populated if specified. Supported values should be from the following list: `is_gc`, " +
-  //         "`source_instance`, `source_cluster`, `source_table`, `tiebreaker`. " +
-  //         "Defaults to all columns to be populated")
+  @TemplateParameter.Text(
+      order = 20,
+      optional = true,
+      description = "Optional changelog table columns to be disabled",
+      helpText = "A comma-separated list of the changelog columns which will not be created and " +
+          "populated if specified. Supported values should be from the following list: `is_gc`, " +
+          "`source_instance`, `source_cluster`, `source_table`, `tiebreaker`, " +
+          "`big_query_commit_timestamp`. Defaults to all columns to be populated")
   String getBigQueryChangelogTableFieldsToIgnore();
 
   void setBigQueryChangelogTableFieldsToIgnore(String value);
 
-  @Description("REMOVE LATER")
-  // @TemplateParameter.GcsWriteFolder(
-  //     order = 21,
-  //     optional = true,
-  //     description = "Dead letter queue directory",
-  //     helpText =
-  //         "The file path to store any unprocessed records with the reason they failed to be processed. Default is a directory under the Dataflow job's temp location. The default value is enough under most conditions.")
+  @TemplateParameter.GcsWriteFolder(
+      order = 21,
+      optional = true,
+      description = "Dead letter queue directory",
+      helpText =
+          "The file path to store any unprocessed records with the reason they failed to be processed. Default is a directory under the Dataflow job's temp location. The default value is enough under most conditions.")
   @Default.String("")
   String getDlqDirectory();
 
   void setDlqDirectory(String value);
 
-  @Description("REMOVE LATER")
-  // @TemplateParameter.Integer(
-  //     order = 22,
-  //     optional = true,
-  //     description = "Dead letter queue retry minutes",
-  //     helpText = "The number of minutes between dead letter queue retries. Defaults to 10.")
+  @TemplateParameter.Integer(
+      order = 22,
+      optional = true,
+      description = "Dead letter queue retry minutes",
+      helpText = "The number of minutes between dead letter queue retries. Defaults to 10.")
   @Default.Integer(10)
   Integer getDlqRetryMinutes();
 
