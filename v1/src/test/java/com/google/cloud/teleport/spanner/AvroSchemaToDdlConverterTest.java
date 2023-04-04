addd70a49844ba2aed828755035796420147ac5d
Cloud Teleport <cloud-teleport@google.com>
2023-04-04 10:07:53 -0700

    Adds Avro format Import/Export support for Foreign Key ON DELETE CASCADE action for GoogleSQL dialect
    PiperOrigin-RevId: 521795272

--- v1/src/test/java/com/google/cloud/teleport/spanner/AvroSchemaToDdlConverterTest.java
+++ v1/src/test/java/com/google/cloud/teleport/spanner/AvroSchemaToDdlConverterTest.java
@@ -146,6 +146,9 @@ public class AvroSchemaToDdlConverterTest {
             + "  \"spannerForeignKey_0\" : "
             + "  \"ALTER TABLE `Users` ADD CONSTRAINT `fk` FOREIGN KEY (`first_name`) "
             + "  REFERENCES `AllowedNames` (`first_name`)\","
+            + "  \"spannerForeignKey_1\" : "
+            + "  \"ALTER TABLE `Users` ADD CONSTRAINT `fk_odc` FOREIGN KEY (`last_name`) "
+            + "  REFERENCES `AllowedNames` (`last_name`) ON DELETE CASCADE\","
             + "  \"spannerCheckConstraint_0\" : "
             + "  \"CONSTRAINT `ck` CHECK(`first_name` != 'last_name')\""
             + "}";
@@ -183,7 +186,10 @@ public class AvroSchemaToDdlConverterTest {
                 + " ) PRIMARY KEY (`id` ASC, `gen_id` ASC, `last_name` DESC)"
                 + " CREATE INDEX `UsersByFirstName` ON `Users` (`first_name`)"
                 + " ALTER TABLE `Users` ADD CONSTRAINT `fk`"
-                + " FOREIGN KEY (`first_name`) REFERENCES `AllowedNames` (`first_name`)"));
+                + " FOREIGN KEY (`first_name`) REFERENCES `AllowedNames` (`first_name`)"
+                + " ALTER TABLE `Users` ADD CONSTRAINT `fk_odc`"
+                + " FOREIGN KEY (`last_name`) REFERENCES "
+                + "`AllowedNames` (`last_name`) ON DELETE CASCADE"));
   }
 
   @Test
