7b624a7e2c2629849bb14f525a2f65e742703d12
pranavbhandari <pranavbhandari@google.com>
2023-05-30 10:53:23 -0700

    Add Pub/Sub to Avro flex template.
    PiperOrigin-RevId: 536449395

---
+++ v2/googlecloud-to-googlecloud/src/main/java/com/google/cloud/teleport/v2/templates/PubsubToAvro.java
@@ -0,0 +1,245 @@
+/*
+ * Copyright (C) 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.cloud.teleport.v2.templates;
+
+import static com.google.cloud.teleport.v2.utils.DurationUtils.parseDuration;
+
+import com.google.cloud.teleport.metadata.Template;
+import com.google.cloud.teleport.metadata.TemplateCategory;
+import com.google.cloud.teleport.metadata.TemplateParameter;
+import com.google.cloud.teleport.v2.avro.AvroPubsubMessageRecord;
+import com.google.cloud.teleport.v2.io.WindowedFilenamePolicy;
+import com.google.cloud.teleport.v2.options.WindowedFilenamePolicyOptions;
+import com.google.cloud.teleport.v2.templates.PubsubToAvro.Options;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.PipelineResult;
+import org.apache.beam.sdk.io.AvroIO;
+import org.apache.beam.sdk.io.FileBasedSink;
+import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
+import org.apache.beam.sdk.io.gcp.pubsub.PubsubMessage;
+import org.apache.beam.sdk.options.Default;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.options.StreamingOptions;
+import org.apache.beam.sdk.options.Validation.Required;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.windowing.FixedWindows;
+import org.apache.beam.sdk.transforms.windowing.Window;
+import org.apache.beam.sdk.values.PCollection;
+
+/**
+ * This pipeline ingests incoming data from a Cloud Pub/Sub topic and outputs the raw data into
+ * windowed Avro files at the specified output directory.
+ *
+ * <p>Files output will have the following schema:
+ *
+ * <pre>
+ *   {
+ *      "type": "record",
+ *      "name": "AvroPubsubMessageRecord",
+ *      "namespace": "com.google.cloud.teleport.v2.avro",
+ *      "fields": [
+ *        {"name": "message", "type": {"type": "array", "items": "bytes"}},
+ *        {"name": "attributes", "type": {"type": "map", "values": "string"}},
+ *        {"name": "timestamp", "type": "long"}
+ *      ]
+ *   }
+ * </pre>
+ *
+ * <p>Check out <a
+ * href="https://github.com/GoogleCloudPlatform/DataflowTemplates/blob/main/v1/README_Cloud_PubSub_to_Avro.md">README</a>
+ * for instructions on how to use or modify this template.
+ */
+@Template(
+    name = "Cloud_PubSub_to_Avro_Flex",
+    category = TemplateCategory.STREAMING,
+    displayName = "Pub/Sub to Avro Files on Cloud Storage",
+    description =
+        "Streaming pipeline. Reads from a Pub/Sub subscription and outputs windowed Avro files to"
+            + " the specified directory.",
+    optionsClass = Options.class,
+    flexContainerName = "pubsub-to-avro",
+    documentation =
+        "https://cloud.google.com/dataflow/docs/guides/templates/provided/pubsub-to-avro",
+    contactInformation = "https://cloud.google.com/support")
+public class PubsubToAvro {
+
+  /**
+   * Options supported by the pipeline.
+   *
+   * <p>Inherits standard configuration options.
+   */
+  public interface Options
+      extends PipelineOptions, StreamingOptions, WindowedFilenamePolicyOptions {
+    @TemplateParameter.PubsubSubscription(
+        order = 1,
+        optional = true,
+        description = "Pub/Sub input subscription",
+        helpText =
+            "Pub/Sub subscription to read the input from, in the format of"
+                + " 'projects/your-project-id/subscriptions/your-subscription-name'",
+        example = "projects/your-project-id/subscriptions/your-subscription-name")
+    String getInputSubscription();
+
+    void setInputSubscription(String value);
+
+    @TemplateParameter.PubsubTopic(
+        order = 2,
+        optional = true,
+        description = "Pub/Sub input topic",
+        helpText =
+            "Pub/Sub topic to read the input from, in the format of "
+                + "'projects/your-project-id/topics/your-topic-name'")
+    String getInputTopic();
+
+    void setInputTopic(String value);
+
+    @TemplateParameter.GcsWriteFolder(
+        order = 3,
+        optional = false,
+        description = "Output file directory in Cloud Storage",
+        helpText =
+            "The path and filename prefix for writing output files. Must end with a slash. DateTime"
+                + " formatting is used to parse directory path for date & time formatters.")
+    @Required
+    String getOutputDirectory();
+
+    void setOutputDirectory(String value);
+
+    @TemplateParameter.Text(
+        order = 4,
+        optional = true,
+        description = "Output filename prefix of the files to write",
+        helpText = "The prefix to place on each windowed file.",
+        regexes = "^[a-zA-Z\\-]+$")
+    @Default.String("output")
+    String getOutputFilenamePrefix();
+
+    void setOutputFilenamePrefix(String value);
+
+    @TemplateParameter.Text(
+        order = 5,
+        optional = true,
+        description = "Output filename suffix of the files to write",
+        helpText =
+            "The suffix to place on each windowed file. Typically a file extension such "
+                + "as .txt or .csv.")
+    @Default.String("")
+    String getOutputFilenameSuffix();
+
+    void setOutputFilenameSuffix(String value);
+
+    @TemplateParameter.GcsWriteFolder(
+        order = 6,
+        optional = false,
+        description = "Temporary Avro write directory",
+        helpText = "Directory for temporary Avro files.")
+    @Required
+    String getAvroTempDirectory();
+
+    void setAvroTempDirectory(String value);
+  }
+
+  /**
+   * Main entry point for executing the pipeline.
+   *
+   * @param args The command-line arguments to the pipeline.
+   */
+  public static void main(String[] args) {
+
+    Options options = PipelineOptionsFactory.fromArgs(args).withValidation().as(Options.class);
+    options.setStreaming(true);
+
+    run(options);
+  }
+
+  /**
+   * Runs the pipeline with the supplied options.
+   *
+   * @param options The execution parameters to the pipeline.
+   * @return The result of the pipeline execution.
+   */
+  public static PipelineResult run(Options options) {
+    // Create the pipeline
+    Pipeline pipeline = Pipeline.create(options);
+
+    PCollection<PubsubMessage> messages = null;
+
+    /*
+     * Steps:
+     *   1) Read messages from PubSub
+     *   2) Window the messages into minute intervals specified by the executor.
+     *   3) Output the windowed data into Avro files, one per window by default.
+     */
+
+    if (options.getInputSubscription() != null) {
+      messages =
+          pipeline.apply(
+              "Read PubSub Events",
+              PubsubIO.readMessagesWithAttributes()
+                  .fromSubscription(options.getInputSubscription()));
+    } else {
+      messages =
+          pipeline.apply(
+              "Read PubSub Events",
+              PubsubIO.readMessagesWithAttributes().fromTopic(options.getInputTopic()));
+    }
+    messages
+        .apply("Map to Archive", ParDo.of(new PubsubMessageToArchiveDoFn()))
+        .apply(
+            options.getWindowDuration() + " Window",
+            Window.into(FixedWindows.of(parseDuration(options.getWindowDuration()))))
+
+        // Apply windowed file writes. Use a NestedValueProvider because the filename
+        // policy requires a resourceId generated from the input value at runtime.
+        .apply(
+            "Write File(s)",
+            AvroIO.write(AvroPubsubMessageRecord.class)
+                .to(
+                    WindowedFilenamePolicy.writeWindowedFiles()
+                        .withOutputDirectory(options.getOutputDirectory())
+                        .withOutputFilenamePrefix(options.getOutputFilenamePrefix())
+                        .withShardTemplate(options.getOutputShardTemplate())
+                        .withSuffix(options.getOutputFilenameSuffix())
+                        .withYearPattern(options.getYearPattern())
+                        .withMonthPattern(options.getMonthPattern())
+                        .withDayPattern(options.getDayPattern())
+                        .withHourPattern(options.getHourPattern())
+                        .withMinutePattern(options.getMinutePattern()))
+                .withTempDirectory(
+                    FileBasedSink.convertToFileResourceIfPossible(options.getOutputDirectory()))
+                .withWindowedWrites()
+                .withNumShards(options.getNumShards()));
+
+    // Execute the pipeline and return the result.
+    return pipeline.run();
+  }
+
+  /**
+   * Converts an incoming {@link PubsubMessage} to the {@link AvroPubsubMessageRecord} class by
+   * copying it's fields and the timestamp of the message.
+   */
+  static class PubsubMessageToArchiveDoFn extends DoFn<PubsubMessage, AvroPubsubMessageRecord> {
+    @ProcessElement
+    public void processElement(ProcessContext context) {
+      PubsubMessage message = context.element();
+      context.output(
+          new AvroPubsubMessageRecord(
+              message.getPayload(), message.getAttributeMap(), context.timestamp().getMillis()));
+    }
+  }
+}
---
+++ v2/googlecloud-to-googlecloud/src/test/java/com/google/cloud/teleport/v2/templates/PubSubToAvroIT.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright (C) 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.cloud.teleport.v2.templates;
+
+import static com.google.cloud.teleport.it.common.matchers.TemplateAsserts.assertThatPipeline;
+import static com.google.cloud.teleport.it.common.matchers.TemplateAsserts.assertThatResult;
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.cloud.teleport.it.common.PipelineLauncher.LaunchConfig;
+import com.google.cloud.teleport.it.common.PipelineLauncher.LaunchInfo;
+import com.google.cloud.teleport.it.common.PipelineOperator.Result;
+import com.google.cloud.teleport.it.common.utils.ResourceManagerUtils;
+import com.google.cloud.teleport.it.gcp.TemplateTestBase;
+import com.google.cloud.teleport.it.gcp.artifacts.Artifact;
+import com.google.cloud.teleport.it.gcp.pubsub.PubsubResourceManager;
+import com.google.cloud.teleport.metadata.SkipDirectRunnerTest;
+import com.google.cloud.teleport.metadata.TemplateIntegrationTest;
+import com.google.cloud.teleport.v2.avro.AvroPubsubMessageRecord;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.protobuf.ByteString;
+import com.google.pubsub.v1.TopicName;
+import java.io.IOException;
+import java.util.List;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.SeekableByteArrayInput;
+import org.apache.avro.reflect.ReflectDatumReader;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Integration test for {@link PubsubToAvro} PubSub to Avro. */
+// SkipDirectRunnerTest: PubsubIO doesn't trigger panes on the DirectRunner.
+@Category({TemplateIntegrationTest.class, SkipDirectRunnerTest.class})
+@TemplateIntegrationTest(PubsubToAvro.class)
+@RunWith(JUnit4.class)
+public class PubSubToAvroIT extends TemplateTestBase {
+  private PubsubResourceManager pubsubResourceManager;
+
+  @Before
+  public void setUp() throws IOException {
+    pubsubResourceManager =
+        PubsubResourceManager.builder(testName, PROJECT)
+            .credentialsProvider(credentialsProvider)
+            .build();
+  }
+
+  @After
+  public void tearDown() {
+    ResourceManagerUtils.cleanResources(pubsubResourceManager);
+  }
+
+  @Test
+  public void testTopicToAvro() throws IOException {
+    // Arrange
+    String name = testName;
+    Pattern expectedFilePattern = Pattern.compile(".*topic-output-.*");
+    TopicName topic = pubsubResourceManager.createTopic("input-topic");
+
+    // Act
+    LaunchInfo info =
+        launchTemplate(
+            LaunchConfig.builder(testName, specPath)
+                .addParameter("inputTopic", topic.toString())
+                .addParameter("outputDirectory", getGcsPath(testName))
+                .addParameter("avroTempDirectory", getGcsPath("avro_tmp"))
+                .addParameter("outputFilenamePrefix", "topic-output-"));
+    assertThatPipeline(info).isRunning();
+
+    ImmutableSet<String> messages =
+        ImmutableSet.of("message1-" + name, "message2-" + name, "message3-" + name);
+
+    AtomicReference<List<Artifact>> artifacts = new AtomicReference<>();
+    Result result =
+        pipelineOperator()
+            .waitForConditionAndFinish(
+                createConfig(info),
+                () -> {
+
+                  // For tests that run against topics, sending repeatedly will make it work for
+                  // cases in which the on-demand subscription is created after sending messages.
+                  for (String message : messages) {
+                    pubsubResourceManager.publish(
+                        topic, ImmutableMap.of(), ByteString.copyFromUtf8(message));
+                  }
+
+                  artifacts.set(gcsClient.listArtifacts(testName, expectedFilePattern));
+                  return !artifacts.get().isEmpty();
+                });
+
+    // Assert
+    assertThatResult(result).meetsConditions();
+    assertThat(
+            artifacts.get().stream()
+                .flatMap(a -> deserialize(a.contents()))
+                .map(r -> new String(r.getMessage()))
+                .collect(Collectors.toUnmodifiableSet()))
+        .isEqualTo(messages);
+  }
+
+  private Stream<AvroPubsubMessageRecord> deserialize(byte[] bytes) {
+    DataFileReader<AvroPubsubMessageRecord> dataFileReader;
+    try {
+      dataFileReader =
+          new DataFileReader<>(
+              new SeekableByteArrayInput(bytes),
+              new ReflectDatumReader<>(AvroPubsubMessageRecord.class));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+    return StreamSupport.stream(
+        Spliterators.spliteratorUnknownSize(dataFileReader, Spliterator.IMMUTABLE), false);
+  }
+}
