0bdae5c9fb3228ec7ff5270339abeadec32e41b3
Shreya Khajanchi <khajanchi@google.com>
2023-06-19 20:29:51 +0530

    license added

--- v2/spanner-common/src/main/java/com/google/cloud/teleport/v2/spanner/migrations/utils/TransformationContextReader.java
+++ v2/spanner-common/src/main/java/com/google/cloud/teleport/v2/spanner/migrations/utils/TransformationContextReader.java
@@ -8,6 +8,21 @@ import java.io.InputStream;
 import java.nio.channels.Channels;
 import java.nio.charset.StandardCharsets;
 import org.apache.beam.sdk.io.FileSystems;
+/*
+ * Copyright (C) 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
