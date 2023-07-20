390c2b9f150b042c5202a20aaa02db85587d4af6
Bruno Volpato <bvolpato@google.com>
2023-07-20 14:46:04 -0400

    Increase integration test parallelism

--- cicd/cmd/run-it-tests/main.go
+++ cicd/cmd/run-it-tests/main.go
@@ -57,7 +57,7 @@ func main() {
 		mvnFlags.FailAtTheEnd(),
 		mvnFlags.RunIntegrationTests(),
 		mvnFlags.ThreadCount(4),
-		mvnFlags.IntegrationTestParallelism(4),
+		mvnFlags.IntegrationTestParallelism(3),
 		mvnFlags.StaticBigtableInstance("teleport"),
 		flags.Region(),
 		flags.Project(),
