e44cfc977d87908acbf37dcbec36cf55f673f9ff
Bruno Volpato <bvolpato@google.com>
2023-09-08 08:42:39 -0400

    Add randomChars to test id generation to prevent name collision

--- v1/src/test/java/com/google/cloud/teleport/templates/DatastoreToTextIT.java
+++ v1/src/test/java/com/google/cloud/teleport/templates/DatastoreToTextIT.java
@@ -32,7 +32,6 @@ import java.util.regex.Pattern;
 import org.apache.beam.it.common.PipelineLauncher.LaunchConfig;
 import org.apache.beam.it.common.PipelineLauncher.LaunchInfo;
 import org.apache.beam.it.common.PipelineOperator.Result;
-import org.apache.beam.it.common.utils.PipelineUtils;
 import org.apache.beam.it.common.utils.ResourceManagerUtils;
 import org.apache.beam.it.gcp.TemplateTestBase;
 import org.apache.beam.it.gcp.artifacts.Artifact;
@@ -53,8 +52,6 @@ public final class DatastoreToTextIT extends TemplateTestBase {
 
   @Before
   public void setup() {
-    testId = PipelineUtils.createJobName("test");
-
     datastoreResourceManager =
         DatastoreResourceManager.builder(PROJECT, testId, credentials).build();
 
