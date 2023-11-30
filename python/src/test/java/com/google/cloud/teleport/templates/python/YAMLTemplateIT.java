fb15c41aec89d67526eebd3bb5a45bf28b3be534
jkinard <jkinard@google.com>
2023-11-30 11:20:01 -0800

    PR #1052: Add YAML Template
    Please approve this CL. It will be submitted automatically, and its GitHub pull request will be marked as merged.
    
    Imported from GitHub PR #1052
    
    Copybara import of the project:
    
      - 0e1022401339583ff4867476c3bb3cd09551a75a YAML demo by Jeffrey Kinard <jeff@thekinards.com>
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/GoogleCloudPlatform/DataflowTemplates/pull/1052 from Polber:jkinard/add-yaml-template 0e1022401339583ff4867476c3bb3cd09551a75a
    PiperOrigin-RevId: 586734918

---
+++ python/src/test/java/com/google/cloud/teleport/templates/python/YAMLTemplateIT.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.cloud.teleport.templates.python;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.apache.beam.it.truthmatchers.PipelineAsserts.assertThatJsonRecords;
+import static org.apache.beam.it.truthmatchers.PipelineAsserts.assertThatPipeline;
+import static org.apache.beam.it.truthmatchers.PipelineAsserts.assertThatResult;
+
+import com.google.cloud.teleport.metadata.SkipDirectRunnerTest;
+import com.google.cloud.teleport.metadata.TemplateIntegrationTest;
+import com.google.common.io.Resources;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+import org.apache.beam.it.common.PipelineLauncher;
+import org.apache.beam.it.common.PipelineOperator;
+import org.apache.beam.it.gcp.TemplateTestBase;
+import org.apache.beam.it.gcp.artifacts.Artifact;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Integration test for {@link YAMLTemplate}. */
+// SkipDirectRunnerTest: Python templates are not supported through DirectRunner yet.
+@Category({TemplateIntegrationTest.class, SkipDirectRunnerTest.class})
+@TemplateIntegrationTest(YAMLTemplate.class)
+@RunWith(JUnit4.class)
+public final class YAMLTemplateIT extends TemplateTestBase {
+
+  @Test
+  public void testSimpleComposite() throws IOException {
+    // Arrange
+    String yamlMessage =
+        Files.readString(Paths.get(Resources.getResource("YamlTemplateIT.yaml").getPath()));
+    yamlMessage = yamlMessage.replaceAll("INPUT_PATH", getGcsBasePath() + "/input/test.csv");
+    yamlMessage = yamlMessage.replaceAll("OUTPUT_PATH", getGcsBasePath() + "/output");
+
+    gcsClient.createArtifact("input/test.csv", "num\n0\n1\n2\n4");
+    gcsClient.createArtifact("input/simple.yaml", yamlMessage);
+
+    // Act
+    runYamlTemplateTest("input/simple.yaml");
+
+    // Assert
+    List<Artifact> goodArtifacts = gcsClient.listArtifacts("output/good-", Pattern.compile(".*"));
+    assertThat(goodArtifacts).hasSize(1);
+
+    List<Artifact> badArtifacts = gcsClient.listArtifacts("output/bad-", Pattern.compile(".*"));
+    assertThat(badArtifacts).hasSize(2);
+
+    String goodRecords = new String(goodArtifacts.get(0).contents());
+    List<Map<String, Object>> expectedGood =
+        List.of(
+            Map.of("num", 2.0, "inverse", 0.5, "sum", 2.5),
+            Map.of("num", 4.0, "inverse", 0.25, "sum", 4.25));
+    assertThatJsonRecords(List.of(goodRecords.split("\n"))).hasRecords(expectedGood);
+
+    String indexError = "IndexError('string index out of range')";
+    String divError = "ZeroDivisionError('division by zero')";
+    String badRecords =
+        new String(badArtifacts.get(0).contents()) + new String(badArtifacts.get(1).contents());
+    assertThat(badRecords).contains(indexError);
+    assertThat(badRecords).contains(divError);
+  }
+
+  private void runYamlTemplateTest(String yamlGcsPath) throws IOException {
+    // Arrange
+    PipelineLauncher.LaunchConfig.Builder options =
+        PipelineLauncher.LaunchConfig.builder(testName, specPath)
+            .addParameter("yaml", getGcsPath(yamlGcsPath));
+
+    // Act
+    PipelineLauncher.LaunchInfo info = launchTemplate(options);
+    assertThatPipeline(info).isRunning();
+
+    PipelineOperator.Result result = pipelineOperator().waitUntilDone(createConfig(info));
+
+    // Assert
+    assertThatResult(result).isLaunchFinished();
+  }
+}
