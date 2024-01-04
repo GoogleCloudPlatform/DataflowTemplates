b1fe3cfb2eadeaadf29a37d16ac2584138c5f2df
Dippatel98 <dippatel@google.com>
2024-01-04 20:22:55 +0000

    Add Streaming Mode Template parameters

---
+++ v2/common/src/main/java/com/google/cloud/teleport/v2/utils/StreamingModeUtils.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.cloud.teleport.v2.utils;
+
+import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
+import org.apache.beam.sdk.options.ExperimentalOptions;
+
+public class StreamingModeUtils {
+  private StreamingModeUtils() {}
+
+  public static void validate(DataflowPipelineOptions options) {
+    if (ExperimentalOptions.hasExperiment(options, "streaming_mode_at_least_once")
+        || (options.getDataflowServiceOptions() != null
+            && options.getDataflowServiceOptions().contains("streaming_mode_at_least_once"))) {
+      options.setEnableStreamingEngine(true);
+    }
+  }
+
+  public static void validateBQOptions(DataflowPipelineOptions options) {
+    validate(options);
+    if (ExperimentalOptions.hasExperiment(options, "streaming_mode_at_least_once")
+        || (options.getDataflowServiceOptions() != null
+            && options.getDataflowServiceOptions().contains("streaming_mode_at_least_once"))) {
+      options.setUseStorageWriteApi(true);
+      options.setUseStorageWriteApiAtLeastOnce(true);
+    }
+  }
+}
