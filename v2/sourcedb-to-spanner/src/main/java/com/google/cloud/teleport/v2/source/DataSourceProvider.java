e0ae387de259a03cacad5418ddc06da9727d3dbb
Vardhan Thigle <vardhan.thigle@gmail.com>
2024-03-07 12:10:14 +0000

    Using java9+ idomatic double checked locking as per rewiew discussion.

--- v2/sourcedb-to-spanner/src/main/java/com/google/cloud/teleport/v2/source/DataSourceProvider.java
+++ v2/sourcedb-to-spanner/src/main/java/com/google/cloud/teleport/v2/source/DataSourceProvider.java
@@ -18,26 +18,110 @@ package com.google.cloud.teleport.v2.source;
 import static com.google.cloud.teleport.v2.utils.KMSUtils.maybeDecrypt;
 
 import com.google.cloud.teleport.v2.options.SourceDbToSpannerOptions;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
+import javax.sql.DataSource;
 import org.apache.beam.sdk.io.jdbc.JdbcIO;
 import org.apache.beam.sdk.options.ValueProvider.StaticValueProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DataSourceProvider
-    implements org.apache.beam.sdk.transforms.SerializableFunction<Void, javax.sql.DataSource> {
+    implements org.apache.beam.sdk.transforms.SerializableFunction<Void, DataSource> {
 
-  private static final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
-  private static final Lock writeLock = readWriteLock.writeLock();
-  private static final Lock readLock = readWriteLock.readLock();
   private static final Logger LOG = LoggerFactory.getLogger(DataSourceProvider.class);
-  private static volatile javax.sql.DataSource dataSource = null;
+  private static volatile DataSource dataSource = null;
+  // VarHandle provides a strongly typed reference to @dataSource
+  // and supports various atomic access modes like the acquire-release semantics.
+  private static final VarHandle DATA_SOURCE;
   private final JdbcIO.DataSourceConfiguration config;
 
+  // Bind DATA_SOURCE to dataSource
+  static {
+    try {
+      MethodHandles.Lookup lookup = MethodHandles.lookup();
+      DATA_SOURCE =
+          lookup
+              .in(DataSourceProvider.class)
+              .findStaticVarHandle(DataSourceProvider.class, "dataSource", DataSource.class);
+    } catch (ReflectiveOperationException e) {
+      // Logger might not be initialized in static scope!
+      System.err.println("Error while binding VarHandle: " + e.toString());
+      throw new ExceptionInInitializerError(e);
+    }
+  }
+
+  /**
+   * Constructs an instance of DataSourceProvider.
+   *
+   * @param options Pipeline options.
+   */
   public DataSourceProvider(SourceDbToSpannerOptions options) {
     config = getDataSourceConfiguration(options);
+  }
+
+  /**
+   * Returns a Singleton {@link DataSource} after initializing it if necessary.
+   *
+   * @see <a href = "https://en.wikipedia.org/wiki/Double-checked_locking#Usage_in_Java"> Idomatic
+   *     implementation of Double checked locking pattern.</a>
+   */
+  @Override
+  public DataSource apply(Void input) {
+    DataSource localRef = getDataSourceAcquire();
+    if (localRef == null) {
+      synchronized (this) {
+        localRef = getDataSourceAcquire();
+        if (localRef == null) {
+          localRef = JdbcIO.PoolableDataSourceProvider.of(config).apply(null);
+          setDataSourceRelease(localRef);
+          LOG.debug("initialize DataSource dataSource {}", localRef);
+        }
+      }
+    }
+    return localRef;
+  }
+
+  /**
+   * Access {@link DataSourceProvider#dataSource} with <code>memory_order_acquire</code> memory
+   * ordering semantics.
+   *
+   * <p>The load operation of {@link DataSourceProvider#dataSource} to the returned value
+   * guarantees:
+   *
+   * <ol>
+   *   <li>No reads or writes in the current thread can be reordered before this load.
+   *   <li>All writes in other threads that release the same variable are visible in the current
+   *       thread.
+   * </ol>
+   *
+   * @see VarHandle
+   * @see <a href="https://en.cppreference.com/w/cpp/atomic/memory_order#Release-Acquire_ordering">
+   *     <code>Release-Acquire_ordering</code></a>
+   */
+  private DataSource getDataSourceAcquire() {
+    return (DataSource) DATA_SOURCE.getAcquire();
+  }
+
+  /**
+   * Set {@link DataSourceProvider#dataSource} with <code>memory_order_release</code> memory
+   * ordering semantics.
+   *
+   * <p>The store operation to {@link DataSourceProvider#dataSource} with this memory ordering
+   * guarantees:
+   *
+   * <ol>
+   *   <li>No reads or writes in the current thread can be reordered after this store.
+   *   <li>All writes in the current thread are visible in other threads that acquire the same
+   *       variable.
+   * </ol>
+   *
+   * @see VarHandle
+   * @see <a href="https://en.cppreference.com/w/cpp/atomic/memory_order#Release-Acquire_ordering">
+   *     <code>Release-Acquire_ordering</code></a>
+   */
+  private void setDataSourceRelease(DataSource value) {
+    DATA_SOURCE.setRelease(value);
   }
 
   private static JdbcIO.DataSourceConfiguration getDataSourceConfiguration(
@@ -57,47 +141,5 @@ public class DataSourceProvider
       config = config.withDriverJars(options.getJdbcDriverJars());
     }
     return config;
-  }
-
-  private javax.sql.DataSource initializeDataSourceIfRequired() {
-    // Initialize the connection pool if required.
-    javax.sql.DataSource ret = null;
-    ret = getDataSource();
-    if (ret == null) {
-      ret = initializeDataSource();
-    }
-    return ret;
-  }
-
-  private javax.sql.DataSource initializeDataSource() {
-    javax.sql.DataSource ret;
-    writeLock.lock();
-    try {
-      // Check if it was already initialized till the write lock was acquired.
-      if (dataSource == null) {
-        dataSource = JdbcIO.PoolableDataSourceProvider.of(config).apply(null);
-        LOG.debug("initializeDataSource dataSource" + dataSource.toString());
-      }
-      ret = dataSource;
-    } finally {
-      writeLock.unlock();
-    }
-    return ret;
-  }
-
-  private javax.sql.DataSource getDataSource() {
-    javax.sql.DataSource ret = null;
-    readLock.lock();
-    try {
-      ret = dataSource;
-    } finally {
-      readLock.unlock();
-    }
-    return ret;
-  }
-
-  @Override
-  public javax.sql.DataSource apply(Void input) {
-    return initializeDataSourceIfRequired();
   }
 }
