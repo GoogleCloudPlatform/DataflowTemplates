46c1a73275ae6a3f0897c9c6085468527a5093a6
Matthew Eagar <meagar@gmail.com>
2024-03-20 13:53:21 -0400

    Backport fixes from integration testing

--- v2/googlecloud-to-googlecloud/src/main/java/com/google/cloud/teleport/v2/templates/bigtablechangestreamstovectorsearch/ChangeStreamMutationToDatapointOperationFn.java
+++ v2/googlecloud-to-googlecloud/src/main/java/com/google/cloud/teleport/v2/templates/bigtablechangestreamstovectorsearch/ChangeStreamMutationToDatapointOperationFn.java
@@ -16,17 +16,14 @@
 package com.google.cloud.teleport.v2.templates.bigtablechangestreamstovectorsearch;
 
 import com.google.cloud.aiplatform.v1.IndexDatapoint;
-import com.google.cloud.bigtable.data.v2.models.ChangeStreamMutation;
-import com.google.cloud.bigtable.data.v2.models.DeleteCells;
-import com.google.cloud.bigtable.data.v2.models.DeleteFamily;
-import com.google.cloud.bigtable.data.v2.models.Entry;
-import com.google.cloud.bigtable.data.v2.models.SetCell;
-import java.util.Map;
+import com.google.cloud.bigtable.data.v2.models.*;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.values.TupleTag;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Map;
 
 /**
  * The {@link ChangeStreamMutationToDatapointOperationFn} class is a {@link DoFn} that takes in a
@@ -40,6 +37,20 @@ public class ChangeStreamMutationToDatapointOperationFn
       new TupleTag<IndexDatapoint>() {};
   public static final TupleTag<String> RemoveDatapointTag = new TupleTag<String>() {};
 
+  private static final Logger LOG =
+          LoggerFactory.getLogger(ChangeStreamMutationToDatapointOperationFn.class);
+
+  private String embeddingsColumn; // "family_name:qualifier"
+  private String embeddingsColumnFamilyName; // "family_name" extracted from embeddingsColumn
+  private int embeddingsByteSize; // 4 or 8
+  private String crowdingTagColumn;
+  private Map<String, String> allowRestrictsMappings;
+  private Map<String, String> denyRestrictsMappings;
+  private Map<String, String> intNumericRestrictsMappings;
+  private Map<String, String> floatNumericRestrictsMappings;
+  private Map<String, String> doubleNumericRestrictsMappings;
+
+
   public ChangeStreamMutationToDatapointOperationFn(
       String embeddingsColumn,
       int embeddingsByteSize,
@@ -49,8 +60,23 @@ public class ChangeStreamMutationToDatapointOperationFn
       Map<String, String> intNumericRestrictsMappings,
       Map<String, String> floatNumericRestrictsMappings,
       Map<String, String> doubleNumericRestrictsMappings) {
-    this.embeddingsColumn = embeddingsColumn;
+
+    {
+      String[] parts = embeddingsColumn.split(":", 2);
+      if (parts.length != 2) {
+        throw new IllegalArgumentException("Invalid embeddingsColumn - should be in the form \"family:qualifier\"");
+      }
+
+      this.embeddingsColumn = embeddingsColumn;
+      this.embeddingsColumnFamilyName = parts[0];
+    }
+
     this.embeddingsByteSize = embeddingsByteSize;
+
+    if (this.embeddingsByteSize != 4 && this.embeddingsByteSize != 8) {
+      throw new IllegalArgumentException("Embeddings byte size must be 4 or 8");
+    }
+
     this.crowdingTagColumn = crowdingTagColumn;
     this.allowRestrictsMappings = allowRestrictsMappings;
     this.denyRestrictsMappings = denyRestrictsMappings;
@@ -58,18 +84,6 @@ public class ChangeStreamMutationToDatapointOperationFn
     this.floatNumericRestrictsMappings = floatNumericRestrictsMappings;
     this.doubleNumericRestrictsMappings = doubleNumericRestrictsMappings;
   }
-
-  private static final Logger LOG =
-      LoggerFactory.getLogger(ChangeStreamMutationToDatapointOperationFn.class);
-
-  private String embeddingsColumn;
-  private int embeddingsByteSize;
-  private String crowdingTagColumn;
-  private Map<String, String> allowRestrictsMappings;
-  private Map<String, String> denyRestrictsMappings;
-  private Map<String, String> intNumericRestrictsMappings;
-  private Map<String, String> floatNumericRestrictsMappings;
-  private Map<String, String> doubleNumericRestrictsMappings;
 
   @ProcessElement
   public void processElement(@Element ChangeStreamMutation mutation, MultiOutputReceiver output) {
@@ -90,6 +104,7 @@ public class ChangeStreamMutationToDatapointOperationFn
 
   private void processInsert(ChangeStreamMutation mutation, MultiOutputReceiver output) {
     IndexDatapoint.Builder datapointBuilder = IndexDatapoint.newBuilder();
+    datapointBuilder.setDatapointId(mutation.getRowKey().toStringUtf8());
 
     for (Entry entry : mutation.getEntries()) {
       LOG.debug("Processing {}", entry);
@@ -159,30 +174,25 @@ public class ChangeStreamMutationToDatapointOperationFn
   private void processDelete(ChangeStreamMutation mutation, MultiOutputReceiver output) {
     LOG.debug("Handling mutation as a deletion");
 
-    String removeDatapoint = null;
-
-    for (Entry entry : mutation.getEntries()) {
-      LOG.debug("Processing {}", entry);
-
+    Boolean isDelete = mutation.getEntries().stream().anyMatch((entry) -> {
+      // Each deletion may come in as one or more DeleteCells mutations, or one more or DeleteFamily mutations
+      // As soon as we find a DeleteCells that covers the fully qualified embeddings column, _or_ a DeleteFamily that
+      // covers the embeddings column's family, we treat the mutation as a deletion of the Datapoint.
       if (entry instanceof DeleteCells) {
-        if (removeDatapoint != null) {
-          LOG.error("A single change stream mutation contained DeleteCells AND DeleteFamily");
-        }
-        // TODO(meagar): Delete cells - verify embeddings column is removed
-        LOG.info("Processing a delete cell");
-        removeDatapoint = mutation.getRowKey().toStringUtf8();
+        DeleteCells m = (DeleteCells)entry;
+        return (m.getFamilyName() + ":" + m.getQualifier() == this.embeddingsColumn);
       } else if (entry instanceof DeleteFamily) {
-        if (removeDatapoint != null) {
-          LOG.error("A single change stream mutation contained DeleteCells AND DeleteFamily");
-        }
-
-        removeDatapoint = mutation.getRowKey().toStringUtf8();
+        DeleteFamily m = (DeleteFamily)entry;
+        return (m.getFamilyName() == this.embeddingsColumnFamilyName);
       }
-    }
 
-    if (removeDatapoint != null) {
-      LOG.info("Emitting a remove datapoint: {}", removeDatapoint);
-      output.get(RemoveDatapointTag).output(removeDatapoint);
+      return false;
+    });
+
+    if (isDelete) {
+      String rowkey = mutation.getRowKey().toStringUtf8();
+      LOG.info("Emitting a remove datapoint: {}", rowkey);
+      output.get(RemoveDatapointTag).output(rowkey);
     }
   }
 }
