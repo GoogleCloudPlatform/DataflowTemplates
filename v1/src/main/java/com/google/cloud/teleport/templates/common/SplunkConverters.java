a0744c018ffd7b350418778c0998961064bbc633
Supriya-Koppa <162451155+Supriya-Koppa@users.noreply.github.com>
2024-05-08 11:09:13 +0530

    Apply suggestions from code review
    Co-authored-by: Rebecca Szper <98840847+rszper@users.noreply.github.com>

--- v1/src/main/java/com/google/cloud/teleport/templates/common/SplunkConverters.java
+++ v1/src/main/java/com/google/cloud/teleport/templates/common/SplunkConverters.java
@@ -210,7 +210,7 @@ public class SplunkConverters {
         order = 11,
         optional = true,
         description = "Enable logs for batches written to Splunk.",
-        helpText = "Specifies whether to enable logs for batches written to Splunk. Default: true.")
+        helpText = "Whether to enable logs for batches written to Splunk. Default: true.")
     @Default.Boolean(true)
     ValueProvider<Boolean> getEnableBatchLogs();
 
@@ -222,7 +222,7 @@ public class SplunkConverters {
         description =
             "Enable compression (gzip content encoding) in HTTP requests sent to Splunk HEC.",
         helpText =
-            "Specifies whether to compress HTTP requests sent to Splunk HEC (gzip content encoded). Default: true.")
+            "Whether to compress HTTP requests sent to Splunk HEC (gzip content encoded). Default: true.")
     @Default.Boolean(true)
     ValueProvider<Boolean> getEnableGzipHttpCompression();
 
