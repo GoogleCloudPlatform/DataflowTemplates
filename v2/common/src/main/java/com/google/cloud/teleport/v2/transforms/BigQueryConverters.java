3daa6bbc5e5345bf111cebb58d47471a46c99352
tamannakakkar93 <51637901+tamannakakkar93@users.noreply.github.com>
2024-05-16 20:03:34 +0530

    Update v2/common/src/main/java/com/google/cloud/teleport/v2/transforms/BigQueryConverters.java
    Co-authored-by: Rebecca Szper <98840847+rszper@users.noreply.github.com>

--- v2/common/src/main/java/com/google/cloud/teleport/v2/transforms/BigQueryConverters.java
+++ v2/common/src/main/java/com/google/cloud/teleport/v2/transforms/BigQueryConverters.java
@@ -183,7 +183,7 @@ public class BigQueryConverters {
         description = "The dead-letter table name to output failed messages to BigQuery",
         helpText =
             "The BigQuery table for messages that failed to reach the output"
-                + " table, in the format <PROJECT_ID>:<DATASET_NAME>.<DEADLETTER_TABLE>.`"
+                + " table, in the format <PROJECT_ID>:<DATASET_NAME>.<DEADLETTER_TABLE>."
                 + " If a table doesn't exist, is is created during pipeline execution. If"
                 + " not specified, `<outputTableSpec>_error_records` is used.",
         example = "your-project-id:your-dataset.your-table-name")
