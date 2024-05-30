a198f4976a5a15eebfeffb4dd22b2883cac7ab07
Anand Inguva <anandinguva98@gmail.com>
2024-05-30 15:11:35 -0400

    Add exception

--- v2/kafka-to-gcs/src/main/java/com/google/cloud/teleport/v2/transforms/AvroWriteTransform.java
+++ v2/kafka-to-gcs/src/main/java/com/google/cloud/teleport/v2/transforms/AvroWriteTransform.java
@@ -85,6 +85,7 @@ public abstract class AvroWriteTransform
         KafkaTemplateParamters.MessageFormatConstants.AVRO_CONFLUENT_WIRE_FORMAT)) {
       String schemaRegistryURL = schemaRegistryURL();
       String schemaPath = schemaPath();
+      // Check for NPE.
       assert schemaRegistryURL != null;
       assert schemaPath != null;
       if (schemaRegistryURL.isBlank() && schemaPath.isBlank()) {
