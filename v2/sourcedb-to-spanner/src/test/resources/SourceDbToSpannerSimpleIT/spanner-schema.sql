89a4be7fe5502909b1c9b2c5436ddf7061a9f45c
Deep1998 <deepchowdhury1998@gmail.com>
2024-07-30 18:06:13 +0530

    Add string mapper case for IT (#1757)

--- v2/sourcedb-to-spanner/src/test/resources/SourceDbToSpannerSimpleIT/spanner-schema.sql
+++ v2/sourcedb-to-spanner/src/test/resources/SourceDbToSpannerSimpleIT/spanner-schema.sql
@@ -2,3 +2,8 @@ CREATE TABLE IF NOT EXISTS SimpleTable (
   id INT64 NOT NULL,
   name STRING(200),
 ) PRIMARY KEY(id);
+
+CREATE TABLE IF NOT EXISTS StringTable (
+  id INT64 NOT NULL,
+  name STRING(200),
+) PRIMARY KEY(name);
