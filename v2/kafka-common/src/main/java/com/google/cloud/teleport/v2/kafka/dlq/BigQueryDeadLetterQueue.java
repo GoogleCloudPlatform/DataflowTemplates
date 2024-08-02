6c1461aef49cbdf8697b0d264c1ffe98b3b86da1
fozzie15 <ozfernandez15@gmail.com>
2024-08-02 11:09:33 -0400

    Add DLQ changes for kafka avro to expose the topic, offset, and partition for better visibility into errors.

--- v2/kafka-common/src/main/java/com/google/cloud/teleport/v2/kafka/dlq/BigQueryDeadLetterQueue.java
+++ v2/kafka-common/src/main/java/com/google/cloud/teleport/v2/kafka/dlq/BigQueryDeadLetterQueue.java
@@ -28,6 +28,9 @@ import org.apache.beam.sdk.values.POutput;
 import org.joda.time.DateTimeZone;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @AutoValue
 public abstract class BigQueryDeadLetterQueue extends PTransform<PCollection<BadRecord>, POutput> {
@@ -40,6 +43,71 @@ public abstract class BigQueryDeadLetterQueue extends PTransform<PCollection<Bad
    */
   private static final DateTimeFormatter TIMESTAMP_FORMATTER =
       DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.SSSSSS");
+
+  public static final Logger LOG = LoggerFactory.getLogger(BigQueryDeadLetterQueue.class);
+
+  private static final String KAFKA_DEADLETTER_SCHEMA =
+      "{\n"
+          + "  \"fields\": [\n"
+          + "    {\n"
+          + "      \"name\": \"timestamp\",\n"
+          + "      \"type\": \"TIMESTAMP\",\n"
+          + "      \"mode\": \"REQUIRED\"\n"
+          + "    },\n"
+          + "    {\n"
+          + "      \"name\": \"payloadString\",\n"
+          + "      \"type\": \"STRING\",\n"
+          + "      \"mode\": \"REQUIRED\"\n"
+          + "    },\n"
+          + "    {\n"
+          + "      \"name\": \"payloadBytes\",\n"
+          + "      \"type\": \"BYTES\",\n"
+          + "      \"mode\": \"REQUIRED\"\n"
+          + "    },\n"
+          + "    {\n"
+          + "      \"name\": \"attributes\",\n"
+          + "      \"type\": \"RECORD\",\n"
+          + "      \"mode\": \"REPEATED\",\n"
+          + "      \"fields\": [\n"
+          + "        {\n"
+          + "          \"name\": \"key\",\n"
+          + "          \"type\": \"STRING\",\n"
+          + "          \"mode\": \"NULLABLE\"\n"
+          + "        },\n"
+          + "        {\n"
+          + "          \"name\": \"value\",\n"
+          + "          \"type\": \"STRING\",\n"
+          + "          \"mode\": \"NULLABLE\"\n"
+          + "        }\n"
+          + "      ]\n"
+          + "    },\n"
+          + "    {\n"
+          + "      \"name\": \"errorMessage\",\n"
+          + "      \"type\": \"STRING\",\n"
+          + "      \"mode\": \"NULLABLE\"\n"
+          + "    },\n"
+          + "    {\n"
+          + "      \"name\": \"stacktrace\",\n"
+          + "      \"type\": \"STRING\",\n"
+          + "      \"mode\": \"NULLABLE\"\n"
+          + "    },\n"
+          + "    {\n"
+          + "      \"name\": \"sourceOffset\",\n"
+          + "      \"type\": \"STRING\",\n"
+          + "      \"mode\": \"NULLABLE\"\n"
+          + "    },\n"
+          + "    {\n"
+          + "      \"name\": \"topic\",\n"
+          + "      \"type\": \"STRING\",\n"
+          + "      \"mode\": \"NULLABLE\"\n"
+          + "    },\n"
+          + "    {\n"
+          + "      \"name\": \"partition\",\n"
+          + "      \"type\": \"STRING\",\n"
+          + "      \"mode\": \"NULLABLE\"\n"
+          + "    }\n"
+          + "  ]\n"
+          + "}";
 
   @AutoValue.Builder
   public abstract static class Builder {
@@ -76,6 +144,7 @@ public abstract class BigQueryDeadLetterQueue extends PTransform<PCollection<Bad
     public void processElement(ProcessContext context) {
       BadRecord element = context.element();
       final String message = element.getRecord().getHumanReadableJsonRecord();
+
       // Format the timestamp for insertion
       String timestamp =
           TIMESTAMP_FORMATTER.print(context.timestamp().toDateTime(DateTimeZone.UTC));
@@ -87,12 +156,33 @@ public abstract class BigQueryDeadLetterQueue extends PTransform<PCollection<Bad
               .set("errorMessage", element.getFailure().getDescription())
               .set("stacktrace", element.getFailure().getExceptionStacktrace());
 
-      // Only set the payload if it;s populated on the message.
+      // Only set the payload if it's populated on the message.
       if (message != null) {
         failedTableRow.set("payloadString", message);
+        JSONObject messageJson = new JSONObject(message);
+
+        if (messageJson.has("offset")) {
+          failedTableRow.set("sourceOffset", messageJson.get("offset").toString());
+        } else {
+          LOG.info(
+              "No offset value in the record. This is likely due to an error writing to BigQuery.");
+        }
+        if (messageJson.has("partition")) {
+          failedTableRow.set("partition", messageJson.get("partition").toString());
+        } else {
+          LOG.info(
+              "No partition value in the record. This is likely due to an error writing to BigQuery.");
+        }
+        if (messageJson.has("topic")) {
+          failedTableRow.set("topic", messageJson.get("topic").toString());
+        } else {
+          LOG.info(
+              "No topic value in the record. This is likely due to an error writing to BigQuery.");
+        }
       }
 
       byte[] encodedMessage = element.getRecord().getEncodedRecord();
+
       if (encodedMessage != null) {
         failedTableRow.set("payloadBytes", encodedMessage);
       }
