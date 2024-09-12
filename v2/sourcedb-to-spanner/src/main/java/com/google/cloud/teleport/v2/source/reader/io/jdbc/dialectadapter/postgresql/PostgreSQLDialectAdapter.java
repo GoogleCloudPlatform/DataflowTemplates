884177647bd6ecc6595c8c63960981236c2f085f
Thiago Nunes <thiagotnunes@google.com>
2024-09-12 17:21:33 +1000

    feat: update pg index discovery query for 9.4+ (#1863)
    * feat: update pg index discovery query for 9.4+
    
    Update the SourceDbToSpanner index discovery query for PostgreSQL as a
    source to work in PG versions older than 11 (9.4+).
    
    Before version 11, PG did not support the INCLUDE clause in indexes, so
    the pg_catalog.pg_index table did not contain the column 'indnkeyatts'
    (number of key columns in an index). It only contained the column
    'indnatts' (number of columns in an index).
    
    In this change, we first try to read from the 'indnkeyatts' and fallback
    to 'indnatts'.
    
    * docs: update db-fiddle

--- v2/sourcedb-to-spanner/src/main/java/com/google/cloud/teleport/v2/source/reader/io/jdbc/dialectadapter/postgresql/PostgreSQLDialectAdapter.java
+++ v2/sourcedb-to-spanner/src/main/java/com/google/cloud/teleport/v2/source/reader/io/jdbc/dialectadapter/postgresql/PostgreSQLDialectAdapter.java
@@ -259,7 +259,7 @@ public class PostgreSQLDialectAdapter implements DialectAdapter {
 
   /**
    * Discover the indexes of tables to migrate. You can try this in <a
-   * href="https://www.db-fiddle.com/f/bWFaWkW6dMw1tcNDaFnChc/0">db-fiddle</a>.
+   * href="https://www.db-fiddle.com/f/kTanXYXoM2VgCjSf6NZHjD/1">db-fiddle</a>.
    *
    * @param dataSource Provider for JDBC connection.
    * @param sourceSchemaReference Source database name and (optionally namespace)
@@ -280,14 +280,19 @@ public class PostgreSQLDialectAdapter implements DialectAdapter {
         sourceSchemaReference,
         tables);
 
-    // TODO(thiagotnunes): Fix the query to work with PG versions < 11
     final String query =
         String.format(
             "SELECT a.attname AS column_name,"
                 + "  ixs.indexname AS index_name,"
                 + "  ix.indisunique AS is_unique,"
                 + "  ix.indisprimary AS is_primary,"
-                + "  ix.indnkeyatts AS cardinality,"
+                // PG < 11 does not have the indnkeyatts column (total key columns in index), since
+                // the INCLUDE SQL feature was not available then. We use the indnatts column
+                // instead (total columns in index).
+                + "  COALESCE("
+                + "    (row_to_json(ix)->>'indnkeyatts')::int8,"
+                + "    (row_to_json(ix)->>'indnatts')::int8"
+                + "  ) AS cardinality,"
                 + "  a.attnum AS ordinal_position,"
                 + "  t.typname AS type_name,"
                 + "  information_schema._pg_char_max_length(a.atttypid, a.atttypmod) AS type_length,"
