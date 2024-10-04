cc17ed154d8c3c385ab3f5a3653a6e4c02539bfa
Manit Gupta <manitgupta@google.com>
2024-10-04 18:16:22 +0530

    IT for validating string and file overrides (#1911)
    * Add IT for string overrides
    
    * Add IT for file overrides
    
    * Resolve comments
    
    * Declare a <String, String> map explicitly
    
    * Rebase changes
    
    * spotless

---
+++ v2/datastream-to-spanner/src/test/resources/DataStreamToSpannerStringOverridesIT/spanner-schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE human1 (
+                        name1 STRING(500),
+                        last_name1 STRING(500),
+                        first_name2 STRING(500),
+                        last_name2 STRING(500),
+                        first_name3 STRING(500),
+                        last_name3 STRING(500),
+                        ID INT64 NOT NULL,
+) PRIMARY KEY(ID);
