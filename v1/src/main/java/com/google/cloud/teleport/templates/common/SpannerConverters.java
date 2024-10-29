de7fd4aa3a884aa85625316452bd682fbb6a2e77
Rebecca Szper <98840847+rszper@users.noreply.github.com>
2024-10-29 12:50:09 -0700

    Update SpannerConverters.java

--- v1/src/main/java/com/google/cloud/teleport/templates/common/SpannerConverters.java
+++ v1/src/main/java/com/google/cloud/teleport/templates/common/SpannerConverters.java
@@ -135,8 +135,8 @@ public class SpannerConverters {
         optional = true,
         description = "Cloud Spanner Endpoint to call",
         helpText = "The Cloud Spanner endpoint to call in the template. Only used for testing.",
-        example = "https://batch-spanner.googleapis.com")
-    @Default.String("https://batch-spanner.googleapis.com")
+        example = "`https://batch-spanner.googleapis.com`")
+    @Default.String("`https://batch-spanner.googleapis.com`")
     ValueProvider<String> getSpannerHost();
 
     @SuppressWarnings("unused")
