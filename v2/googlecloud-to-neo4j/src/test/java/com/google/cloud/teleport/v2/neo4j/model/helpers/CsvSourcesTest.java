7a797868c65e7b4e2479c1de3e0bb38b4db7484b
emrehizal <emre.hizal@neo4j.com>
2024-11-08 15:48:55 +0100

    test: improvement on CsvSources test

--- v2/googlecloud-to-neo4j/src/test/java/com/google/cloud/teleport/v2/neo4j/model/helpers/CsvSourcesTest.java
+++ v2/googlecloud-to-neo4j/src/test/java/com/google/cloud/teleport/v2/neo4j/model/helpers/CsvSourcesTest.java
@@ -2,24 +2,55 @@ package com.google.cloud.teleport.v2.neo4j.model.helpers;
 
 import com.google.cloud.teleport.v2.neo4j.model.sources.TextFormat;
 import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import org.junit.Test;
-
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import static com.google.common.truth.Truth.assertThat;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+@RunWith(Parameterized.class)
 public class CsvSourcesTest {
 
-    @Test
-    public void convertsTextFormatToCsvFormat() {
-        assertThat(CsvSources.toCsvFormat(TextFormat.EXCEL)).isEqualTo(CSVFormat.EXCEL.withNullString(""));
-        assertThat(CsvSources.toCsvFormat(TextFormat.INFORMIX)).isEqualTo(CSVFormat.INFORMIX_UNLOAD_CSV.withNullString(""));
-        assertThat(CsvSources.toCsvFormat(TextFormat.MONGO)).isEqualTo(CSVFormat.MONGODB_CSV.withNullString(""));
-        assertThat(CsvSources.toCsvFormat(TextFormat.MONGO_TSV)).isEqualTo(CSVFormat.MONGODB_TSV.withNullString(""));
-        assertThat(CsvSources.toCsvFormat(TextFormat.MYSQL)).isEqualTo(CSVFormat.MYSQL.withNullString(""));
-        assertThat(CsvSources.toCsvFormat(TextFormat.ORACLE)).isEqualTo(CSVFormat.ORACLE.withNullString(""));
-        assertThat(CsvSources.toCsvFormat(TextFormat.POSTGRES)).isEqualTo(CSVFormat.POSTGRESQL_TEXT.withNullString(""));
-        assertThat(CsvSources.toCsvFormat(TextFormat.POSTGRESQL_CSV)).isEqualTo(CSVFormat.POSTGRESQL_CSV.withNullString(""));
-        assertThat(CsvSources.toCsvFormat(TextFormat.RFC4180)).isEqualTo(CSVFormat.RFC4180.withNullString(""));
-        assertThat(CsvSources.toCsvFormat(TextFormat.DEFAULT)).isEqualTo(CSVFormat.DEFAULT.withNullString(""));
+    @Parameterized.Parameter(0)
+    public TextFormat textFormat;
+
+    @Parameterized.Parameter(1)
+    public char delimiter;
+
+    @Parameterized.Parameters(name = "{0}")
+    public static List<Object[]> testParameters() {
+        return Arrays.asList(new Object[][]{
+                {TextFormat.EXCEL, ','},
+                {TextFormat.INFORMIX, ','},
+                {TextFormat.MONGO, ','},
+                {TextFormat.MONGO_TSV, '\t'},
+                {TextFormat.MYSQL, '\t'},
+                {TextFormat.ORACLE, ','},
+                {TextFormat.POSTGRES, '\t'},
+                {TextFormat.POSTGRESQL_CSV, ','},
+                {TextFormat.RFC4180, ','},
+                {TextFormat.DEFAULT, ','}
+        });
     }
 
+    @Test
+    public void shouldParseEmptyColumnsAsNullValues() throws IOException {
+        String line = "1" + delimiter;
+        CSVFormat csvFormat = CsvSources.toCsvFormat(textFormat);
+
+        try(CSVParser csvParser = CSVParser.parse(line, csvFormat)) {
+            List<CSVRecord> csvRecords = csvParser.getRecords();
+            assertThat(csvRecords).hasSize(1);
+
+            CSVRecord csvRecord = csvRecords.get(0);
+            assertThat(csvRecord).hasSize(2);
+            assertThat(csvRecord.get(0)).isEqualTo("1");
+            assertThat(csvRecord.get(1)).isEqualTo(null);
+        }
+    }
 }
