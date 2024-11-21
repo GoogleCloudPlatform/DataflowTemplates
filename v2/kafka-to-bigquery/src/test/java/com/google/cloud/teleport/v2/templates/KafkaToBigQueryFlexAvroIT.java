aeae5d4fd27ef16494ba54a6455f99390cd7fea0
an2x <52892974+an2x@users.noreply.github.com>
2024-11-21 15:01:46 -0500

    Apply spotless.

--- v2/kafka-to-bigquery/src/test/java/com/google/cloud/teleport/v2/templates/KafkaToBigQueryFlexAvroIT.java
+++ v2/kafka-to-bigquery/src/test/java/com/google/cloud/teleport/v2/templates/KafkaToBigQueryFlexAvroIT.java
@@ -487,7 +487,8 @@ public final class KafkaToBigQueryFlexAvroIT extends TemplateTestBase {
       GenericRecord pubsub = createRecord(Integer.valueOf(i + "2"), "Pub/Sub", "MEDIUM");
       publish(kafkaProducer, topicName, i + "2", pubsub);
 
-      GenericRecord invalid = createRecord(Integer.valueOf(i + "3"), "InvalidNameTooLong", "UNDEFINED");
+      GenericRecord invalid =
+          createRecord(Integer.valueOf(i + "3"), "InvalidNameTooLong", "UNDEFINED");
       publish(kafkaProducer, topicName, i + "3", invalid);
 
       try {
@@ -515,7 +516,8 @@ public final class KafkaToBigQueryFlexAvroIT extends TemplateTestBase {
       GenericRecord pubsub = createRecord(Integer.valueOf(i + "2"), "Pub/Sub", "MEDIUM");
       publish(kafkaProducer, topicName, i + "2", pubsub);
 
-      GenericRecord invalid = createRecord(Integer.valueOf(i + "3"), "InvalidNameTooLong", "UNDEFINED");
+      GenericRecord invalid =
+          createRecord(Integer.valueOf(i + "3"), "InvalidNameTooLong", "UNDEFINED");
       publish(kafkaProducer, topicName, i + "3", invalid);
 
       GenericRecord otherDataflow =
@@ -549,7 +551,8 @@ public final class KafkaToBigQueryFlexAvroIT extends TemplateTestBase {
       GenericRecord pubsub = createRecord(Integer.valueOf(i + "2"), "Pub/Sub", "MEDIUM");
       publishBinary(kafkaProducer, topicName, i + "2", pubsub);
 
-      GenericRecord invalid = createRecord(Integer.valueOf(i + "3"), "InvalidNameTooLong", "UNDEFINED");
+      GenericRecord invalid =
+          createRecord(Integer.valueOf(i + "3"), "InvalidNameTooLong", "UNDEFINED");
       publishBinary(kafkaProducer, topicName, i + "3", invalid);
 
       try {
