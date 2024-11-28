129e9280e9906d66da09a66bd8c9e832d0c8cad6
emrehizal <emre.hizal@neo4j.com>
2024-11-28 14:55:13 +0100

    build: add access token for project

--- v2/googlecloud-to-neo4j/src/test/java/com/google/cloud/teleport/v2/neo4j/templates/MinimalServiceAccountIT.java
+++ v2/googlecloud-to-neo4j/src/test/java/com/google/cloud/teleport/v2/neo4j/templates/MinimalServiceAccountIT.java
@@ -15,16 +15,93 @@
  */
 package com.google.cloud.teleport.v2.neo4j.templates;
 
+import static com.google.cloud.teleport.v2.neo4j.templates.Resources.contentOf;
 import static com.google.common.truth.Truth.assertThat;
+import static org.apache.beam.it.truthmatchers.PipelineAsserts.assertThatPipeline;
+import static org.apache.beam.it.truthmatchers.PipelineAsserts.assertThatResult;
 
+import org.apache.beam.it.common.PipelineOperator;
+import org.apache.beam.it.common.TestProperties;
+import org.apache.beam.it.common.utils.ResourceManagerUtils;
+import org.apache.beam.it.gcp.TemplateTestBase;
+import org.apache.beam.it.neo4j.Neo4jResourceManager;
+import org.apache.beam.it.common.PipelineLauncher.LaunchConfig;
+import org.apache.beam.it.common.PipelineLauncher.LaunchInfo;
+import org.apache.beam.it.neo4j.conditions.Neo4jQueryCheck;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
-@Category(Neo4jCustomTestCategory.class)
-public class MinimalServiceAccountIT {
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
-  @Test
-  public void test() {
-    assertThat(2).isEqualTo(1 + 2);
-  }
+@Category(Neo4jCustomTestCategory.class)
+public class MinimalServiceAccountIT extends TemplateTestBase {
+
+    private final String MINIMAL_SERVICE_ACCOUNT = "dataflow-exp2@team-connectors-dev.iam.gserviceaccount.com";
+
+    private Neo4jResourceManager neo4jClient;
+
+    @Before
+    public void setup() {
+        neo4jClient = Neo4jResourceManager.builder(testName)
+                .setAdminPassword("letmein!")
+                .setHost(TestProperties.hostIp())
+                .build();
+    }
+
+    @After
+    public void tearDown() {
+        ResourceManagerUtils.cleanResources(neo4jClient);
+    }
+
+    @Test
+    public void test() {
+        assertThat(2).isEqualTo(1 + 2);
+    }
+
+    @Test
+    public void runsWithProjectAndDataset() throws IOException {
+        gcsClient.createArtifact(
+                "project-and-dataset.json",
+                contentOf("/testing-specs/minimal-service-account/project-and-dataset.json"));
+        gcsClient.createArtifact(
+                "neo4j.json",
+                String.format(
+                        "{\n"
+                                + "  \"server_url\": \"%s\",\n"
+                                + "  \"database\": \"%s\",\n"
+                                + "  \"auth_type\": \"basic\",\n"
+                                + "  \"username\": \"neo4j\",\n"
+                                + "  \"pwd\": \"%s\"\n"
+                                + "}",
+                        neo4jClient.getUri(), neo4jClient.getDatabaseName(), neo4jClient.getAdminPassword()));
+
+        LaunchConfig.Builder options = LaunchConfig.builder(testName, specPath)
+                .addParameter("jobSpecUri", getGcsPath("spec.json"))
+                .addParameter("neo4jConnectionUri", getGcsPath("neo4j.json"))
+                .addParameter("serviceAccount", MINIMAL_SERVICE_ACCOUNT);
+
+        LaunchInfo info = launchTemplate(options);
+        assertThatPipeline(info).isRunning();
+        PipelineOperator.Result result = pipelineOperator()
+                .waitForCondition(
+                        createConfig(info),
+                        Neo4jQueryCheck.builder(neo4jClient)
+                                .setQuery("RETURN EXISTS {\n" +
+                                        "    MATCH (p:Person)\n" +
+                                        "    RETURN p\n" +
+                                        "} AS personExists")
+                                .setExpectedResult(List.of(Map.of("personExists", true)))
+                                .build());
+        assertThatResult(result).meetsConditions();
+    }
+
+    @Test
+    public void runsWithOnlyDataset() {}
+
+    @Test
+    public void failsWithoutProjectAndDataset() {}
 }
