101789530540ab1687eb7357b0121ad1958ae309
pawankashyapollion <v-pawan.kumar@ollion.com>
2024-12-28 13:50:51 +0530

    Removed Unwanted options

--- v2/spanner-to-sourcedb/src/main/java/com/google/cloud/teleport/v2/templates/SpannerToSourceDb.java
+++ v2/spanner-to-sourcedb/src/main/java/com/google/cloud/teleport/v2/templates/SpannerToSourceDb.java
@@ -419,15 +419,6 @@ public class SpannerToSourceDb {
     String getFilterEventsDirectoryName();
 
     void setFilterEventsDirectoryName(String value);
-
-    @TemplateParameter.GcsReadFile(
-        order = 10,
-        optional = false,
-        description = "Path to GCS file containing the the Cassandra Config details",
-        helpText = "Path to GCS file containing connection profile info for cassandra.")
-    String getCassandraConfigFilePath();
-
-    void setCassandraConfigFilePath(String value);
   }
 
   /**
@@ -550,7 +541,7 @@ public class SpannerToSourceDb {
       }
     } else {
       CassandraConfigFileReader cassandraConfigFileReader = new CassandraConfigFileReader();
-      shards = cassandraConfigFileReader.getCassandraShard(options.getCassandraConfigFilePath());
+      shards = cassandraConfigFileReader.getCassandraShard(options.getSourceShardsFilePath());
       LOG.info("Cassandra config is: {}", shards.get(0));
       if (shards.size() == 1) {
         shardingMode = Constants.SHARDING_MODE_SINGLE_SHARD;
