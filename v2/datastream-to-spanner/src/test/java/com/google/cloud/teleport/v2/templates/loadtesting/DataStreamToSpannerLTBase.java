c6ecb5bd64f710563b4c124ea49c071c06ac170f
djagaluru <djagaluru@google.com>
2024-12-29 11:50:44 +0000

    correcting lt failures

--- v2/datastream-to-spanner/src/test/java/com/google/cloud/teleport/v2/templates/loadtesting/DataStreamToSpannerLTBase.java
+++ v2/datastream-to-spanner/src/test/java/com/google/cloud/teleport/v2/templates/loadtesting/DataStreamToSpannerLTBase.java
@@ -168,7 +168,7 @@ public class DataStreamToSpannerLTBase extends TemplateLoadTestBase {
       checks[iterationCount] =
           SpannerRowsCheck.builder(spannerResourceManager, entry.getKey())
               .setMinRows(entry.getValue())
-              .setMaxRows(entry.getValue())
+              // .setMaxRows(entry.getValue())
               .build();
       iterationCount++;
     }
@@ -204,6 +204,7 @@ public class DataStreamToSpannerLTBase extends TemplateLoadTestBase {
   @After
   public void cleanUp() throws IOException {
     ResourceManagerUtils.cleanResources(
+        secretClient,
         spannerResourceManager,
         pubsubResourceManager,
         gcsResourceManager,
