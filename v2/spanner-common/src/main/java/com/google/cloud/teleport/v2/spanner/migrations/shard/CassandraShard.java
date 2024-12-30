d0ba08e255a066891f85757d9b896855ad8ff10f
pawankashyapollion <v-pawan.kumar@ollion.com>
2024-12-30 17:03:24 +0530

    Added Missing Getter for Configuration

--- v2/spanner-common/src/main/java/com/google/cloud/teleport/v2/spanner/migrations/shard/CassandraShard.java
+++ v2/spanner-common/src/main/java/com/google/cloud/teleport/v2/spanner/migrations/shard/CassandraShard.java
@@ -79,6 +79,14 @@ public class CassandraShard extends Shard {
     return this.optionsMap;
   }
 
+  public String getUsername() {
+    return getProfile().getString(DefaultDriverOption.AUTH_PROVIDER_USER_NAME);
+  }
+
+  public String getPassword() {
+    return getProfile().getString(DefaultDriverOption.AUTH_PROVIDER_PASSWORD);
+  }
+
   @Override
   public String toString() {
     return String.format(
