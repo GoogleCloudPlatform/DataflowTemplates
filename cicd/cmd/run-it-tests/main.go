bf1c3d74b058474087f9353ef9a13ee422df71f5
nasir19noor <nasir@nasir.id>
2025-01-15 18:01:33 +0700

    modify the format

--- cicd/cmd/run-it-tests/main.go
+++ cicd/cmd/run-it-tests/main.go
@@ -14,72 +14,72 @@
  * the License.
  */
 
- package main
+package main
 
- import (
-	 "flag"
-	 "log"
- 
-	 "github.com/GoogleCloudPlatform/DataflowTemplates/cicd/internal/flags"
-	 "github.com/GoogleCloudPlatform/DataflowTemplates/cicd/internal/workflows"
- )
- 
- func main() {
-	 flags.RegisterCommonFlags()
-	 flags.RegisterItFlags()
-	 flag.Parse()
- 
-	 // Run mvn install before running integration tests
-	 mvnFlags := workflows.NewMavenFlags()
-	 err := workflows.MvnCleanInstall().Run(
-		 mvnFlags.IncludeDependencies(),
-		 mvnFlags.IncludeDependents(),
-		 mvnFlags.SkipDependencyAnalysis(),
-		 mvnFlags.SkipCheckstyle(),
-		 mvnFlags.SkipJib(),
-		 mvnFlags.SkipTests(),
-		 mvnFlags.SkipJacoco(),
-		 mvnFlags.SkipShade(),
-		 mvnFlags.ThreadCount(8),
-		 mvnFlags.InternalMaven())
-	 if err != nil {
-		 log.Fatalf("%v\n", err)
-	 }
- 
-	 // Run integration tests
-	 mvnFlags = workflows.NewMavenFlags()
-	 err = workflows.MvnVerify().Run(
-		 mvnFlags.IncludeDependencies(),
-		 mvnFlags.IncludeDependents(),
-		 mvnFlags.SkipDependencyAnalysis(),
-		 mvnFlags.SkipCheckstyle(),
-		 mvnFlags.SkipJib(),
-		 mvnFlags.SkipShade(),
-		 mvnFlags.RunIntegrationTests(flags.UnifiedWorkerHarnessContainerImage() != ""),
-		 mvnFlags.ThreadCount(4),
-		 mvnFlags.IntegrationTestParallelism(3),
-		 mvnFlags.StaticBigtableInstance("teleport"),
-		 mvnFlags.StaticSpannerInstance("spanner-demo"),
-		 mvnFlags.InternalMaven(),
-		 flags.Region(),
-		 flags.Project(),
-		 flags.ArtifactBucket(),
-		 flags.StageBucket(),
-		 flags.HostIp(),
-		 flags.PrivateConnectivity(),
-		 flags.SpannerHost(),
-		 flags.FailureMode(),
-		 flags.RetryFailures(),
-		 flags.StaticOracleHost(),
-		 flags.StaticOracleSysPassword(),
-		 flags.CloudProxyHost(),
-		 flags.CloudProxyMySqlPort(),
-		 flags.CloudProxyPostgresPort(),
-		 flags.CloudProxyPassword(),
-		 flags.UnifiedWorkerHarnessContainerImage(),
-		 flags.CloudProxyPassword())
-	 if err != nil {
-		 log.Fatalf("%v\n", err)
-	 }
-	 log.Println("Build Successful!")
- }
+import (
+	"flag"
+	"log"
+
+	"github.com/GoogleCloudPlatform/DataflowTemplates/cicd/internal/flags"
+	"github.com/GoogleCloudPlatform/DataflowTemplates/cicd/internal/workflows"
+)
+
+func main() {
+	flags.RegisterCommonFlags()
+	flags.RegisterItFlags()
+	flag.Parse()
+
+	// Run mvn install before running integration tests
+	mvnFlags := workflows.NewMavenFlags()
+	err := workflows.MvnCleanInstall().Run(
+		mvnFlags.IncludeDependencies(),
+		mvnFlags.IncludeDependents(),
+		mvnFlags.SkipDependencyAnalysis(),
+		mvnFlags.SkipCheckstyle(),
+		mvnFlags.SkipJib(),
+		mvnFlags.SkipTests(),
+		mvnFlags.SkipJacoco(),
+		mvnFlags.SkipShade(),
+		mvnFlags.ThreadCount(8),
+		mvnFlags.InternalMaven())
+	if err != nil {
+		log.Fatalf("%v\n", err)
+	}
+
+	// Run integration tests
+	mvnFlags = workflows.NewMavenFlags()
+	err = workflows.MvnVerify().Run(
+		mvnFlags.IncludeDependencies(),
+		mvnFlags.IncludeDependents(),
+		mvnFlags.SkipDependencyAnalysis(),
+		mvnFlags.SkipCheckstyle(),
+		mvnFlags.SkipJib(),
+		mvnFlags.SkipShade(),
+		mvnFlags.RunIntegrationTests(flags.UnifiedWorkerHarnessContainerImage() != ""),
+		mvnFlags.ThreadCount(4),
+		mvnFlags.IntegrationTestParallelism(3),
+		mvnFlags.StaticBigtableInstance("teleport"),
+		mvnFlags.StaticSpannerInstance("spanner-demo"),
+		mvnFlags.InternalMaven(),
+		flags.Region(),
+		flags.Project(),
+		flags.ArtifactBucket(),
+		flags.StageBucket(),
+		flags.HostIp(),
+		flags.PrivateConnectivity(),
+		flags.SpannerHost(),
+		flags.FailureMode(),
+		flags.RetryFailures(),
+		flags.StaticOracleHost(),
+		flags.StaticOracleSysPassword(),
+		flags.CloudProxyHost(),
+		flags.CloudProxyMySqlPort(),
+		flags.CloudProxyPostgresPort(),
+		flags.CloudProxyPassword(),
+		flags.UnifiedWorkerHarnessContainerImage(),
+		flags.CloudProxyPassword())
+	if err != nil {
+		log.Fatalf("%v\n", err)
+	}
+	log.Println("Build Successful!")
+}
