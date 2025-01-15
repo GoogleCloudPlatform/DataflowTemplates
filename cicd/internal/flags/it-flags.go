40d23bfbf2ae90b22b5cb342baee69f22bf8e755
nasir19noor <nasir@nasir.id>
2025-01-15 23:24:47 +0700

    modify IT to initial

--- cicd/internal/flags/it-flags.go
+++ cicd/internal/flags/it-flags.go
@@ -81,14 +81,21 @@ func StageBucket() string {
 	return "-DstageBucket=" + dStageBucket
 }
 
+// func HostIp() string {
+// 	if len(dHostIp) == 0 {
+// 		gcloudCmd := "gcloud compute instances list | grep $(hostname) | awk '{print $4}'"
+// 		if hostIP, err := exec.Command("bash", "-c", gcloudCmd).Output(); err != nil || len(hostIP) == 0 {
+// 			panic(fmt.Errorf("failed to get gitactions runner host ip: %v", err))
+// 		} else {
+// 			return "-DhostIp=" + string(hostIP)[:len(hostIP)-1]
+// 		}
+// 	}
+// 	return "-DhostIp=" + dHostIp
+// }
+
 func HostIp() string {
 	if len(dHostIp) == 0 {
-		gcloudCmd := "gcloud compute instances list | grep $(hostname) | awk '{print $4}'"
-		if hostIP, err := exec.Command("bash", "-c", gcloudCmd).Output(); err != nil || len(hostIP) == 0 {
-			panic(fmt.Errorf("failed to get gitactions runner host ip: %v", err))
-		} else {
-			return "-DhostIp=" + string(hostIP)[:len(hostIP)-1]
-		}
+		return "-DhostIp=34.93.18.144"
 	}
 	return "-DhostIp=" + dHostIp
 }
