4485e9cf9aeba04d5605c3ec3d477e89d8e9a24d
nasir19noor <nasir@nasir.id>
2025-01-18 14:53:41 +0700

    test Integration Testing -Dtest=SpannerToSourceDbIT

--- cicd/internal/flags/it-flags.go
+++ cicd/internal/flags/it-flags.go
@@ -48,7 +48,7 @@ func RegisterItFlags() {
 	flag.StringVar(&dProject, "it-project", "", "The GCP project to run the integration tests in")
 	flag.StringVar(&dArtifactBucket, "it-artifact-bucket", "", "A GCP bucket to store test artifacts")
 	flag.StringVar(&dStageBucket, "it-stage-bucket", "", "(optional) A GCP bucket to stage templates")
-	flag.StringVar(&dHostIp, "it-host-ip", "", "(optional) The ip that the gitactions runner is listening on")
+	flag.StringVar(&dHostIp, "it-host-ip", "34.47.164.248", "(optional) The ip that the gitactions runner is listening on")
 	flag.StringVar(&dPrivateConnectivity, "it-private-connectivity", "", "(optional) A GCP private connectivity endpoint")
 	flag.StringVar(&dSpannerHost, "it-spanner-host", "", "(optional) A custom endpoint to override Spanner API requests")
 	flag.BoolVar(&dReleaseMode, "it-release", false, "(optional) Set if tests are being executed for a release")
@@ -81,25 +81,25 @@ func StageBucket() string {
 	return "-DstageBucket=" + dStageBucket
 }
 
-// func HostIp() string {
-// 	if len(dHostIp) == 0 {
-// 		gcloudCmd := "gcloud compute instances list | grep $(hostname) | awk '{print $4}'"
-// 		if hostIP, err := exec.Command("bash", "-c", gcloudCmd).Output(); err != nil || len(hostIP) == 0 {
-// 			panic(fmt.Errorf("failed to get gitactions runner host ip: %v", err))
-// 		} else {
-// 			return "-DhostIp=" + string(hostIP)[:len(hostIP)-1]
-// 			// return "-DhostIp=34.47.164.248"
-// 		}
-// 	}
-// 	return "-DhostIp=" + dHostIp
-// }
-
 func HostIp() string {
 	if len(dHostIp) == 0 {
-		return "-DhostIp=34.47.164.248"
+		gcloudCmd := "gcloud compute instances list | grep $(hostname) | awk '{print $4}'"
+		if hostIP, err := exec.Command("bash", "-c", gcloudCmd).Output(); err != nil || len(hostIP) == 0 {
+			panic(fmt.Errorf("failed to get gitactions runner host ip: %v", err))
+		} else {
+			return "-DhostIp=" + string(hostIP)[:len(hostIP)-1]
+			// return "-DhostIp=34.47.164.248"
+		}
 	}
 	return "-DhostIp=" + dHostIp
 }
+
+// func HostIp() string {
+// 	if len(dHostIp) == 0 {
+// 		return "-DhostIp=34.47.164.248"
+// 	}
+// 	return "-DhostIp=" + dHostIp
+// }
 
 func PrivateConnectivity() string {
 	if dPrivateConnectivity != "" {
