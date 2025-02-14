8492efc9d9c1d39c3761b22ef1a0973830fccadb
Hengfeng Li <hengfeng@google.com>
2025-02-14 20:58:29 +1100

    Fix lint issues.

--- v1/src/test/java/com/google/cloud/teleport/spanner/ImportPipelineIT.java
+++ v1/src/test/java/com/google/cloud/teleport/spanner/ImportPipelineIT.java
@@ -97,26 +97,26 @@ public class ImportPipelineIT extends SpannerTemplateITBase {
               .getPath());
 
       gcsClient.uploadArtifact(
-              "input/Identity.avro-00000-of-00001",
-              Resources.getResource("ImportPipelineIT/" + subdirectory + "/Identity.avro").getPath());
+          "input/Identity.avro-00000-of-00001",
+          Resources.getResource("ImportPipelineIT/" + subdirectory + "/Identity.avro").getPath());
       gcsClient.uploadArtifact(
-              "input/Identity-manifest.json",
-              Resources.getResource("ImportPipelineIT/" + subdirectory + "/Identity-manifest.json")
-                      .getPath());
+          "input/Identity-manifest.json",
+          Resources.getResource("ImportPipelineIT/" + subdirectory + "/Identity-manifest.json")
+              .getPath());
       gcsClient.uploadArtifact(
-              "input/Sequence1.avro-00000-of-00001",
-              Resources.getResource("ImportPipelineIT/" + subdirectory + "/Sequence1.avro").getPath());
+          "input/Sequence1.avro-00000-of-00001",
+          Resources.getResource("ImportPipelineIT/" + subdirectory + "/Sequence1.avro").getPath());
       gcsClient.uploadArtifact(
-              "input/Sequence1-manifest.json",
-              Resources.getResource("ImportPipelineIT/" + subdirectory + "/Sequence1-manifest.json")
-                      .getPath());
+          "input/Sequence1-manifest.json",
+          Resources.getResource("ImportPipelineIT/" + subdirectory + "/Sequence1-manifest.json")
+              .getPath());
       gcsClient.uploadArtifact(
-              "input/Sequence2.avro-00000-of-00001",
-              Resources.getResource("ImportPipelineIT/" + subdirectory + "/Sequence2.avro").getPath());
+          "input/Sequence2.avro-00000-of-00001",
+          Resources.getResource("ImportPipelineIT/" + subdirectory + "/Sequence2.avro").getPath());
       gcsClient.uploadArtifact(
-              "input/Sequence2-manifest.json",
-              Resources.getResource("ImportPipelineIT/" + subdirectory + "/Sequence2-manifest.json")
-                      .getPath());
+          "input/Sequence2-manifest.json",
+          Resources.getResource("ImportPipelineIT/" + subdirectory + "/Sequence2-manifest.json")
+              .getPath());
     }
 
     gcsClient.uploadArtifact(
