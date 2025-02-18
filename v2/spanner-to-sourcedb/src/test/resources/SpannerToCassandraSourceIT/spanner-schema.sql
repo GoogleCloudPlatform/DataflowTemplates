a735775e17a0219b538a815f34d0eb232a8b957b
pawankashyapollion <v-pawan.kumar@ollion.com>
2025-02-18 10:12:17 +0530

    Added Fixes

--- v2/spanner-to-sourcedb/src/test/resources/SpannerToCassandraSourceIT/spanner-schema.sql
+++ v2/spanner-to-sourcedb/src/test/resources/SpannerToCassandraSourceIT/spanner-schema.sql
@@ -1,13 +1,19 @@
+DROP TABLE IF EXISTS users;
+
 CREATE TABLE IF NOT EXISTS users (
     id INT64 NOT NULL,
     full_name STRING(25),
     `from` STRING(25)
 ) PRIMARY KEY(id);
 
+DROP TABLE IF EXISTS users2;
+
 CREATE TABLE IF NOT EXISTS users2 (
     id INT64 NOT NULL,
     full_name STRING(25),
     ) PRIMARY KEY(id);
+
+DROP TABLE IF EXISTS alldatatypetransformation;
 
 CREATE TABLE IF NOT EXISTS alldatatypetransformation (
     varchar_column STRING(20) NOT NULL,
@@ -51,6 +57,8 @@ CREATE TABLE IF NOT EXISTS alldatatypetransformation (
     frozen_list_of_sets_column JSON,
     varint_column STRING(MAX)
 ) PRIMARY KEY(varchar_column);
+
+DROP TABLE IF EXISTS alldatatypecolumns;
 
 CREATE TABLE IF NOT EXISTS alldatatypecolumns (
     varchar_column STRING(20) NOT NULL,
@@ -96,6 +104,54 @@ CREATE TABLE IF NOT EXISTS alldatatypecolumns (
     varint_column STRING(MAX),
     inet_column STRING(MAX)
 ) PRIMARY KEY(varchar_column);
+
+DROP TABLE IF EXISTS alldatatypecolumnsfornullkey;
+
+CREATE TABLE IF NOT EXISTS alldatatypecolumnsfornullkey (
+    varchar_column STRING(20) NOT NULL,
+    tinyint_column INT64,
+    text_column STRING(MAX),
+    date_column DATE,
+    smallint_column INT64,
+    mediumint_column INT64,
+    int_column INT64,
+    bigint_column INT64,
+    float_column FLOAT64,
+    double_column FLOAT64,
+    decimal_column NUMERIC,
+    datetime_column TIMESTAMP,
+    timestamp_column TIMESTAMP,
+    time_column STRING(MAX),
+    year_column STRING(MAX),
+    char_column STRING(10),
+    tinytext_column STRING(MAX),
+    mediumtext_column STRING(MAX),
+    longtext_column STRING(MAX),
+    enum_column STRING(MAX),
+    bool_column BOOL,
+    other_bool_column BOOL,
+    bytes_column BYTES(MAX),
+    list_text_column JSON,
+    list_int_column JSON,
+    frozen_list_bigint_column JSON,
+    set_text_column JSON,
+    set_date_column JSON,
+    frozen_set_bool_column JSON,
+    map_text_to_int_column JSON,
+    map_date_to_text_column JSON,
+    frozen_map_int_to_bool_column JSON,
+    map_text_to_list_column JSON,
+    map_text_to_set_column JSON,
+    set_of_maps_column JSON,
+    list_of_sets_column JSON,
+    frozen_map_text_to_list_column JSON,
+    frozen_map_text_to_set_column JSON,
+    frozen_set_of_maps_column JSON,
+    frozen_list_of_sets_column JSON,
+    varint_column STRING(MAX),
+    inet_column STRING(MAX)
+) PRIMARY KEY(varchar_column);
+
 
 CREATE CHANGE STREAM allstream
   FOR ALL OPTIONS (
