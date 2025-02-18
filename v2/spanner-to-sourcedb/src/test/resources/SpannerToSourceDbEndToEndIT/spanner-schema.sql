f84e59051ddae8c3a0abe21519da3626c820f5f4
asthamohta <asthamohta@google.com>
2025-02-18 08:37:26 +0000

    changes

--- v2/spanner-to-sourcedb/src/test/resources/SpannerToSourceDbEndToEndIT/spanner-schema.sql
+++ v2/spanner-to-sourcedb/src/test/resources/SpannerToSourceDbEndToEndIT/spanner-schema.sql
@@ -2,3 +2,9 @@ CREATE TABLE Authors (
     id INT64 NOT NULL ,
     name STRING(200),
 ) PRIMARY KEY (id);
+
+CREATE CHANGE STREAM allstream
+  FOR ALL OPTIONS (
+  value_capture_type = 'NEW_ROW',
+  retention_period = '7d'
+);
