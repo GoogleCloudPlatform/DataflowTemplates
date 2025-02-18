b2814b1c5ee3409a9e5f38c10d6a7de97ae4a13d
pawankashyapollion <v-pawan.kumar@ollion.com>
2025-02-18 11:35:56 +0530

    Added Change Stream

--- v2/spanner-to-sourcedb/src/test/resources/SpannerToCassandraSourceIT/spanner-transformation-schema.sql
+++ v2/spanner-to-sourcedb/src/test/resources/SpannerToCassandraSourceIT/spanner-transformation-schema.sql
@@ -5,3 +5,9 @@ CREATE TABLE IF NOT EXISTS customers (
     first_name STRING(25),
     last_name STRING(25)
 ) PRIMARY KEY(id);
+
+CREATE CHANGE STREAM allstream
+  FOR ALL OPTIONS (
+  value_capture_type = 'NEW_ROW',
+  retention_period = '7d'
+);
