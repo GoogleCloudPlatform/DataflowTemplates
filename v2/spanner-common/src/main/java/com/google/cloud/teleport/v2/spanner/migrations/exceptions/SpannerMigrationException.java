91ccf57886cbdc919da510751bd6140fda9a0a06
djagaluru <djagaluru@google.com>
2025-02-19 17:38:53 +0000

    Creating Spanner Migration Exception

---
+++ v2/spanner-common/src/main/java/com/google/cloud/teleport/v2/spanner/migrations/exceptions/SpannerMigrationException.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2025 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.cloud.teleport.v2.spanner.migrations.exceptions;
+
+public class SpannerMigrationException extends Exception {
+  public enum ErrorCode {
+    // Errors thrown from Spanner
+    SPANNER_TRANSACTION_ABORTED(1001),
+    SPANNER_UNIQUE_INDEX_VIOLATION(1002),
+    SPANNER_TABLE_NOT_FOUND(1003),
+    SPANNER_COLUMN_NOT_FOUND(1004),
+    SPANNER_PARENT_NOT_FOUND(1005),
+    SPANNER_DEADLINE_EXCEEDED(1006),
+    SPANNER_INVALID_VALUE(1007),
+    SPANNER_NULL_VALUE_FOR_REQUIRED_COLUMN(1008),
+    SPANNER_WRITE_TO_GENERATED_COLUMN_NOT_ALLOWED(1009),
+    SPANNER_DEPENDENT_COLUMN_NOT_SPECIFIED(1010),
+    SPANNER_WRONG_NUMBER_OF_KEY_PARTS(1011),
+    SPANNER_FOREIGN_KEY_CONSTRAINT_VIOLATION(1012),
+    SPANNER_VALUE_TOO_LARGE(1013),
+    SPANNER_INVALID_ARGUMENT(1014),
+    SPANNER_CHECK_CONSTRAINT_VIOLATION(1015),
+    SPANNER_OUT_OF_RANGE(1016),
+    SPANNER_PERMISSION_DENIED(1017),
+    SPANNER_RESOURCE_EXHAUSTED(1018),
+    SPANNER_UNAUTHENTICATED(1019),
+    SPANNER_UNIMPLEMENTED(1020),
+    SPANNER_DUPLICATE_COLUMN(1021),
+    SPANNER_UNAVAILABLE(1022),
+    SPANNER_ALREADY_EXISTS(1023),
+    SPANNER_NOT_FOUND(1024),
+    SPANNER_UNKNOWN_ERROR(1200), // SpannerException catch all error code
+
+    // Conversion and Transformation errors
+    AVRO_TYPE_CONVERSION_ERROR(2001),
+    CHANGE_EVENT_CONVERSION_ERROR(2002),
+    INVALID_CHANGE_EVENT(2003),
+    CUSTOM_TRANSFORMATION_ERROR(2004),
+
+    // Errors from Source Database
+
+    // Generic error
+    UNKNOWN_ERROR(9999);
+
+    private final int code;
+
+    ErrorCode(int code) {
+      this.code = code;
+    }
+
+    public int getCode() {
+      return code;
+    }
+  }
+
+  private final ErrorCode errorCode;
+
+  public SpannerMigrationException(ErrorCode errorCode, String message) {
+    super(errorCode.getCode() + " - " + message);
+    this.errorCode = errorCode;
+  }
+
+  public SpannerMigrationException(ErrorCode errorCode, String message, Throwable cause) {
+    super(errorCode.getCode() + " - " + message, cause);
+    this.errorCode = errorCode;
+  }
+
+  public ErrorCode getErrorCode() {
+    return errorCode;
+  }
+}
--- v2/spanner-common/src/test/java/com/google/cloud/teleport/v2/spanner/exceptions/SpannerExceptionParserTest.java
+++ v2/spanner-common/src/test/java/com/google/cloud/teleport/v2/spanner/exceptions/SpannerExceptionParserTest.java
@@ -13,44 +13,41 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.google.cloud.teleport.v2.templates.spanner;
-
-import static com.google.cloud.teleport.v2.templates.spanner.SpannerExceptionClassifier.ErrorTag.PERMANENT_ERROR;
-import static com.google.cloud.teleport.v2.templates.spanner.SpannerExceptionClassifier.ErrorTag.RETRYABLE_ERROR;
+package com.google.cloud.teleport.v2.spanner.exceptions;
 
 import com.google.cloud.spanner.ErrorCode;
 import com.google.cloud.spanner.SpannerException;
 import com.google.cloud.spanner.SpannerExceptionFactory;
 import com.google.cloud.teleport.v2.spanner.migrations.exceptions.SpannerExceptionParser;
-import com.google.cloud.teleport.v2.templates.spanner.SpannerExceptionClassifier.ErrorTag;
+import com.google.cloud.teleport.v2.spanner.migrations.exceptions.SpannerMigrationException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import org.junit.Assert;
 import org.junit.Test;
 
-/* Unit test for SpannerExceptionClassifier */
-public class SpannerExceptionClassifierTest {
+/* Unit test for SpannerExceptionParser */
+public class SpannerExceptionParserTest {
 
-  private static Map<SpannerException, ErrorTag> exceptionToExpectedTag =
+  private static Map<SpannerException, SpannerMigrationException.ErrorCode> exceptionToExpectedTag =
       new HashMap<>() {
         {
-          // Map of SpannerException: Expected tag
+          // Map of SpannerException: ErrorCode
 
           // ABORTED
           put(
               SpannerExceptionFactory.newSpannerException(ErrorCode.ABORTED, "Transaction Aborted"),
-              RETRYABLE_ERROR);
+              SpannerMigrationException.ErrorCode.SPANNER_TRANSACTION_ABORTED);
           // ALREADY_EXISTS
           put(
               SpannerExceptionFactory.newSpannerException(
                   ErrorCode.ALREADY_EXISTS,
                   "Unique index violation on index idx_authors_name at index key [Jane Austen,2]. It conflicts with row [2] in table Authors."),
-              PERMANENT_ERROR);
+              SpannerMigrationException.ErrorCode.SPANNER_UNIQUE_INDEX_VIOLATION);
           put(
               SpannerExceptionFactory.newSpannerException(
                   ErrorCode.ALREADY_EXISTS, "Already exists"),
-              RETRYABLE_ERROR);
+              SpannerMigrationException.ErrorCode.SPANNER_ALREADY_EXISTS);
           // NOT_FOUND
           put(
               SpannerExceptionFactory.newSpannerException(
@@ -59,157 +56,149 @@ public class SpannerExceptionClassifierTest {
                       + "resource_type: \"spanner.googleapis.com/Table\"\n"
                       + "resource_name: \"AAAA\"\n"
                       + "description: \"Table not found\n"),
-              PERMANENT_ERROR);
+              SpannerMigrationException.ErrorCode.SPANNER_TABLE_NOT_FOUND);
           put(
               SpannerExceptionFactory.newSpannerException(
                   ErrorCode.NOT_FOUND,
                   "Column not found in table Authors: age\n"
                       + "resource_type: \"spanner.googleapis.com/Column\"\n"
                       + "resource_name: \"age\"\n"),
-              PERMANENT_ERROR);
+              SpannerMigrationException.ErrorCode.SPANNER_COLUMN_NOT_FOUND);
           put(
               SpannerExceptionFactory.newSpannerException(
                   ErrorCode.NOT_FOUND,
                   "Parent row for row [100,4] in table Books is missing. Row cannot be written."),
-              RETRYABLE_ERROR);
+              SpannerMigrationException.ErrorCode.SPANNER_PARENT_NOT_FOUND);
           put(
               SpannerExceptionFactory.newSpannerException(ErrorCode.NOT_FOUND, "Not found"),
-              RETRYABLE_ERROR);
+              SpannerMigrationException.ErrorCode.SPANNER_NOT_FOUND);
           // CANCELLED
           put(
               SpannerExceptionFactory.newSpannerException(
                   ErrorCode.CANCELLED, "Operation cancelled"),
-              RETRYABLE_ERROR);
+              SpannerMigrationException.ErrorCode.SPANNER_UNKNOWN_ERROR);
           // DEADLINE_EXCEEDED
           put(
               SpannerExceptionFactory.newSpannerException(
                   ErrorCode.DEADLINE_EXCEEDED, "Deadline exceeded while processing the request"),
-              RETRYABLE_ERROR);
+              SpannerMigrationException.ErrorCode.SPANNER_DEADLINE_EXCEEDED);
           // FAILED_PRECONDITION
           put(
               SpannerExceptionFactory.newSpannerException(
                   ErrorCode.FAILED_PRECONDITION,
                   "Invalid value for column id in table Books: Expected INT64"),
-              PERMANENT_ERROR);
+              SpannerMigrationException.ErrorCode.SPANNER_INVALID_VALUE);
           put(
               SpannerExceptionFactory.newSpannerException(
                   ErrorCode.FAILED_PRECONDITION, "title must not be NULL in table Books"),
-              PERMANENT_ERROR);
+              SpannerMigrationException.ErrorCode.SPANNER_NULL_VALUE_FOR_REQUIRED_COLUMN);
           put(
               SpannerExceptionFactory.newSpannerException(
                   ErrorCode.FAILED_PRECONDITION,
                   "Cannot write into generated column `Books.titleUpperStored`"),
-              PERMANENT_ERROR);
+              SpannerMigrationException.ErrorCode.SPANNER_WRITE_TO_GENERATED_COLUMN_NOT_ALLOWED);
           put(
               SpannerExceptionFactory.newSpannerException(
                   ErrorCode.FAILED_PRECONDITION,
                   "For an Update, the value of a generated primary key `id2` must be explicitly specified, or else its non-key dependent column `part1` must be specified. Key: [6,<default>]"),
-              PERMANENT_ERROR);
+              SpannerMigrationException.ErrorCode.SPANNER_DEPENDENT_COLUMN_NOT_SPECIFIED);
           put(
               SpannerExceptionFactory.newSpannerException(
                   ErrorCode.FAILED_PRECONDITION,
                   "Wrong number of key parts for genPK. Expected: 2. Got: [\"6\",\"7\",\"8\"]"),
-              PERMANENT_ERROR);
+              SpannerMigrationException.ErrorCode.SPANNER_WRONG_NUMBER_OF_KEY_PARTS);
           put(
               SpannerExceptionFactory.newSpannerException(
                   ErrorCode.FAILED_PRECONDITION,
                   "Foreign key constraint `FK_C1_Books_C0E4C66AABC06BFE_1` is violated on table `C1`. Cannot find referenced values in Books(id)."),
-              RETRYABLE_ERROR);
+              SpannerMigrationException.ErrorCode.SPANNER_FOREIGN_KEY_CONSTRAINT_VIOLATION);
           put(
               SpannerExceptionFactory.newSpannerException(
                   ErrorCode.FAILED_PRECONDITION,
                   "Integrity constraint violation during DELETE/REPLACE. Found child row [1,1] in table Books"),
-              RETRYABLE_ERROR);
+              SpannerMigrationException.ErrorCode.SPANNER_FOREIGN_KEY_CONSTRAINT_VIOLATION);
           put(
               SpannerExceptionFactory.newSpannerException(
                   ErrorCode.FAILED_PRECONDITION,
                   "Foreign key constraint violation when deleting or updating referenced row(s): referencing row(s) found in table `FKC`"),
-              RETRYABLE_ERROR);
+              SpannerMigrationException.ErrorCode.SPANNER_FOREIGN_KEY_CONSTRAINT_VIOLATION);
           // INTERNAL
           put(
               SpannerExceptionFactory.newSpannerException(
                   ErrorCode.INTERNAL, "Internal Error occurred"),
-              RETRYABLE_ERROR);
+              SpannerMigrationException.ErrorCode.SPANNER_UNKNOWN_ERROR);
           // INVALID_ARGUMENT
           put(
               SpannerExceptionFactory.newSpannerException(
                   ErrorCode.INVALID_ARGUMENT,
                   "Invalid commit request: maximum size for a single column value is: 10485760 bytes."),
-              PERMANENT_ERROR);
+              SpannerMigrationException.ErrorCode.SPANNER_VALUE_TOO_LARGE);
           put(
               SpannerExceptionFactory.newSpannerException(
                   ErrorCode.INVALID_ARGUMENT, "Invalid argument passed."),
-              PERMANENT_ERROR);
+              SpannerMigrationException.ErrorCode.SPANNER_INVALID_ARGUMENT);
           // OUT_OF_RANGE
           put(
               SpannerExceptionFactory.newSpannerException(
                   ErrorCode.OUT_OF_RANGE,
                   "Check constraint `Authors`.`check_author_id` is violated for key `(-1)`"),
-              PERMANENT_ERROR);
+              SpannerMigrationException.ErrorCode.SPANNER_CHECK_CONSTRAINT_VIOLATION);
           put(
               SpannerExceptionFactory.newSpannerException(
                   ErrorCode.OUT_OF_RANGE, "Input is out of range"),
-              PERMANENT_ERROR);
+              SpannerMigrationException.ErrorCode.SPANNER_OUT_OF_RANGE);
           // PERMISSION_DENIED
           put(
               SpannerExceptionFactory.newSpannerException(
                   ErrorCode.PERMISSION_DENIED, "Permission Denied"),
-              PERMANENT_ERROR);
+              SpannerMigrationException.ErrorCode.SPANNER_PERMISSION_DENIED);
           // RESOURCE_EXHAUSTED
           put(
               SpannerExceptionFactory.newSpannerException(
                   ErrorCode.RESOURCE_EXHAUSTED, "Spanner Resource Exhausted"),
-              RETRYABLE_ERROR);
+              SpannerMigrationException.ErrorCode.SPANNER_RESOURCE_EXHAUSTED);
           // UNAUTHENTICATED
           put(
               SpannerExceptionFactory.newSpannerException(
                   ErrorCode.UNAUTHENTICATED, "Unauthenticated"),
-              PERMANENT_ERROR);
+              SpannerMigrationException.ErrorCode.SPANNER_UNAUTHENTICATED);
           // UNAVAILABLE
           put(
               SpannerExceptionFactory.newSpannerException(
                   ErrorCode.UNAVAILABLE, "Spanner temporarily Unavailable"),
-              RETRYABLE_ERROR);
+              SpannerMigrationException.ErrorCode.SPANNER_UNAVAILABLE);
           // UNIMPLEMENTED
           put(
               SpannerExceptionFactory.newSpannerException(
                   ErrorCode.UNIMPLEMENTED, "Feature used is not implemented"),
-              PERMANENT_ERROR);
+              SpannerMigrationException.ErrorCode.SPANNER_UNIMPLEMENTED);
           // UNKNOWN
           put(
               SpannerExceptionFactory.newSpannerException(
                   ErrorCode.UNKNOWN, "Duplicate column: col2"),
-              PERMANENT_ERROR);
+              SpannerMigrationException.ErrorCode.SPANNER_DUPLICATE_COLUMN);
           put(
               SpannerExceptionFactory.newSpannerException(ErrorCode.UNKNOWN, "Unknown exception"),
-              RETRYABLE_ERROR);
+              SpannerMigrationException.ErrorCode.SPANNER_UNKNOWN_ERROR);
         }
       };
 
   @Test
   public void testSpannerExceptionClassification() {
-    for (Entry<SpannerException, ErrorTag> entry : exceptionToExpectedTag.entrySet()) {
-      ErrorTag actual = SpannerExceptionClassifier.classify(SpannerExceptionParser.parse(entry.getKey()));
+    for (Entry<SpannerException, SpannerMigrationException.ErrorCode> entry : exceptionToExpectedTag.entrySet()) {
+      SpannerMigrationException actual = SpannerExceptionParser.parse(entry.getKey());
       assertSpannerExceptionClassification(entry.getKey(), actual, entry.getValue());
     }
   }
 
   public static void assertSpannerExceptionClassification(
-      SpannerException e, ErrorTag actual, ErrorTag expected) {
-    if (expected == RETRYABLE_ERROR) {
+      SpannerException e, SpannerMigrationException actual, SpannerMigrationException.ErrorCode expected) {
       Assert.assertEquals(
           String.format(
-              "Incorrect SpannerException classification: SpannerException=%s Classification=RETRYABLE_ERROR_TAG",
-              e.getMessage()),
+              "Incorrect SpannerException error code parsing: SpannerException=%s, Actual errorCode=%s, Expected errorCode=%s",
+              e.getMessage(), actual.getErrorCode(), expected),
           expected,
           actual);
-    } else {
-      Assert.assertEquals(
-          String.format(
-              "Incorrect SpannerException classification: SpannerException=%s Classification=PERMANENT_ERROR_TAG",
-              e.getMessage()),
-          expected,
-          actual);
-    }
+      Assert.assertEquals(expected.getCode() + " - " + e.getMessage(), actual.getMessage());
   }
 }
