1ab982f7e88af9126450fa3d2e261357d5104458
asthamohta <asthamohta@google.com>
2025-02-19 07:34:56 +0000

    changes

--- v2/spanner-to-sourcedb/src/test/java/com/google/cloud/teleport/v2/templates/SpannerToSourceDbITBase.java
+++ v2/spanner-to-sourcedb/src/test/java/com/google/cloud/teleport/v2/templates/SpannerToSourceDbITBase.java
@@ -124,7 +124,7 @@ public abstract class SpannerToSourceDbITBase extends TemplateTestBase {
     Shard shard = new Shard();
     shard.setLogicalShardId("Shard1");
     shard.setUser(jdbcResourceManager.getUsername());
-    shard.setHost(TestProperties.hostIp());
+    shard.setHost("34.71.118.71");
     shard.setPassword(jdbcResourceManager.getPassword());
     shard.setPort(String.valueOf(jdbcResourceManager.getPort()));
     shard.setDbName(jdbcResourceManager.getDatabaseName());
