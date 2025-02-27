4357ee911e8ab302b5a5ccad0670c3cdb6b6ac20
pawankashyapollion <v-pawan.kumar@ollion.com>
2025-02-27 10:47:22 +0530

    Added Missing Alignment issue with CustomTransformation

--- v2/spanner-to-sourcedb/src/test/resources/SpannerToCassandraSourceIT/spanner-transformation-schema.sql
+++ v2/spanner-to-sourcedb/src/test/resources/SpannerToCassandraSourceIT/spanner-transformation-schema.sql
@@ -1,7 +1,6 @@
 DROP TABLE IF EXISTS customers;
 CREATE TABLE IF NOT EXISTS customers (
     id INT64 NOT NULL,
-    full_name STRING(25),
     first_name STRING(25),
     last_name STRING(25)
 ) PRIMARY KEY(id);
