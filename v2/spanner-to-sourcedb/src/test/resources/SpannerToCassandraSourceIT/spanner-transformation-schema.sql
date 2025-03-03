bc90790835b1bcf2fc540332fa611bd402da60ed
Taher Lakdawala <78196491+taherkl@users.noreply.github.com>
2025-03-03 21:54:42 +0530

    Custom transformation for Cassandra to Spanner SourceDB (#2201)
    * RR custom transformation feature (#102)
    
    * sync upstream/main (#98)
    
    * Custom transformation fixes
    
    * Added Custom Transformation
    
    * Added Custom Transformation
    
    * Added Fixes
    
    * Address null to all columns
    
    * Added Null Assert
    
    * Added Timeout fixes
    
    * Added Spotless fixes
    
    * reverse merge the main
    
    * Added Custom Fixes
    
    * Added Drop Keys
    
    ---------
    
    * Move null test case into previous tests
    
    Move null test case into previous tests
    
    * Issue fix
    
    * Null fix
    
    * Null fix for varint
    
    * Removed unwanted code
    
    * Added Bytes Fixes
    
    * Missing Bytes Error Code Pushed
    
    * PR comment fixes (#114)
    
    * Regex fixes to validate binary string vs base64
    
    * Added Review Comments (#122)
    
    * Added Review Comments
    
    * Address TODO
    
    * Added POM File
    
    * Added Ignore Case in table name check
    
    * Reference Mismatch
    
    * Added full_name as we are skiping extra column and adding as null
    
    * Schema Fixes
    
    * Added Missing Alignment issue with CustomTransformation
    
    * Removed binaryString encoded Support
    
    * removed unwanted log
    
    * Keep Log Level As warm
    
    ---------
    
    Co-authored-by: pawankashyapollion <v-pawan.kumar@ollion.com>

---
+++ v2/spanner-to-sourcedb/src/test/resources/SpannerToCassandraSourceIT/spanner-transformation-schema.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS customers;
+CREATE TABLE IF NOT EXISTS customers (
+    id INT64 NOT NULL,
+    first_name STRING(25),
+    last_name STRING(25)
+) PRIMARY KEY(id);
+
+CREATE CHANGE STREAM allstream
+  FOR ALL OPTIONS (
+  value_capture_type = 'NEW_ROW',
+  retention_period = '7d'
+);
