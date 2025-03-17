04bd350cb4221c311b9309d1d0406f835a525077
Manit Gupta <manitgupta@google.com>
2025-03-17 14:53:28 +0530

    Skip Datastream oracle to spanner IT

--- v2/datastream-to-spanner/src/test/java/com/google/cloud/teleport/v2/templates/DataStreamToSpannerIT.java
+++ v2/datastream-to-spanner/src/test/java/com/google/cloud/teleport/v2/templates/DataStreamToSpannerIT.java
@@ -139,6 +139,7 @@ public class DataStreamToSpannerIT extends SpannerTemplateITBase {
   }
 
   @Test
+  @Ignore("This test is flaky, and Oracle is not fully supported migration source yet")
   public void testDataStreamOracleToSpanner() throws IOException {
     // Run a simple IT
     simpleOracleToSpannerTest(
