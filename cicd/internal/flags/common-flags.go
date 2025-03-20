4e948ac28f65bf7d0cc320a7c13bbea3f90f2c31
Akash Thawait <aakash@ollion.com>
2025-03-20 22:22:12 +0530

    removed other modules from flag

--- cicd/internal/flags/common-flags.go
+++ cicd/internal/flags/common-flags.go
@@ -41,13 +41,15 @@ var (
 			"v2/kafka-to-pubsub/",
 			"plugins/templates-maven-plugin",
 		},
-		SPANNER: {"v2/datastream-to-spanner/",
-			"v2/spanner-change-streams-to-sharded-file-sink/",
-			"v2/gcs-to-sourcedb/",
-			"v2/sourcedb-to-spanner/",
-			"v2/spanner-to-sourcedb/",
-			"v2/spanner-custom-shard",
-			"plugins/templates-maven-plugin"},
+// 		SPANNER: {"v2/datastream-to-spanner/",
+// 			"v2/spanner-change-streams-to-sharded-file-sink/",
+// 			"v2/gcs-to-sourcedb/",
+// 			"v2/sourcedb-to-spanner/",
+// 			"v2/spanner-to-sourcedb/",
+// 			"v2/spanner-custom-shard",
+// 			"plugins/templates-maven-plugin"},
+SPANNER: {
+			"v2/sourcedb-to-spanner/","plugins/templates-maven-plugin"},
 	}
 )
 
