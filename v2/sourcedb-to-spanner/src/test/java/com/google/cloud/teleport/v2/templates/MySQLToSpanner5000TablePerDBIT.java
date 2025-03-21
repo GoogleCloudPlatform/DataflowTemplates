ccd5c0d70a5dd77b1a2fa6f431e11fa48f04cf30
Akash Thawait <aakash@ollion.com>
2025-03-21 13:53:51 +0530

    fix spotless issyes

--- v2/sourcedb-to-spanner/src/test/java/com/google/cloud/teleport/v2/templates/MySQLToSpanner5000TablePerDBIT.java
+++ v2/sourcedb-to-spanner/src/test/java/com/google/cloud/teleport/v2/templates/MySQLToSpanner5000TablePerDBIT.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.google.cloud.teleport.v2.templates;
 
 import static org.apache.beam.it.truthmatchers.PipelineAsserts.assertThatResult;
@@ -5,7 +20,6 @@ import static org.apache.beam.it.truthmatchers.PipelineAsserts.assertThatResult;
 import com.google.cloud.teleport.metadata.SkipDirectRunnerTest;
 import com.google.cloud.teleport.metadata.TemplateIntegrationTest;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.IntStream;
@@ -52,14 +66,15 @@ public class MySQLToSpanner5000TablePerDBIT extends SourceDbToSpannerITBase {
   public void testMySQLToSpannerMigration() throws Exception {
     IntStream.range(0, NUM_TABLES).forEach(this::createSchemas);
 
-    jobInfo = launchDataflowJob(
-        getClass().getSimpleName(),
-        null,
-        null,
-        mySQLResourceManager,
-        spannerResourceManager,
-        null,
-        null);
+    jobInfo =
+        launchDataflowJob(
+            getClass().getSimpleName(),
+            null,
+            null,
+            mySQLResourceManager,
+            spannerResourceManager,
+            null,
+            null);
 
     PipelineOperator.Result result = pipelineOperator().waitUntilDone(createConfig(jobInfo));
     assertThatResult(result).isLaunchFinished();
@@ -76,23 +91,21 @@ public class MySQLToSpanner5000TablePerDBIT extends SourceDbToSpannerITBase {
   private void verifySpannerData(int tableNumber) {
     String tableName = "table_" + tableNumber;
     SpannerAsserts.assertThatStructs(
-        spannerResourceManager.readTableRecords(tableName, "id", "name"))
+            spannerResourceManager.readTableRecords(tableName, "id", "name"))
         .hasRecordsUnorderedCaseInsensitiveColumns(getMySQLData());
   }
 
   private JDBCResourceManager.JDBCSchema getMySQLSchema() {
-    Map<String, String> columns = Map.of(
-        "id", "INTEGER NOT NULL",
-        "name", "VARCHAR(20)"
-    );
+    Map<String, String> columns =
+        Map.of(
+            "id", "INTEGER NOT NULL",
+            "name", "VARCHAR(20)");
     return new JDBCResourceManager.JDBCSchema(columns, "id");
   }
 
   private List<Map<String, Object>> getMySQLData() {
-    return Collections.singletonList(Map.of(
-        "id", 1,
-        "name", RandomStringUtils.randomAlphabetic(10)
-    ));
+    return Collections.singletonList(
+        Map.of("id", 1, "name", RandomStringUtils.randomAlphabetic(10)));
   }
 
   private void createMySQLSchema(MySQLResourceManager mySQLResourceManager, String tableName) {
@@ -100,9 +113,11 @@ public class MySQLToSpanner5000TablePerDBIT extends SourceDbToSpannerITBase {
     mySQLResourceManager.write(tableName, getMySQLData());
   }
 
-  private void createSpannerSchema(SpannerResourceManager spannerResourceManager, String tableName) {
-    String createTableStatement = String.format(
-        "CREATE TABLE %s (id INT64 NOT NULL, name STRING(20)) PRIMARY KEY (id)", tableName);
+  private void createSpannerSchema(
+      SpannerResourceManager spannerResourceManager, String tableName) {
+    String createTableStatement =
+        String.format(
+            "CREATE TABLE %s (id INT64 NOT NULL, name STRING(20)) PRIMARY KEY (id)", tableName);
     spannerResourceManager.executeDdlStatement(createTableStatement);
   }
 }
