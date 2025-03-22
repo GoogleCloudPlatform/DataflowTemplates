eeeb56ad04272f6e0786c79908a145c19f193f5b
Akash Thawait <aakash@ollion.com>
2025-03-22 15:03:27 +0530

    added logs'

--- v2/sourcedb-to-spanner/src/test/java/com/google/cloud/teleport/v2/templates/MySQLSourceDBToSpannerWideRowMaxSizeStringIT.java
+++ v2/sourcedb-to-spanner/src/test/java/com/google/cloud/teleport/v2/templates/MySQLSourceDBToSpannerWideRowMaxSizeStringIT.java
@@ -37,11 +37,15 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Category({TemplateIntegrationTest.class, SkipDirectRunnerTest.class})
 @TemplateIntegrationTest(SourceDbToSpanner.class)
 @RunWith(JUnit4.class)
 public class MySQLSourceDBToSpannerWideRowMaxSizeStringIT extends SourceDbToSpannerITBase {
+  private static final Logger LOG =
+      LoggerFactory.getLogger(MySQLSourceDBToSpannerWideRowMaxSizeStringIT.class);
   private static PipelineLauncher.LaunchInfo jobInfo;
   private static final Integer MAX_CHARACTER_SIZE = 2621440;
   private static final String TABLENAME = "WideRowTable";
@@ -50,12 +54,43 @@ public class MySQLSourceDBToSpannerWideRowMaxSizeStringIT extends SourceDbToSpan
 
   @Before
   public void setUp() {
-    mySQLResourceManager = setUpMySQLResourceManager();
-    spannerResourceManager = setUpSpannerResourceManager();
+    try {
+      LOG.info("Setting up MySQL Resource Manager");
+      mySQLResourceManager = setUpMySQLResourceManager();
+
+      LOG.info("Setting up Spanner Resource Manager");
+      spannerResourceManager = setUpSpannerResourceManager();
+
+      if (spannerResourceManager == null) {
+        throw new IllegalStateException("Failed to initialize Spanner Resource Manager");
+      }
+
+      LOG.info(
+          "Successfully setup resource managers. Spanner instance: {}, database: {}",
+          spannerResourceManager.getInstanceId(),
+          spannerResourceManager.getDatabaseId());
+    } catch (Exception e) {
+      LOG.error("Error during setup", e);
+      throw new RuntimeException("Failed to set up test resources", e);
+    }
+  }
+
+  @Override
+  public SpannerResourceManager setUpSpannerResourceManager() {
+    try {
+      // Explicitly use the base class builder with the proper project and region
+      return SpannerResourceManager.builder(testName, PROJECT, REGION)
+          .maybeUseStaticInstance()
+          .build();
+    } catch (Exception e) {
+      LOG.error("Failed to create SpannerResourceManager", e);
+      throw new RuntimeException("Failed to create SpannerResourceManager", e);
+    }
   }
 
   @After
   public void cleanUp() {
+    LOG.info("Cleaning up resources");
     ResourceManagerUtils.cleanResources(spannerResourceManager, mySQLResourceManager);
   }
 
@@ -88,11 +123,16 @@ public class MySQLSourceDBToSpannerWideRowMaxSizeStringIT extends SourceDbToSpan
 
   @Test
   public void testMySQLToSpannerWiderowForMaxSizeString() throws Exception {
-
+    LOG.info("Creating MySQL table: {}", TABLENAME);
     mySQLResourceManager.createTable(TABLENAME, getMySQLSchema());
+
+    LOG.info("Creating Spanner DDL");
     createSpannerDDL(spannerResourceManager, getSpannerSchema());
 
+    LOG.info("Writing data to MySQL table");
     mySQLResourceManager.write(TABLENAME, getMySQLData());
+
+    LOG.info("Launching Dataflow job");
     jobInfo =
         launchDataflowJob(
             getClass().getSimpleName(),
@@ -102,8 +142,12 @@ public class MySQLSourceDBToSpannerWideRowMaxSizeStringIT extends SourceDbToSpan
             spannerResourceManager,
             null,
             null);
+
+    LOG.info("Waiting for job to complete with ID: {}", jobInfo.jobId());
     PipelineOperator.Result result = pipelineOperator().waitUntilDone(createConfig(jobInfo));
     assertThatResult(result).isLaunchFinished();
+
+    LOG.info("Verifying data in Spanner");
     SpannerAsserts.assertThatStructs(
             spannerResourceManager.readTableRecords(TABLENAME, "id", "max_string_col"))
         .hasRecordsUnorderedCaseInsensitiveColumns(getMySQLData());
