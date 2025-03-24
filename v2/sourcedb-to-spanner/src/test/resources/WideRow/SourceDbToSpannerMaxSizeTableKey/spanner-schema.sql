7c50f6ea3c7ab7ea73c40a76aba4729dbb1f7777
Akash Thawait <aakash@ollion.com>
2025-03-24 10:50:49 +0530

    fixed spanner schema

--- v2/sourcedb-to-spanner/src/test/resources/WideRow/SourceDbToSpannerMaxSizeTableKey/spanner-schema.sql
+++ v2/sourcedb-to-spanner/src/test/resources/WideRow/SourceDbToSpannerMaxSizeTableKey/spanner-schema.sql
@@ -2,6 +2,5 @@ CREATE TABLE LargePrimaryKeyTable (
     pk_col1 STRING(255) NOT NULL,
     pk_col2 STRING(255) NOT NULL,
     pk_col3 STRING(255) NOT NULL,
-    value_col STRING(MAX),
-    PRIMARY KEY (pk_col1, pk_col2, pk_col3)
-);
+    value_col STRING(MAX)
+)PRIMARY KEY (pk_col1, pk_col2, pk_col3);
