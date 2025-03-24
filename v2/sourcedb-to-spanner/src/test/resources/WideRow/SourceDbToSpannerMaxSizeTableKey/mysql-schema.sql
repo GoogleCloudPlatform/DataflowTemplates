2f26660b97349f228dad334ea413f8d21713ec7d
Akash Thawait <aakash@ollion.com>
2025-03-24 10:27:24 +0530

    fixed sql query

--- v2/sourcedb-to-spanner/src/test/resources/WideRow/SourceDbToSpannerMaxSizeTableKey/mysql-schema.sql
+++ v2/sourcedb-to-spanner/src/test/resources/WideRow/SourceDbToSpannerMaxSizeTableKey/mysql-schema.sql
@@ -1,9 +1,9 @@
 CREATE TABLE LargePrimaryKeyTable (
-    pk_col1 VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL,
-    pk_col2 VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL,
-    pk_col3 VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL,
-    value_col TEXT,
-    PRIMARY KEY (pk_col1, pk_col2, pk_col3)  -- Only 3 columns allowed
+pk_col1 VARCHAR(255) NOT NULL,
+pk_col2 VARCHAR(255) NOT NULL,
+pk_col3 VARCHAR(255) NOT NULL,
+value_col TEXT,
+PRIMARY KEY (pk_col1, pk_col2, pk_col3)
 );
 
 INSERT INTO LargePrimaryKeyTable (
