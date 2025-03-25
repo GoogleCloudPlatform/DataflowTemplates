94bf1f45e2319171bfe4229827548a293b013711
Akash Thawait <aakash@ollion.com>
2025-03-25 19:45:17 +0530

    added sql schema file for 5k tables

--- v2/sourcedb-to-spanner/src/test/java/com/google/cloud/teleport/v2/templates/MySQLToSpanner5000TablePerDBIT.java
+++ v2/sourcedb-to-spanner/src/test/java/com/google/cloud/teleport/v2/templates/MySQLToSpanner5000TablePerDBIT.java
@@ -16,7 +16,6 @@
 package com.google.cloud.teleport.v2.templates;
 
 import static org.apache.beam.it.truthmatchers.PipelineAsserts.assertThatResult;
-import static org.junit.Assert.assertEquals;
 
 import com.google.cloud.teleport.metadata.SkipDirectRunnerTest;
 import com.google.cloud.teleport.metadata.TemplateIntegrationTest;
@@ -51,6 +50,10 @@ public class MySQLToSpanner5000TablePerDBIT extends SourceDbToSpannerITBase {
   private static final String COL_ID = "id";
   private static final String COL_NAME = "name";
   private static final int MAX_ALLOWED_PACKET = 128 * 1024 * 1024;
+  private static final String MYSQL_DUMP_FILE_RESOURCE =
+      "WideRow/5000TablePerDBIT/mysql-schema.sql";
+  private static final String SPANNER_SCHEMA_FILE_RESOURCE =
+      "WideRow/5000TablePerDBIT/spanner-schema.sql";
 
   @Before
   public void setUp() {
@@ -63,92 +66,17 @@ public class MySQLToSpanner5000TablePerDBIT extends SourceDbToSpannerITBase {
     ResourceManagerUtils.cleanResources(spannerResourceManager, mySQLResourceManager);
   }
 
-  private void configureMySQL() {
-    try {
-      // Increase packet size for large queries
-      String allowedGlobalPacket = "SET GLOBAL max_allowed_packet = " + MAX_ALLOWED_PACKET;
-      mySQLResourceManager.runSQLUpdate(allowedGlobalPacket);
-
-      // Additional MySQL optimizations
-      mySQLResourceManager.runSQLUpdate("SET GLOBAL innodb_buffer_pool_size = 512M");
-      mySQLResourceManager.runSQLUpdate("SET GLOBAL innodb_flush_log_at_trx_commit = 2");
-      mySQLResourceManager.runSQLUpdate("SET GLOBAL sync_binlog = 0");
-    } catch (Exception e) {
-      LOG.warn("Failed to configure MySQL settings: {}", e.getMessage(), e);
-      // Continue with test as some settings might not be configurable in certain environments
-    }
-  }
-
-  private void createMySQLSchema() {
-    LOG.info("Creating {} MySQL tables in batches of {}", NUM_TABLES, BATCH_SIZE);
-
-    // Create tables in batches to avoid overwhelming the MySQL server
-    for (int batchStart = 0; batchStart < NUM_TABLES; batchStart += BATCH_SIZE) {
-      int batchEnd = Math.min(batchStart + BATCH_SIZE, NUM_TABLES);
-      StringBuilder batchSQL = new StringBuilder();
-
-      for (int i = batchStart; i < batchEnd; i++) {
-        batchSQL.append("CREATE TABLE IF NOT EXISTS table").append(i).append(" (");
-        batchSQL.append(COL_ID + " INT PRIMARY KEY, ");
-        batchSQL.append(COL_NAME + " VARCHAR(255))");
-        batchSQL.append(";");
-      }
-
-      try {
-        LOG.info("Creating MySQL tables batch {}-{}", batchStart, batchEnd - 1);
-        loadSQLToJdbcResourceManager(mySQLResourceManager, batchSQL.toString());
-      } catch (Exception e) {
-        LOG.error(
-            "Error creating MySQL tables batch {}-{}: {}",
-            batchStart,
-            batchEnd - 1,
-            e.getMessage(),
-            e);
-        throw new RuntimeException("Failed to create MySQL tables", e);
-      }
-    }
-  }
-
-  private void createSpannerSchema() {
-    LOG.info("Creating {} Spanner tables in batches of {}", NUM_TABLES, BATCH_SIZE);
-
-    // Create tables in batches to avoid overwhelming Spanner
-    for (int batchStart = 0; batchStart < NUM_TABLES; batchStart += BATCH_SIZE) {
-      int batchEnd = Math.min(batchStart + BATCH_SIZE, NUM_TABLES);
-      StringBuilder batchSQL = new StringBuilder();
-
-      for (int i = batchStart; i < batchEnd; i++) {
-        batchSQL.append("CREATE TABLE table").append(i).append(" (");
-        batchSQL.append(COL_ID + " INT64, ");
-        batchSQL.append(COL_NAME + " STRING(MAX)) PRIMARY KEY (").append(COL_ID).append(")");
-        batchSQL.append(";");
-      }
-
-      try {
-        LOG.info("Creating Spanner tables batch {}-{}", batchStart, batchEnd - 1);
-        spannerResourceManager.executeDdlStatement(batchSQL.toString());
-      } catch (Exception e) {
-        LOG.error(
-            "Error creating Spanner tables batch {}-{}: {}",
-            batchStart,
-            batchEnd - 1,
-            e.getMessage(),
-            e);
-        throw new RuntimeException("Failed to create Spanner tables", e);
-      }
-    }
+  private void increasePacketSize() {
+    String allowedGlobalPacket = "SET GLOBAL max_allowed_packet = " + MAX_ALLOWED_PACKET;
+    mySQLResourceManager.runSQLUpdate(allowedGlobalPacket);
   }
 
   @Test
   public void testMySQLToSpannerMigration() throws Exception {
-    // Configure MySQL for better performance
-    configureMySQL();
+    increasePacketSize();
+    loadSQLFileResource(mySQLResourceManager, MYSQL_DUMP_FILE_RESOURCE);
+    createSpannerDDL(spannerResourceManager, SPANNER_SCHEMA_FILE_RESOURCE);
 
-    // Create schemas in batches
-    createMySQLSchema();
-    createSpannerSchema();
-
-    // Launch the Dataflow job
     jobInfo =
         launchDataflowJob(
             getClass().getSimpleName(),
@@ -158,18 +86,7 @@ public class MySQLToSpanner5000TablePerDBIT extends SourceDbToSpannerITBase {
             spannerResourceManager,
             null,
             null);
-
     PipelineOperator.Result result = pipelineOperator().waitUntilDone(createConfig(jobInfo));
     assertThatResult(result).isLaunchFinished();
-
-    // Verify the results
-    LOG.info("Verifying data migration for {} tables", NUM_TABLES);
-    for (int i = 0; i < NUM_TABLES; i++) {
-      String tableName = "table" + i;
-      assertEquals(
-          "Table " + tableName + " migrated",
-          0,
-          spannerResourceManager.getRowCount(tableName).longValue());
-    }
   }
 }
