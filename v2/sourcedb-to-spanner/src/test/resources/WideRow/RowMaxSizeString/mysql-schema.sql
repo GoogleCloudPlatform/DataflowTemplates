287ffb1fde42f4ec3cb4935137c62f2f30ea1e89
Akash Thawait <aakash@ollion.com>
2025-04-01 10:28:28 +0530

    fix code

--- v2/sourcedb-to-spanner/src/test/resources/WideRow/RowMaxSizeString/mysql-schema.sql
+++ v2/sourcedb-to-spanner/src/test/resources/WideRow/RowMaxSizeString/mysql-schema.sql
@@ -1,12 +1,7 @@
--- MySQL schema for testing very large string migration
 CREATE TABLE WideRowTable (
   id INT PRIMARY KEY,
   max_string_col MEDIUMTEXT
 );
 
--- Insert a row with normal size data that should succeed
 INSERT INTO WideRowTable (id, max_string_col) VALUES (1, REPEAT('a', 1000000));
-
--- Insert a row with string exceeding 10MB which should cause failure when migrating to Spanner
--- 11MB string (exceeds Spanner's 10MB limit)
 INSERT INTO WideRowTable (id, max_string_col) VALUES (2, REPEAT('b', 11000000));
