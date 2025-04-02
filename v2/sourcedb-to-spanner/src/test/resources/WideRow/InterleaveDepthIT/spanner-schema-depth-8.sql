75e32fcd532924760785d8137643dfd4d90a3753
Akash Thawait <aakash@ollion.com>
2025-04-02 09:06:05 +0530

    fix interleave depth 8

--- v2/sourcedb-to-spanner/src/test/resources/WideRow/InterleaveDepthIT/spanner-schema-depth-8.sql
+++ v2/sourcedb-to-spanner/src/test/resources/WideRow/InterleaveDepthIT/spanner-schema-depth-8.sql
@@ -1,85 +1,93 @@
-CREATE TABLE parent (
-	id INT64 NOT NULL,
-	name STRING(255) NOT NULL
+CREATE TABLE new_parent (
+    id INT64 NOT NULL,
+    name STRING(255) NOT NULL
 ) PRIMARY KEY (id);
 
-CREATE TABLE child1 (
-	id INT64 NOT NULL,
-	parent_id INT64 NOT NULL,
-	name STRING(255) NOT NULL
+CREATE TABLE new_child1 (
+    id INT64 NOT NULL,
+    parent_id INT64 NOT NULL,
+    name STRING(255) NOT NULL
 ) PRIMARY KEY (id);
 
-CREATE INDEX parent_id ON child1 (parent_id);
+CREATE INDEX new_parent_id ON new_child1 (parent_id);
 
-ALTER TABLE child1 ADD CONSTRAINT child1_ibfk_1 FOREIGN KEY (parent_id) REFERENCES parent (id) ON DELETE CASCADE;
+ALTER TABLE new_child1 ADD CONSTRAINT new_child1_ibfk_1 FOREIGN KEY (parent_id)
+    REFERENCES new_parent (id) ON DELETE CASCADE;
 
-CREATE TABLE child2 (
-	id INT64 NOT NULL,
-	child1_id INT64 NOT NULL,
-	name STRING(255) NOT NULL
+CREATE TABLE new_child2 (
+    id INT64 NOT NULL,
+    child1_id INT64 NOT NULL,
+    name STRING(255) NOT NULL
 ) PRIMARY KEY (id);
 
-CREATE INDEX child1_id ON child2 (child1_id);
+CREATE INDEX new_child1_id ON new_child2 (child1_id);
 
-ALTER TABLE child2 ADD CONSTRAINT child2_ibfk_1 FOREIGN KEY (child1_id) REFERENCES child1 (id) ON DELETE CASCADE;
+ALTER TABLE new_child2 ADD CONSTRAINT new_child2_ibfk_1 FOREIGN KEY (child1_id)
+    REFERENCES new_child1 (id) ON DELETE CASCADE;
 
-CREATE TABLE child3 (
-	id INT64 NOT NULL,
-	child2_id INT64 NOT NULL,
-	name STRING(255) NOT NULL
+CREATE TABLE new_child3 (
+    id INT64 NOT NULL,
+    child2_id INT64 NOT NULL,
+    name STRING(255) NOT NULL
 ) PRIMARY KEY (id);
 
-CREATE INDEX child2_id ON child3 (child2_id);
+CREATE INDEX new_child2_id ON new_child3 (child2_id);
 
-ALTER TABLE child3 ADD CONSTRAINT child3_ibfk_1 FOREIGN KEY (child2_id) REFERENCES child2 (id) ON DELETE CASCADE;
+ALTER TABLE new_child3 ADD CONSTRAINT new_child3_ibfk_1 FOREIGN KEY (child2_id)
+    REFERENCES new_child2 (id) ON DELETE CASCADE;
 
-CREATE TABLE child4 (
-	id INT64 NOT NULL,
-	child3_id INT64 NOT NULL,
-	name STRING(255) NOT NULL
+CREATE TABLE new_child4 (
+    id INT64 NOT NULL,
+    child3_id INT64 NOT NULL,
+    name STRING(255) NOT NULL
 ) PRIMARY KEY (id);
 
-CREATE INDEX child3_id ON child4 (child3_id);
+CREATE INDEX new_child3_id ON new_child4 (child3_id);
 
-ALTER TABLE child4 ADD CONSTRAINT child4_ibfk_1 FOREIGN KEY (child3_id) REFERENCES child3 (id) ON DELETE CASCADE;
+ALTER TABLE new_child4 ADD CONSTRAINT new_child4_ibfk_1 FOREIGN KEY (child3_id)
+    REFERENCES new_child3 (id) ON DELETE CASCADE;
 
-CREATE TABLE child5 (
-	id INT64 NOT NULL,
-	child4_id INT64 NOT NULL,
-	name STRING(255) NOT NULL
+CREATE TABLE new_child5 (
+    id INT64 NOT NULL,
+    child4_id INT64 NOT NULL,
+    name STRING(255) NOT NULL
 ) PRIMARY KEY (id);
 
-CREATE INDEX child4_id ON child5 (child4_id);
+CREATE INDEX new_child4_id ON new_child5 (child4_id);
 
-ALTER TABLE child5 ADD CONSTRAINT child5_ibfk_1 FOREIGN KEY (child4_id) REFERENCES child4 (id) ON DELETE CASCADE;
+ALTER TABLE new_child5 ADD CONSTRAINT new_child5_ibfk_1 FOREIGN KEY (child4_id)
+    REFERENCES new_child4 (id) ON DELETE CASCADE;
 
-CREATE TABLE child6 (
-	id INT64 NOT NULL,
-	child5_id INT64 NOT NULL,
-	name STRING(255) NOT NULL
+CREATE TABLE new_child6 (
+    id INT64 NOT NULL,
+    child5_id INT64 NOT NULL,
+    name STRING(255) NOT NULL
 ) PRIMARY KEY (id);
 
-CREATE INDEX child5_id ON child6 (child5_id);
+CREATE INDEX new_child5_id ON new_child6 (child5_id);
 
-ALTER TABLE child6 ADD CONSTRAINT child6_ibfk_1 FOREIGN KEY (child5_id) REFERENCES child5 (id) ON DELETE CASCADE;
+ALTER TABLE new_child6 ADD CONSTRAINT new_child6_ibfk_1 FOREIGN KEY (child5_id)
+    REFERENCES new_child5 (id) ON DELETE CASCADE;
 
-CREATE TABLE child7 (
-	id INT64 NOT NULL,
-	child6_id INT64 NOT NULL,
-	name STRING(255) NOT NULL
+CREATE TABLE new_child7 (
+    id INT64 NOT NULL,
+    child6_id INT64 NOT NULL,
+    name STRING(255) NOT NULL
 ) PRIMARY KEY (id);
 
-CREATE INDEX child6_id ON child7 (child6_id);
+CREATE INDEX new_child6_id ON new_child7 (child6_id);
 
-ALTER TABLE child7 ADD CONSTRAINT child7_ibfk_1 FOREIGN KEY (child6_id) REFERENCES child6 (id) ON DELETE CASCADE;
+ALTER TABLE new_child7 ADD CONSTRAINT new_child7_ibfk_1 FOREIGN KEY (child6_id)
+    REFERENCES new_child6 (id) ON DELETE CASCADE;
 
--- New table: child8, referencing child7.
-CREATE TABLE child8 (
-	id INT64 NOT NULL,
-	child7_id INT64 NOT NULL,
-	name STRING(255) NOT NULL
+-- New table: new_child8, referencing new_child7.
+CREATE TABLE new_child8 (
+    id INT64 NOT NULL,
+    child7_id INT64 NOT NULL,
+    name STRING(255) NOT NULL
 ) PRIMARY KEY (id);
 
-CREATE INDEX child7_id ON child8 (child7_id);
+CREATE INDEX new_child7_id ON new_child8 (child7_id);
 
-ALTER TABLE child8 ADD CONSTRAINT child8_ibfk_1 FOREIGN KEY (child7_id) REFERENCES child7 (id) ON DELETE CASCADE;
+ALTER TABLE new_child8 ADD CONSTRAINT new_child8_ibfk_1 FOREIGN KEY (child7_id)
+    REFERENCES new_child7 (id) ON DELETE CASCADE;
