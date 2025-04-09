eededc2bb15bfa8d43b96bf807e16d118e554410
Akash Thawait <aakash@ollion.com>
2025-04-09 19:54:53 +0530

    fix query issue

--- v2/sourcedb-to-spanner/src/test/resources/SourceDbToSpannerLT/WideRow/spanner-schema-10mib-per-cell.sql
+++ v2/sourcedb-to-spanner/src/test/resources/SourceDbToSpannerLT/WideRow/spanner-schema-10mib-per-cell.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS WideRowTable(
 id INT64 NOT NULL,
-max_bytes_col_1 BYTES(MAX)
-max_bytes_col_2 BYTES(MAX)
-max_bytes_col_3 BYTES(MAX)
-max_bytes_col_4 BYTES(MAX)
-max_bytes_col_5 BYTES(MAX)
+max_bytes_col_1 BYTES(MAX),
+max_bytes_col_2 BYTES(MAX),
+max_bytes_col_3 BYTES(MAX),
+max_bytes_col_4 BYTES(MAX),
+max_bytes_col_5 BYTES(MAX),
 max_bytes_col_6 BYTES(MAX)
 ) PRIMARY KEY (id)
