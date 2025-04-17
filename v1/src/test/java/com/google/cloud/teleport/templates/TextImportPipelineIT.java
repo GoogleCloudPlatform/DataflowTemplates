b7e46e09d39da6afe58de2b158921b6ce3755b9a
djagaluru <djagaluru@google.com>
2025-04-17 08:13:05 +0000

    Corrected the datatype of Integration test

--- v1/src/test/java/com/google/cloud/teleport/templates/TextImportPipelineIT.java
+++ v1/src/test/java/com/google/cloud/teleport/templates/TextImportPipelineIT.java
@@ -479,7 +479,7 @@ public final class TextImportPipelineIT extends SpannerTemplateITBase {
         "CREATE TABLE \"UuidTable\" (\n"
             + "  \"Key\"      uuid NOT NULL,\n"
             + "  \"Val1\"     uuid,\n"
-            + "  \"Val2\"     INT64,\n"
+            + "  \"Val2\"     bigint,\n"
             + "PRIMARY KEY (\"Key\"))");
     postgresResourceManager.executeDdlStatements(statements);
 
@@ -496,7 +496,7 @@ public final class TextImportPipelineIT extends SpannerTemplateITBase {
             + "      \"columns\": [\n"
             + "        {\"column_name\": \"Key\", \"type_name\": \"uuid\"},\n"
             + "        {\"column_name\": \"Val1\", \"type_name\": \"uuid\"},\n"
-            + "        {\"column_name\": \"Val2\", \"type_name\": \"INT64\"}\n"
+            + "        {\"column_name\": \"Val2\", \"type_name\": \"bigint\"}\n"
             + "      ]\n"
             + "    }\n"
             + "  ],\n"
