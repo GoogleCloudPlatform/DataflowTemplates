e7cce75b59c9ecdcfe72aa297e5c2d008ccf4d3a
Ron Gal <rongal@google.com>
2025-04-28 22:10:11 +0000

    fix encoding

--- v2/googlecloud-to-googlecloud/src/test/java/com/google/cloud/teleport/v2/templates/bigtablechangestreamstopubsub/BigtableChangeStreamsToPubSubIT.java
+++ v2/googlecloud-to-googlecloud/src/test/java/com/google/cloud/teleport/v2/templates/bigtablechangestreamstopubsub/BigtableChangeStreamsToPubSubIT.java
@@ -371,7 +371,7 @@ public final class BigtableChangeStreamsToPubSubIT extends TemplateTestBase {
             .setTieBreaker(1)
             .setTimestamp(timestamp)
             .setSourceTable(srcTable)
-            .setValue(goodValue)
+            .setValue(new String(goodValue, Charset.forName("ASCII")))
             .build();
 
     List<ReceivedMessage> receivedMessages = getAtLeastOneMessage(launchInfo);
