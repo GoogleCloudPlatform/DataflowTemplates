aefc636a174e284a05521130e03581c685402eb0
Ron Gal <rongal@google.com>
2025-05-01 18:29:08 +0000

    fix tests

--- v2/googlecloud-to-googlecloud/src/test/java/com/google/cloud/teleport/v2/templates/bigtablechangestreamstopubsub/schemautils/PubSubUtilsTest.java
+++ v2/googlecloud-to-googlecloud/src/test/java/com/google/cloud/teleport/v2/templates/bigtablechangestreamstopubsub/schemautils/PubSubUtilsTest.java
@@ -15,9 +15,13 @@
  */
 package com.google.cloud.teleport.v2.templates.bigtablechangestreamstopubsub.schemautils;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
+
 import com.google.cloud.bigtable.data.v2.models.ChangeStreamMutation.MutationType;
 import com.google.cloud.bigtable.data.v2.models.SetCell;
 import com.google.cloud.teleport.v2.templates.bigtablechangestreamstopubsub.model.BigtableSource;
+import com.google.cloud.teleport.v2.templates.bigtablechangestreamstopubsub.model.InvalidModException;
 import com.google.cloud.teleport.v2.templates.bigtablechangestreamstopubsub.model.MessageEncoding;
 import com.google.cloud.teleport.v2.templates.bigtablechangestreamstopubsub.model.MessageFormat;
 import com.google.cloud.teleport.v2.templates.bigtablechangestreamstopubsub.model.Mod;
@@ -27,15 +31,20 @@ import com.google.protobuf.ByteString;
 import com.google.pubsub.v1.PubsubMessage;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import org.joda.time.Instant;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /** Test class for {@link PubSubUtils}. */
 @RunWith(JUnit4.class)
 public class PubSubUtilsTest {
   static final Charset CHARSET = StandardCharsets.UTF_8;
+
+  private static final Logger LOG = LoggerFactory.getLogger(PubSubUtilsTest.class);
 
   // Pipeline specific variables to be used throughout testing of PubSubUtils
   static final String FAKE_PROJECT_ID = "fakeproject";
@@ -44,7 +53,7 @@ public class PubSubUtilsTest {
   static final String FAKE_TOPIC = "faketopic";
 
   @Test
-  public void testGetValidEntriesAllEntriesAreValid() {
+  public void testMapChangeJsonStringToPubSubMessageAsJsonValid() throws Exception {
     PubSubUtils utils = initPubSubUtils();
 
     SetCell setCell =
@@ -66,9 +75,45 @@ public class PubSubUtilsTest {
             setCell);
 
     Mod mod = new Mod(utils.getSource(), mutation, setCell);
-    String json = mod.toJson();
+    String json = mod.getChangeJson();
+    PubsubMessage message = utils.mapChangeJsonStringToPubSubMessageAsJson(json);
 
-    PubsubMessage actualEntries = utils.mapChangeJsonStringToPubSubMessageAsJson(json);
+    assertNotNull(message.getData());
+  }
+
+  @Test
+  public void testMapChangeJsonStringToPubSubMessageAsJsonInvalid() throws Exception {
+    PubSubUtils utils = initPubSubUtils();
+
+    SetCell setCell =
+        SetCell.create(
+            "test_column_family",
+            ByteString.copyFrom("test_column", CHARSET),
+            1000L, // timestamp
+            ByteString.copyFrom("test_value", CHARSET));
+
+    TestChangeStreamMutation mutation =
+        new TestChangeStreamMutation(
+            "test_rowkey",
+            MutationType.USER,
+            "source_cluster",
+            java.time.Instant.now(), // commit timestamp
+            1, // tiebreaker
+            "token",
+            java.time.Instant.now(), // low watermark
+            setCell);
+
+    Mod mod = new Mod(utils.getSource(), mutation, setCell);
+    String json = mod.getChangeJson();
+
+    String base64Value = Base64.getEncoder().encodeToString("test_value".getBytes());
+    String invalidJson = json.replace(",\"VALUE_BYTES\":\"" + base64Value + "\"", "");
+
+    assertThrows(
+        InvalidModException.class,
+        () -> {
+          PubsubMessage message = utils.mapChangeJsonStringToPubSubMessageAsJson(invalidJson);
+        });
   }
 
   private PubSubUtils initPubSubUtils() {
