1a06ce4f4f281410cf6bfb024dbbb17061ceac66
Deep1998 <deepchowdhury1998@gmail.com>
2025-07-02 15:50:26 +0530

    Add ITs

---
+++ v2/spanner-to-sourcedb/src/test/resources/SpannerToSourceDbOverridesIT/mysql-schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE source_table1 (
+    id_col1 INT PRIMARY KEY,
+    name_col1 VARCHAR(255),
+    data_col1 TEXT
+);
+
+CREATE TABLE source_table2 (
+    key_col2 VARCHAR(50) PRIMARY KEY,
+    category_col2 VARCHAR(100),
+    value_col2 TEXT
+); 
