8fe77a85b26956e0351b5d961774d6a0cdd9de5f
Shreya Khajanchi <khajanchi@google.com>
2025-07-29 14:29:17 +0530

    Exclude forward transaction from change streams

--- v2/spanner-to-sourcedb/src/test/resources/SpannerToSourceDbCustomShardIT/spanner-schema.sql
+++ v2/spanner-to-sourcedb/src/test/resources/SpannerToSourceDbCustomShardIT/spanner-schema.sql
@@ -6,5 +6,6 @@ CREATE TABLE IF NOT EXISTS Singers (
 CREATE CHANGE STREAM allstream
   FOR ALL OPTIONS (
   value_capture_type = 'NEW_ROW',
-  retention_period = '7d'
+  retention_period = '7d',
+  allow_txn_exclusion = true
 );
