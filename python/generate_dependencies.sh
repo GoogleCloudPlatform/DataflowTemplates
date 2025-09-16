8be19aa5767a4ea12dd13c384f5d3ecff73e65d6
liferoad <huxiangqian@gmail.com>
2025-09-16 09:31:03 -0400

    Update generate_dependencies.sh (#2777)
    * Update generate_dependencies.sh
    
    * Update generate_dependencies.sh
    
    * remove the version

--- python/generate_dependencies.sh
+++ python/generate_dependencies.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Copyright 2024 Google Inc. All Rights Reserved.
 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -27,8 +28,7 @@ fi
 
 set -ex
 
-PY_VERSION_NO_DOT=$(echo "$PY_VERSION" | tr -d '.')
-ENV_PATH="$PWD/__build__/python${PY_VERSION_NO_DOT}_requirements_gen"
+ENV_PATH="$PWD/__build__/python_requirements_gen"
 
 rm -rf "$ENV_PATH" 2>/dev/null || true
 # These python versions need to be kept in sync with our dockerfile python versions (https://github.com/GoogleCloudPlatform/DataflowTemplates/blob/0ac92513838ca525adb3f616c9e1f65237334d1e/plugins/core-plugin/src/main/java/com/google/cloud/teleport/plugin/DockerfileGenerator.java#L46)
@@ -53,7 +53,6 @@ pip freeze
 echo "Running pip-compile to generate hashes"
 pip-compile $BASE_REQUIREMENTS_PATH -o $TARGET_REQUIREMENTS_PATH --generate-hashes --allow-unsafe
 
-PY_IMAGE="py${PY_VERSION//.}"
 cat <<EOT > "$TARGET_REQUIREMENTS_PATH"
 # Copyright 2025 Google Inc. All Rights Reserved.
 
