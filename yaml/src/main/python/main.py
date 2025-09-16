93d4e98135cd5d49ced01dea861a60986f289117
Derrick Williams <derrickaw@google.com>
2025-09-16 14:23:36 +0000

    update main.py to handle the processing of jinja variables

--- yaml/src/main/python/main.py
+++ yaml/src/main/python/main.py
@@ -16,26 +16,126 @@
 
 import argparse
 import logging
+import json
+import pprint
 
 from apache_beam.io.filesystems import FileSystems
 from apache_beam.yaml import cache_provider_artifacts
 from apache_beam.yaml import main
 
+# These seem to be the common pipeline args that are not jinja pipeline
+# parameters.
+PIPELINE_ARGS = {
+    '--template_location',
+    '--service_account_email',
+    '--updateCompatibilityVersion',
+    '--runner',
+    '--job_name',
+    '--temp_location',
+    '--labels',
+    '--project',
+    '--region',
+    '--staging_location'
+}
+
+JINJA_ARG = '--jinja_variables'
+
+YAML_ARGS = {
+    '--yaml_pipeline',
+    JINJA_ARG
+}
 
 def _get_pipeline_yaml():
+    """Reads the pipeline definition from the 'template.yaml' file.
+
+    This function assumes that a 'template.yaml' file is present in the
+    current working directory of the execution environment. It opens the file,
+    reads its content, decodes it as a string, and returns it. 
+
+    Reminder - The 'template.yaml' file is just the renamed yaml 
+
+    Returns:
+        str: The content of the 'template.yaml' file as a string.
+    """
     with FileSystems.open("template.yaml") as fin:
         pipeline_yaml = fin.read().decode()
     return pipeline_yaml
 
-
 def run(argv=None):
+    """Runs a Beam YAML pipeline, processing command-line arguments for Jinja
+    templating.
+
+    This method orchestrates the execution of a Beam pipeline defined in
+    'template.yaml'. It parses command-line arguments, distinguishing between
+    standard pipeline options and those intended as Jinja templating variables.
+    It then prepares these variables and the YAML pipeline definition for
+    processing by `apache_beam.yaml.main.run`.
+
+    Args:
+        argv (list, optional): A list of command-line arguments. If None,
+            `sys.argv` is used. Defaults to None.
+
+    The method performs the following steps:
+    1. Determines if jinja argument present or not and branches off that.
+    2. Parses all command-line arguments.
+    3. Filters out standard pipeline arguments, treating the rest as Jinja
+    variables.
+    4. Reads the YAML pipeline definition from 'template.yaml'.
+    5. Constructs the final argument list for `apache_beam.yaml.main.run`.
+    6. Caches Beam YAML provider artifacts.
+    7. Executes the Beam pipeline.
+    """
     parser = argparse.ArgumentParser()
     _, pipeline_args = parser.parse_known_args(argv)
+    logging.info("Original pipeline args: \n%s\n", \
+                 pprint.pformat(pipeline_args,indent=2))
+    
+    if all([arg.split('=',1)[0] != JINJA_ARG for arg in pipeline_args]):
+        logging.info("Jinja variable parameter not found. Compiling individual parameters.")
+        # Filter out for only jinja args
+        # NOTE: If extra arguments get passed to the jinja variables, it is a
+        # no-op. This processing is just to keep things as clean as possible.
+        jinja_pipeline_args = \
+            [arg for arg in pipeline_args \
+             if arg.split('=',1)[0] not in PIPELINE_ARGS]
+        logging.info("Jinja pipeline args: \n%s\n", \
+                     pprint.pformat(jinja_pipeline_args, indent=2))
+
+        # Remove jinja pipeline args from pipeline args
+        pipeline_args = \
+            [arg for arg in pipeline_args \
+             if arg.split('=',1)[0] in PIPELINE_ARGS]
+        logging.info("Pipeline args without jinja args: \n%s\n", \
+                     pprint.pformat(pipeline_args,indent=2))
+
+        # Process args as key value pairs for later jinja processing.
+        jinja_vars = {}    
+        for arg in jinja_pipeline_args:
+            arg_key_value = arg.strip('--').split('=')
+            jinja_vars[arg_key_value[0]] = arg_key_value[1]
+        logging.info("Jinja variables: \n%s\n", \
+                     pprint.pformat(jinja_vars,indent=2))
+
+        # Save jinja vars as pipeline_args command. In theory, there should be
+        # at least one unless there are no manadatory arguments.
+        if jinja_vars:
+            jinja_vars_output =  [f'--jinja_variables={json.dumps(jinja_vars)}']
+            pipeline_args += jinja_vars_output
+
+    else:
+        logging.info("Jinja variable found and will override all other" \
+        "pipeline arguments if present.")
+
+    # Save the pipeline yaml template to the appropriate pipeline option
     pipeline_args += [f'--yaml_pipeline={_get_pipeline_yaml()}']
+    logging.info("Final pipeline args: \n%s\n", \
+                 pprint.pformat(pipeline_args,indent=2))
 
+    # Cache provider artifacts
     cache_provider_artifacts.cache_provider_artifacts()
-    main.run(argv=pipeline_args)
 
+    # Run with updated pipeline args
+    main.run(argv=pipeline_args)
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
