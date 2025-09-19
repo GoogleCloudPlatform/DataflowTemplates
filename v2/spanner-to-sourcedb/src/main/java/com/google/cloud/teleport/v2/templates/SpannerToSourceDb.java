2e71b0e2f00db1c426575f0eca74a1c1471cf29c
Aditya <50752534+bharadwaj-aditya@users.noreply.github.com>
2025-09-19 20:48:03 +0530

    CPU priority in reverse replication jobs (#2795)
    * updated CPU priority to HIGH for reverse replication jobs
    
    * taken priority as option
    
    * spotless check
    
    ---------
    
    Co-authored-by: Aditya Bharadwaj <adibh@google.com>

--- v2/spanner-to-sourcedb/src/main/java/com/google/cloud/teleport/v2/templates/SpannerToSourceDb.java
+++ v2/spanner-to-sourcedb/src/main/java/com/google/cloud/teleport/v2/templates/SpannerToSourceDb.java
@@ -21,6 +21,7 @@ import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.core.CqlSessionBuilder;
 import com.datastax.oss.driver.api.core.config.DriverConfigLoader;
 import com.google.cloud.Timestamp;
+import com.google.cloud.spanner.Options.RpcPriority;
 import com.google.cloud.teleport.metadata.Template;
 import com.google.cloud.teleport.metadata.TemplateCategory;
 import com.google.cloud.teleport.metadata.TemplateParameter;
@@ -503,6 +504,23 @@ public class SpannerToSourceDb {
     String getFailureInjectionParameter();
 
     void setFailureInjectionParameter(String value);
+
+    @TemplateParameter.Enum(
+        order = 34,
+        enumOptions = {
+          @TemplateEnumOption("LOW"),
+          @TemplateEnumOption("MEDIUM"),
+          @TemplateEnumOption("HIGH")
+        },
+        optional = true,
+        description = "Priority for Spanner RPC invocations",
+        helpText =
+            "The request priority for Cloud Spanner calls. The value must be one of:"
+                + " [`HIGH`,`MEDIUM`,`LOW`]. Defaults to `HIGH`.")
+    @Default.Enum("HIGH")
+    RpcPriority getSpannerPriority();
+
+    void setSpannerPriority(RpcPriority value);
   }
 
   /**
@@ -565,7 +583,8 @@ public class SpannerToSourceDb {
         SpannerConfig.create()
             .withProjectId(ValueProvider.StaticValueProvider.of(options.getSpannerProjectId()))
             .withInstanceId(ValueProvider.StaticValueProvider.of(options.getInstanceId()))
-            .withDatabaseId(ValueProvider.StaticValueProvider.of(options.getDatabaseId()));
+            .withDatabaseId(ValueProvider.StaticValueProvider.of(options.getDatabaseId()))
+            .withRpcPriority(ValueProvider.StaticValueProvider.of(options.getSpannerPriority()));
 
     // Create shadow tables
     // Note that there is a limit on the number of tables that can be created per DB: 5000.
@@ -578,7 +597,8 @@ public class SpannerToSourceDb {
         SpannerConfig.create()
             .withProjectId(ValueProvider.StaticValueProvider.of(options.getSpannerProjectId()))
             .withInstanceId(ValueProvider.StaticValueProvider.of(options.getMetadataInstance()))
-            .withDatabaseId(ValueProvider.StaticValueProvider.of(options.getMetadataDatabase()));
+            .withDatabaseId(ValueProvider.StaticValueProvider.of(options.getMetadataDatabase()))
+            .withRpcPriority(ValueProvider.StaticValueProvider.of(options.getSpannerPriority()));
 
     ShadowTableCreator shadowTableCreator =
         new ShadowTableCreator(
