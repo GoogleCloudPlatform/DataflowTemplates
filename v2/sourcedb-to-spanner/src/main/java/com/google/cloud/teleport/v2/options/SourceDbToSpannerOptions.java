9820686a8ba89d2b501896d05bdabbb243241be7
sm745052 <79754058+sm745052@users.noreply.github.com>
2025-10-28 13:02:32 +0530

    Exposing spannerio maxCommitDelay as a DF job parameter. (#2928)
    set maxCommitDelay=-1 to use the default setting
    
    ---------
    
    Co-authored-by: Sandeep Mishra <iamsandeep@google.com>

--- v2/sourcedb-to-spanner/src/main/java/com/google/cloud/teleport/v2/options/SourceDbToSpannerOptions.java
+++ v2/sourcedb-to-spanner/src/main/java/com/google/cloud/teleport/v2/options/SourceDbToSpannerOptions.java
@@ -415,4 +415,18 @@ public interface SourceDbToSpannerOptions extends CommonTemplateOptions {
   String getFailureInjectionParameter();
 
   void setFailureInjectionParameter(String value);
+
+  @TemplateParameter.Text(
+      order = 33,
+      optional = true,
+      description =
+          "Maximum commit delay time (in milliseconds) to optimize write throughput in Spanner. Reference https://cloud.google.com/spanner/docs/throughput-optimized-writes",
+      helpText =
+          "Maximum commit delay time to optimize write throughput in Spanner. Reference https://cloud.google.com/spanner/docs/throughput-optimized-writes."
+              + "Set -1 to let spanner choose the default. Set to a positive value to override for best suited tradeoff of throughput vs latency."
+              + "Defaults to -1.")
+  @Default.Long(-1)
+  Long getMaxCommitDelay();
+
+  void setMaxCommitDelay(Long value);
 }
