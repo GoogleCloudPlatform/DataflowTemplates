3dc2365dabf3d397bdfacd1baef5585466486553
Darshan Siddesh Jagaluru <djagaluru@google.com>
2025-11-03 15:15:53 +0000

    Reducing number of MySQL tests to test the container issue

--- v2/sourcedb-to-spanner/src/test/java/com/google/cloud/teleport/v2/templates/MySQLFileOverridesSchemaMapperIT.java
+++ v2/sourcedb-to-spanner/src/test/java/com/google/cloud/teleport/v2/templates/MySQLFileOverridesSchemaMapperIT.java
@@ -32,6 +32,7 @@ import org.apache.beam.it.gcp.storage.GcsResourceManager;
 import org.apache.beam.it.jdbc.MySQLResourceManager;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
@@ -47,6 +48,7 @@ import org.testcontainers.shaded.com.google.common.io.Resources;
 @Category({TemplateIntegrationTest.class, SkipDirectRunnerTest.class})
 @TemplateIntegrationTest(SourceDbToSpanner.class)
 @RunWith(JUnit4.class)
+@Ignore("Disabling to test MySQL container issues.")
 public class MySQLFileOverridesSchemaMapperIT extends SourceDbToSpannerITBase {
   private static final Logger LOG = LoggerFactory.getLogger(MySQLFileOverridesSchemaMapperIT.class);
   private static final HashSet<MySQLFileOverridesSchemaMapperIT> testInstances = new HashSet<>();
--- v2/sourcedb-to-spanner/src/test/java/com/google/cloud/teleport/v2/templates/MySQLReservedKeywordsIT.java
+++ v2/sourcedb-to-spanner/src/test/java/com/google/cloud/teleport/v2/templates/MySQLReservedKeywordsIT.java
@@ -27,6 +27,7 @@ import org.apache.beam.it.gcp.spanner.matchers.SpannerAsserts;
 import org.apache.beam.it.jdbc.MySQLResourceManager;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
@@ -39,6 +40,7 @@ import org.junit.runners.JUnit4;
 @Category({TemplateIntegrationTest.class, SkipDirectRunnerTest.class})
 @TemplateIntegrationTest(SourceDbToSpanner.class)
 @RunWith(JUnit4.class)
+@Ignore("Disabling to test MySQL container issues.")
 public class MySQLReservedKeywordsIT extends SourceDbToSpannerITBase {
   private static PipelineLauncher.LaunchInfo jobInfo;
 
