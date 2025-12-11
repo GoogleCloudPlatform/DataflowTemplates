c3d62d39190e072d32fd6cbb749171a1dedba404
Derrick Williams <derrickaw@google.com>
2025-12-11 15:58:53 +0000

    first draft of it file

---
+++ yaml/src/test/java/com/google/cloud/teleport/templates/yaml/PubSubToBigTableYamlIT.java
@@ -0,0 +1,241 @@
+/*
+ * Copyright (C) 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.cloud.teleport.templates.yaml;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.apache.beam.it.truthmatchers.PipelineAsserts.assertThatPipeline;
+import static org.apache.beam.it.truthmatchers.PipelineAsserts.assertThatResult;
+
+import com.google.cloud.bigtable.data.v2.models.Row;
+import com.google.cloud.teleport.metadata.SkipDirectRunnerTest;
+import com.google.cloud.teleport.metadata.TemplateIntegrationTest;
+import com.google.common.collect.ImmutableList;
+import com.google.protobuf.ByteString;
+import com.google.pubsub.v1.TopicName;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import org.apache.beam.it.common.PipelineLauncher;
+import org.apache.beam.it.common.PipelineOperator;
+import org.apache.beam.it.common.utils.ResourceManagerUtils;
+import org.apache.beam.it.gcp.TemplateTestBase;
+import org.apache.beam.it.gcp.bigtable.BigtableResourceManager;
+import org.apache.beam.it.gcp.pubsub.PubsubResourceManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// import com.google.pubsub.v1.SubscriptionName;
+// import org.apache.beam.it.gcp.pubsub.conditions.PubsubMessagesCheck;
+
+@Category({TemplateIntegrationTest.class, SkipDirectRunnerTest.class})
+@TemplateIntegrationTest(PubSubToBigTableYaml.class)
+@RunWith(JUnit4.class)
+public final class PubSubToBigTableYamlIT extends TemplateTestBase {
+
+  private static final Logger LOG = LoggerFactory.getLogger(PubSubToBigTableYamlIT.class);
+
+  private PubsubResourceManager pubsubResourceManager;
+  private BigtableResourceManager bigtableResourceManager;
+
+  @Before
+  public void setup() throws IOException {
+    // Pubsub and BigTable resource managers
+    pubsubResourceManager =
+        PubsubResourceManager.builder(testName, PROJECT, credentialsProvider).build();
+    bigtableResourceManager =
+        BigtableResourceManager.builder(testName, PROJECT, credentialsProvider).build();
+  }
+
+  @After
+  public void tearDown() {
+    // Clean up resource managers
+    ResourceManagerUtils.cleanResources(pubsubResourceManager, bigtableResourceManager);
+  }
+
+  @Test
+  public void testPubSubToBigTable() throws IOException {
+    pubSubToBigTable(Function.identity()); // no extra parameters
+  }
+
+  public void pubSubToBigTable(
+      Function<PipelineLauncher.LaunchConfig.Builder, PipelineLauncher.LaunchConfig.Builder>
+          paramsAdder)
+      throws IOException {
+
+    /******************************* Arrange ********************************/
+
+    // Create main and dead letter queue topics
+    TopicName topic = pubsubResourceManager.createTopic("input");
+    TopicName dlqTopic = pubsubResourceManager.createTopic("dlq");
+
+    // Create Bigtable table
+    String tableId = "test_table";
+    bigtableResourceManager.createTable(tableId, ImmutableList.of("cf"));
+
+    // Create launch config with the yaml pipeline parameters
+    PipelineLauncher.LaunchConfig.Builder options =
+        paramsAdder.apply(
+            PipelineLauncher.LaunchConfig.builder(testName, specPath)
+                .addParameter("topic", topic.toString())
+                .addParameter("format", "JSON")
+                // .addParameter("schema",
+                // "{\"type\":\"object\",\"properties\":{\"key\":{\"type\":\"string\"},\"type\":{\"type\":\"string\"},\"family_name\":{\"type\":\"string\"},\"column_qualifier\":{\"type\":\"string\"},\"value\":{\"type\":\"string\"},\"timestamp_micros\":{\"type\":\"integer\"}},\"required\":[\"key\",\"type\",\"family_name\",\"column_qualifier\",\"value\"]}")
+                .addParameter(
+                    "schema",
+                    "{\"type\":\"object\",\"properties\":{\"key\":\"string\",\"type\":\"string\",\"family_name\":\"string\",\"column_qualifier\":\"string\",\"value\":\"string\",\"timestamp_micros\":\"integer\"})")
+                // .addParameter(
+                //     "schema",
+                //
+                // "{\"type\":\"object\",\"properties\":{\"id\":{\"type\":\"integer\"},\"name\":{\"type\":\"string\"}}}")
+                .addParameter("table_id", tableId)
+                .addParameter("instance_id", bigtableResourceManager.getInstanceId())
+                .addParameter("project_id", PROJECT)
+                .addParameter("language", "generic")
+                .addParameter(
+                    "fields",
+                    "[{\"name\": \"rowkey\", \"value\": \"id\"}, {\"name\": \"cf:name\", \"value\": \"name\"}]")
+                .addParameter("outputDeadLetterPubSubTopic", dlqTopic.toString()));
+
+    /********************************* Act **********************************/
+
+    // Launch pipeline and assert running
+    PipelineLauncher.LaunchInfo info = launchTemplate(options);
+    assertThatPipeline(info).isRunning();
+
+    // Publish messages to the topic while the pipeline is running
+    List<Map<String, Object>> expectedSuccessRecords = new ArrayList<>();
+    List<ByteString> expectedFailureRecords = new ArrayList<>();
+    for (int i = 1; i <= 10; i++) {
+      // Valid schema
+      long id1 = Long.parseLong(i + "1");
+      long id2 = Long.parseLong(i + "2");
+      pubsubResourceManager.publish(
+          topic,
+          null,
+          ByteString.copyFromUtf8(
+              "{\"key\": \"row"
+                  + id1
+                  + "\", \"type\": \"SetCell\", \"family_name\": \"cf1\", \"column_qualifier\": \"cq1\", \"value\": \"value1\", \"timestamp_micros\": 5000}"));
+      pubsubResourceManager.publish(
+          topic,
+          null,
+          ByteString.copyFromUtf8(
+              "{\"key\": \"row"
+                  + id2
+                  + "\", \"type\": \"SetCell\", \"family_name\": \"cf1\", \"column_qualifier\": \"cq2\", \"value\": \"value2\", \"timestamp_micros\": 1000}"));
+      expectedSuccessRecords.add(
+          Map.of(
+              "key",
+              "row" + id1,
+              "type",
+              "SetCell",
+              "family_name",
+              "cf1",
+              "column_qualifier",
+              "cq1",
+              "value",
+              "value1",
+              "timestamp_micros",
+              5000));
+      expectedSuccessRecords.add(
+          Map.of(
+              "key",
+              "row" + id2,
+              "type",
+              "SetCell",
+              "family_name",
+              "cf1",
+              "column_qualifier",
+              "cq2",
+              "value",
+              "value2",
+              "timestamp_micros",
+              1000));
+
+      // Invalid schema
+      String invalidRow =
+          "{\"key\": 123, \"type\": \"SetCell\", \"family_name\": \"cf1\", \"column_qualifier\": \"cq-invalid\", \"value\": \"invalid-value\", \"timestamp_micros\": 0}";
+      pubsubResourceManager.publish(topic, null, ByteString.copyFromUtf8(invalidRow));
+      expectedFailureRecords.add(ByteString.copyFromUtf8(invalidRow));
+
+      try {
+        TimeUnit.SECONDS.sleep(3);
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+      }
+    }
+
+    // Add check that 10 dlq messages were received
+    // PubsubMessagesCheck dlqCheck =
+    //     PubsubMessagesCheck.builder(pubsubResourceManager, dlqTopic, "dlq-topic")
+    //         .setMinMessages(10)
+    //         .build();
+
+    // Add check that 20 messages were written to the table
+
+    // PipelineOperator.Result result =
+    // pipelineOperator().waitForConditionAndFinish(createConfig(info), dlqCheck);
+
+    PipelineOperator.Result result =
+        pipelineOperator()
+            .waitForConditionAndFinish(
+                createConfig(info),
+                // dlqCheck,
+                () -> bigtableResourceManager.readTable(tableId).size() >= 20);
+
+    /******************************** Assert ********************************/
+    assertThatResult(result).meetsConditions();
+
+    // Verify 20 rows in the table
+    List<Row> tableRows = bigtableResourceManager.readTable(tableId);
+    assertThat(tableRows).hasSize(20);
+
+    // Verify 20 rows are expected
+    Map<String, Row> rowMap =
+        tableRows.stream()
+            .collect(Collectors.toMap(row -> row.getKey().toStringUtf8(), row -> row));
+
+    for (int i = 1; i <= 10; i++) {
+      String key1 = "row" + i + "1";
+      assertThat(rowMap).containsKey(key1);
+      Row row1 = rowMap.get(key1);
+      assertThat(row1.getCells()).hasSize(1);
+      assertThat(row1.getCells().get(0).getFamily()).isEqualTo("cf1");
+      assertThat(row1.getCells().get(0).getQualifier().toStringUtf8()).isEqualTo("cq1");
+      assertThat(row1.getCells().get(0).getValue().toStringUtf8()).isEqualTo("value1");
+      assertThat(row1.getCells().get(0).getTimestamp()).isEqualTo(5000L);
+
+      String key2 = "row" + i + "2";
+      assertThat(rowMap).containsKey(key2);
+      Row row2 = rowMap.get(key2);
+      assertThat(row2.getCells()).hasSize(1);
+      assertThat(row2.getCells().get(0).getFamily()).isEqualTo("cf1");
+      assertThat(row2.getCells().get(0).getQualifier().toStringUtf8()).isEqualTo("cq2");
+      assertThat(row2.getCells().get(0).getValue().toStringUtf8()).isEqualTo("value2");
+      assertThat(row2.getCells().get(0).getTimestamp()).isEqualTo(1000L);
+    }
+  }
+}
