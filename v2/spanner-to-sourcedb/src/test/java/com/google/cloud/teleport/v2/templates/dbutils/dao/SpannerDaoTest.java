bf172e22e6f4f917d0bb2d9a5cb6bd2eeb47f63f
Aditya <50752534+bharadwaj-aditya@users.noreply.github.com>
2025-12-16 13:10:58 +0530

    fixed priority handling in assign shard id (#3110)
    * fixed priority handling in assign shrad id
    
    * fixed tests
    
    * fixed tests
    
    * fixed tests
    
    * moved result set to closeable block
    
    ---------
    
    Co-authored-by: Aditya Bharadwaj <adibh@google.com>

--- v2/spanner-to-sourcedb/src/test/java/com/google/cloud/teleport/v2/templates/dbutils/dao/SpannerDaoTest.java
+++ v2/spanner-to-sourcedb/src/test/java/com/google/cloud/teleport/v2/templates/dbutils/dao/SpannerDaoTest.java
@@ -15,24 +15,16 @@
  */
 package com.google.cloud.teleport.v2.templates.dbutils.dao;
 
-import static com.google.common.truth.Truth.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import com.google.cloud.Timestamp;
 import com.google.cloud.spanner.DatabaseClient;
-import com.google.cloud.spanner.Key;
 import com.google.cloud.spanner.Mutation;
 import com.google.cloud.spanner.ReadOnlyTransaction;
-import com.google.cloud.spanner.Struct;
 import com.google.cloud.spanner.TransactionContext;
-import com.google.cloud.teleport.v2.templates.constants.Constants;
 import com.google.cloud.teleport.v2.templates.dbutils.dao.spanner.SpannerDao;
-import com.google.cloud.teleport.v2.templates.utils.ShadowTableRecord;
 import org.apache.beam.sdk.io.gcp.spanner.SpannerAccessor;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
@@ -60,46 +52,6 @@ public final class SpannerDaoTest {
     when(mockSpannerAccessor.getDatabaseClient()).thenReturn(mockDatabaseClient);
     when(mockDatabaseClient.singleUse()).thenReturn(mockReadOnlyTransaction);
     doNothing().when(mockSpannerAccessor).close();
-  }
-
-  @Test
-  public void testGetShadowTableRecordReturnsNull() {
-    SpannerDao spannerDao = new SpannerDao(mockSpannerAccessor);
-    // Mock readRow
-    when(mockReadOnlyTransaction.readRow(eq("tableName"), any(Key.class), any(Iterable.class)))
-        .thenReturn(null);
-    assertThat(spannerDao.getShadowTableRecord("tableName", null)).isNull();
-  }
-
-  @Test
-  public void testGetShadowTableRecordReturnsRecord() {
-    SpannerDao spannerDao = new SpannerDao(mockSpannerAccessor);
-    Struct row =
-        Struct.newBuilder()
-            .set(Constants.PROCESSED_COMMIT_TS_COLUMN_NAME)
-            .to(Timestamp.parseTimestamp("2023-05-18T12:01:13.088397258Z"))
-            .set(Constants.RECORD_SEQ_COLUMN_NAME)
-            .to(1)
-            .build();
-
-    when(mockReadOnlyTransaction.readRow(eq("junk"), any(), any(Iterable.class))).thenReturn(row);
-    ShadowTableRecord response = spannerDao.getShadowTableRecord("junk", null);
-    spannerDao.close();
-
-    ShadowTableRecord expectedResponse =
-        new ShadowTableRecord(Timestamp.parseTimestamp("2023-05-18T12:01:13.088397258Z"), 1);
-    assertThat(response.getProcessedCommitTimestamp())
-        .isEqualTo(expectedResponse.getProcessedCommitTimestamp());
-    assertThat(response.getRecordSequence()).isEqualTo(expectedResponse.getRecordSequence());
-  }
-
-  @Test(expected = RuntimeException.class)
-  public void testGetShadowTableRecordException() {
-    SpannerDao spannerDao = new SpannerDao(mockSpannerAccessor);
-    doThrow(new RuntimeException("generic exception"))
-        .when(mockReadOnlyTransaction)
-        .readRow(eq("error"), any(), any(Iterable.class));
-    ShadowTableRecord response = spannerDao.getShadowTableRecord("error", null);
   }
 
   @Test
