723d31a9bfba10325690076309dfb13511d2a6fa
Pratick Chokhani <pratick.chokhani@gmail.com>
2026-01-28 09:57:07 +0530

    fix: Version mismatch in integration tests.

--- it/google-cloud-platform/src/main/java/org/apache/beam/it/gcp/JDBCBaseIT.java
+++ it/google-cloud-platform/src/main/java/org/apache/beam/it/gcp/JDBCBaseIT.java
@@ -57,8 +57,8 @@ public abstract class JDBCBaseIT extends TemplateTestBase {
   // NOTE: These versions must correspond to the versions declared in the `it/pom.xml` file.
   private static final String MYSQL_VERSION = "8.0.30";
   private static final String POSTGRES_VERSION = "42.6.1";
-  private static final String ORACLE_VERSION = "23.2.0.0";
-  private static final String MSSQL_VERSION = "12.2.0.jre11";
+  private static final String ORACLE_VERSION = "23.9.0.25.07";
+  private static final String MSSQL_VERSION = "13.2.1.jre11";
 
   @Before
   public void setUpJDBC() throws IOException {
