5f4662235592cdd7a87c4ed641e7e1ec8206bbb4
Lester Szeto <lester.szeto@gmail.com>
2026-01-29 11:04:50 -0800

    Increase test runner timeout

--- v2/sourcedb-to-spanner/src/test/java/com/google/cloud/teleport/v2/templates/PostgreSQLDataTypesPGDialectIT.java
+++ v2/sourcedb-to-spanner/src/test/java/com/google/cloud/teleport/v2/templates/PostgreSQLDataTypesPGDialectIT.java
@@ -88,7 +88,7 @@ public class PostgreSQLDataTypesPGDialectIT extends SourceDbToSpannerITBase {
             jobParameters,
             null);
     PipelineOperator.Result result =
-        pipelineOperator().waitUntilDone(createConfig(jobInfo, Duration.ofMinutes(30L)));
+        pipelineOperator().waitUntilDone(createConfig(jobInfo, Duration.ofMinutes(50L)));
     assertThatResult(result).isLaunchFinished();
 
     Map<String, List<Map<String, Object>>> expectedData = getExpectedData();
