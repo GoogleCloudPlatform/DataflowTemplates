98771bf099081447350ca5c655885fbf8b0d6248
Manit Gupta <manitgupta@google.com>
2026-02-11 16:29:49 +0530

    feat(dv): scaffold module, constants, and data transfer objects (DTOs)

---
+++ v2/gcs-spanner-dv/src/main/java/com/google/cloud/teleport/v2/dto/Column.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.cloud.teleport.v2.dto;
+
+import com.google.auto.value.AutoValue;
+import org.apache.beam.sdk.schemas.AutoValueSchema;
+import org.apache.beam.sdk.schemas.annotations.DefaultSchema;
+
+@AutoValue
+@DefaultSchema(AutoValueSchema.class)
+public abstract class Column {
+
+  public abstract String getColName();
+
+  public abstract String getColValue();
+
+  public static Builder builder() {
+    return new AutoValue_Column.Builder();
+  }
+
+  @AutoValue.Builder
+  public abstract static class Builder {
+    public abstract Builder setColName(String colName);
+
+    public abstract Builder setColValue(String colValue);
+
+    public abstract Column build();
+  }
+}
