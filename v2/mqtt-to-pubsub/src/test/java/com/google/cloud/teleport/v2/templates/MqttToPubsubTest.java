1b9d18474381e5c9b38d65f972f0889d69bffd2f
Svetak Sundhar <svetaksundhar@google.com>
2026-03-25 13:39:38 -0400

    Update MqttToPubsubTest.java

--- v2/mqtt-to-pubsub/src/test/java/com/google/cloud/teleport/v2/templates/MqttToPubsubTest.java
+++ v2/mqtt-to-pubsub/src/test/java/com/google/cloud/teleport/v2/templates/MqttToPubsubTest.java
@@ -69,4 +69,26 @@ public class MqttToPubsubTest {
     // Expected to not throw an exception:
     MqttToPubsub.validate(options);
   }
+
+  @Test
+  public void testValidationFailPubSubTopicAsMqttInput() {
+    MqttToPubsub.MqttToPubsubOptions options =
+        PipelineOptionsFactory.create().as(MqttToPubsub.MqttToPubsubOptions.class);
+    // Simulate a user accidentally pasting a Pub/Sub topic string
+    options.setInputTopic("projects/my-gcp-project/topics/my-pubsub-topic");
+    
+    assertThrows(IllegalArgumentException.class, () -> MqttToPubsub.validate(options));
+  }
+
+  @Test
+  public void testValidationSuccess() {
+    MqttToPubsub.MqttToPubsubOptions options =
+        PipelineOptionsFactory.create().as(MqttToPubsub.MqttToPubsubOptions.class);
+    options.setUsername("test");
+    options.setPassword("test");
+    options.setInputTopic("sensor/data/temperature"); // Added a valid MQTT topic
+
+    // Expected to not throw an exception:
+    MqttToPubsub.validate(options);
+  }
 }
