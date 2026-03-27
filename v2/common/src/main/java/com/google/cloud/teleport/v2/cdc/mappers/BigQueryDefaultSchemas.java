df0831524ea5c9cb34148c77156ddfe767c6c740
Yi Hu <huuyyi@gmail.com>
2026-03-27 15:14:48 -0400

    Revert "feat(Datastream): Add SQL Server (MSSQL) source support (#3396)"
    This reverts commit c208d43bef3bb21ecb30d169830d8c6f59feb7c8.

--- v2/common/src/main/java/com/google/cloud/teleport/v2/cdc/mappers/BigQueryDefaultSchemas.java
+++ v2/common/src/main/java/com/google/cloud/teleport/v2/cdc/mappers/BigQueryDefaultSchemas.java
@@ -58,9 +58,6 @@ public final class BigQueryDefaultSchemas {
           // MySQL Specific Metadata
           put("_metadata_log_file", StandardSQLTypeName.STRING);
           put("_metadata_log_position", StandardSQLTypeName.INT64);
-
-          // PostgreSQL / SQL Server Specific Metadata
-          put("_metadata_lsn", StandardSQLTypeName.STRING);
         }
       };
 }
