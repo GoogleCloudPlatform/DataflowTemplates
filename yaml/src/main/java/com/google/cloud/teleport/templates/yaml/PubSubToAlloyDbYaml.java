c3bd1124187957c066e16d6ba71e6d108e97be80
Ulada Butsenka <uladabutsenka@google.com>
2026-03-30 14:50:57 +0200

    Update PubSub to AlloyDb Yaml template parameters

--- yaml/src/main/java/com/google/cloud/teleport/templates/yaml/PubSubToAlloyDbYaml.java
+++ yaml/src/main/java/com/google/cloud/teleport/templates/yaml/PubSubToAlloyDbYaml.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2026 Google LLC
+ * Copyright (C) 2025 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -18,6 +18,7 @@ package com.google.cloud.teleport.templates.yaml;
 import com.google.cloud.teleport.metadata.Template;
 import com.google.cloud.teleport.metadata.TemplateCategory;
 import com.google.cloud.teleport.metadata.TemplateParameter;
+import org.apache.beam.sdk.options.Default;
 import org.apache.beam.sdk.options.Validation;
 
 @Template(
@@ -51,41 +52,32 @@ public interface PubSubToAlloyDbYaml {
   @TemplateParameter.Text(
       order = 1,
       name = "topic",
-      optional = true,
-      description = "Pub/Sub topic",
-      helpText =
-          "Pub/Sub topic to read the input from. Provide either topic or subscription, not both.",
+      optional = false,
+      description = "Pub/Sub input topic",
+      helpText = "Pub/Sub topic to read the input from.",
       example = "projects/your-project-id/topics/your-topic-name")
+  @Validation.Required
   String getTopic();
 
   @TemplateParameter.Text(
       order = 2,
-      name = "subscription",
-      optional = true,
-      description = "Pub/Sub subscription",
-      helpText =
-          "Pub/Sub subscription to read the input from. Provide either subscription or topic, not both.",
-      example = "projects/your-project-id/subscriptions/your-subscription-name")
-  String getSubscription();
-
-  @TemplateParameter.Text(
-      order = 3,
       name = "format",
       optional = false,
       description = "The message format.",
       helpText = "The message format. One of: AVRO, JSON, PROTO, RAW, or STRING.",
-      example = "JSON")
+      example = "")
   @Validation.Required
   String getFormat();
 
   @TemplateParameter.Text(
       order = 3,
       name = "schema",
-      optional = true,
+      optional = false,
       description = "Data schema.",
       helpText =
-          "A schema is required if data format is JSON, AVRO or PROTO. For JSON, this is a JSON schema. For AVRO and PROTO, this is the full schema definition.",
-      example = "fields:\n  - name: \"message_body\"\n    type: \"STRING\"")
+          "A schema is required if data format is JSON, AVRO or PROTO. For JSON,  this is a JSON schema. For AVRO and PROTO, this is the full schema  definition.",
+      example = "")
+  @Validation.Required
   String getSchema();
 
   @TemplateParameter.Text(
@@ -94,19 +86,19 @@ public interface PubSubToAlloyDbYaml {
       optional = true,
       description = "List of attribute keys.",
       helpText =
-          "List of attribute keys whose values will be flattened into the output message as additional fields.",
+          "List of attribute keys whose values will be flattened into the output message as additional fields.  For example, if the format is `raw` and attributes is `[a, b]` then this read will produce elements of the form `Row(payload=..., a=..., b=...)`.",
       example = "")
   String getAttributes();
 
   @TemplateParameter.Text(
       order = 5,
-      name = "attributeMap",
+      name = "attributesMap",
       optional = true,
       description = "Name of a field in which to store the full set of attributes.",
       helpText =
-          "Name of a field in which to store the full set of attributes associated with this message.",
+          "Name of a field in which to store the full set of attributes associated with this message.  For example, if the format is `raw` and `attribute_map` is set to `attrs` then this read will produce elements of the form `Row(payload=..., attrs=...)` where `attrs` is a Map type of string to string. If both `attributes` and `attribute_map` are set, the overlapping attribute values will be present in both the flattened structure and the attribute map.",
       example = "")
-  String getAttributeMap();
+  String getAttributesMap();
 
   @TemplateParameter.Text(
       order = 6,
@@ -115,7 +107,7 @@ public interface PubSubToAlloyDbYaml {
       description =
           "The attribute on incoming Pub/Sub messages to use as a unique record identifier.",
       helpText =
-          "The attribute on incoming Pub/Sub messages to use as a unique record identifier. When specified, the value of this attribute will be used for deduplication of messages.",
+          "The attribute on incoming Pub/Sub messages to use as a unique record identifier. When specified, the value of this attribute (which can be any string that uniquely identifies the record) will be used for deduplication of messages. If not provided, we cannot guarantee that no duplicate data will be delivered on the Pub/Sub stream. In this case, deduplication of the stream will be strictly best effort.",
       example = "")
   String getIdAttribute();
 
@@ -125,23 +117,41 @@ public interface PubSubToAlloyDbYaml {
       optional = true,
       description = "Message value to use as element timestamp.",
       helpText =
-          "Message value to use as element timestamp. If None, uses message publishing time as the timestamp.",
+          "Message value to use as element timestamp. If None, uses message  publishing time as the timestamp. Timestamp values should be in one of two formats: 1). A numerical value representing the number of milliseconds since the Unix epoch. 2). A string in RFC 3339 format, UTC timezone. Example: ``2015-10-29T23:41:41.123Z``. The sub-second component of the timestamp is optional, and digits beyond the first three (i.e., time units smaller than milliseconds) may be ignored.",
       example = "")
   String getTimestampAttribute();
 
   @TemplateParameter.Text(
       order = 8,
+      name = "errorHandling",
+      optional = true,
+      description = "Error handling configuration",
+      helpText = "This option specifies whether and where to output error rows.",
+      example = "")
+  String getErrorHandling();
+
+  @TemplateParameter.Text(
+      order = 9,
+      name = "subscription",
+      optional = true,
+      description = "Pub/Sub subscription",
+      helpText = "Pub/Sub subscription to read the input from.",
+      example = "projects/your-project-id/subscriptions/your-subscription-name")
+  String getSubscription();
+
+  @TemplateParameter.Text(
+      order = 10,
       name = "language",
       optional = false,
       description = "Language used to define the expressions.",
       helpText =
-          "The language used to define (and execute) the expressions and callables in fields.",
-      example = "python")
+          "The language used to define (and execute) the expressions and/or  callables in fields. Defaults to generic.",
+      example = "")
   @Validation.Required
   String getLanguage();
 
   @TemplateParameter.Text(
-      order = 9,
+      order = 11,
       name = "fields",
       optional = false,
       description = "Field mapping configuration",
@@ -152,29 +162,29 @@ public interface PubSubToAlloyDbYaml {
   String getFields();
 
   @TemplateParameter.Text(
-      order = 10,
+      order = 12,
       name = "windowing",
       optional = true,
       description = "Windowing options",
       helpText =
           "Windowing options - see https://beam.apache.org/documentation/sdks/yaml/#windowing",
-      example = "type: fixed\nsize: 60s")
+      example = "")
   String getWindowing();
 
   @TemplateParameter.Text(
-      order = 11,
+      order = 13,
       name = "url",
       optional = false,
       description = "JDBC connection URL for AlloyDb",
       helpText =
-          "Connection URL for the JDBC sink. Format: jdbc:postgresql:///db?socketFactory=com.google.cloud.alloydb.SocketFactory&alloydbInstanceName=projects/<PROJECT>/locations/<REGION>/clusters/<CLUSTER>/instances/<INSTANCE>&alloydbIpType=PRIVATE",
+          "The JDBC URL for connecting to AlloyDb instance. Format: jdbc:postgresql:///db?socketFactory=com.google.cloud.alloydb.SocketFactory&alloydbInstanceName=projects/<PROJECT>/locations/<REGION>/clusters/<CLUSTER>/instances/<INSTANCE>&alloydbIpType=PRIVATE",
       example =
           "jdbc:postgresql:///mydatabase?socketFactory=com.google.cloud.alloydb.SocketFactory&alloydbInstanceName=projects/my-project/locations/us-central1/clusters/my-cluster/instances/my-instance&alloydbIpType=PRIVATE")
   @Validation.Required
   String getUrl();
 
   @TemplateParameter.Text(
-      order = 12,
+      order = 14,
       name = "username",
       optional = false,
       description = "AlloyDb database username",
@@ -184,16 +194,17 @@ public interface PubSubToAlloyDbYaml {
   String getUsername();
 
   @TemplateParameter.Password(
-      order = 13,
+      order = 15,
       name = "password",
       optional = false,
       description = "AlloyDb database password",
-      helpText = "Password for AlloyDb authentication.")
+      helpText = "Password for AlloyDb authentication",
+      example = "your-password")
   @Validation.Required
   String getPassword();
 
   @TemplateParameter.Text(
-      order = 14,
+      order = 16,
       name = "connectionProperties",
       optional = true,
       description = "Connection properties for AlloyDb",
@@ -203,17 +214,35 @@ public interface PubSubToAlloyDbYaml {
   String getConnectionProperties();
 
   @TemplateParameter.Text(
-      order = 15,
+      order = 17,
+      name = "network",
+      optional = true,
+      description = "VPC Network name",
+      helpText = "The VPC network where AlloyDB is located.",
+      example = "default")
+  String getNetwork();
+
+  @TemplateParameter.Text(
+      order = 18,
+      name = "subnetwork",
+      optional = true,
+      description = "Subnetwork name",
+      helpText = "The subnetwork for Dataflow workers. Required for custom VPCs.",
+      example = "regions/us-central1/subnetworks/my-subnet")
+  String getSubnetwork();
+
+  @TemplateParameter.Text(
+      order = 19,
       name = "table",
       optional = false,
       description = "Target table name in AlloyDb",
-      helpText = "Name of the table to write to.",
+      helpText = "The name of the table where records will be written",
       example = "my_table")
   @Validation.Required
   String getTable();
 
   @TemplateParameter.Text(
-      order = 16,
+      order = 20,
       name = "query",
       optional = false,
       description = "SQL query for writing records",
@@ -223,7 +252,7 @@ public interface PubSubToAlloyDbYaml {
   String getQuery();
 
   @TemplateParameter.Integer(
-      order = 17,
+      order = 21,
       name = "batchSize",
       optional = true,
       description = "Batch size for write operations",
@@ -232,33 +261,17 @@ public interface PubSubToAlloyDbYaml {
   Integer getBatchSize();
 
   @TemplateParameter.Boolean(
-      order = 18,
+      order = 22,
       name = "autoSharding",
       optional = true,
       description = "Enable autosharding for parallel writes",
-      helpText = "If true, enables using a dynamically determined number of shards to write.")
+      helpText = "If true, enables using a dynamically determined number of shards to write.",
+      example = "True")
+  @Default.Boolean(false)
   Boolean getAutoSharding();
 
   @TemplateParameter.Text(
-      order = 19,
-      name = "network",
-      optional = true,
-      description = "VPC Network name",
-      helpText = "The VPC network where AlloyDB is located.",
-      example = "default")
-  String getNetwork();
-
-  @TemplateParameter.Text(
-      order = 20,
-      name = "subnetwork",
-      optional = true,
-      description = "Subnetwork name",
-      helpText = "The subnetwork for Dataflow workers. Required for custom VPCs.",
-      example = "regions/us-central1/subnetworks/my-subnet")
-  String getSubnetwork();
-
-  @TemplateParameter.Text(
-      order = 21,
+      order = 23,
       name = "outputDeadLetterPubSubTopic",
       optional = false,
       description = "Pub/Sub transformation error topic",
