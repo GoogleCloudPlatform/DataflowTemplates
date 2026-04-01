7090df214bc0e697823885e6dd583f0d850acb70
aasthabharill <aasthabharill4@gmail.com>
2026-04-01 16:38:09 +0530

    initial changes

--- v2/spanner-custom-shard/src/main/java/com/custom/CustomTransformationWithShardForBulkIT.java
+++ v2/spanner-custom-shard/src/main/java/com/custom/CustomTransformationWithShardForBulkIT.java
@@ -43,12 +43,15 @@ public class CustomTransformationWithShardForBulkIT implements ISpannerMigration
       throws InvalidTransformationException {
     if (request.getTableName().equals("Customers")) {
       Map<String, Object> row = new HashMap<>(request.getRequestRow());
-      row.put("full_name", row.get("first_name") + " " + row.get("last_name"));
-      row.put("migration_shard_id", request.getShardId() + "_" + row.get("id"));
-      MigrationTransformationResponse response = new MigrationTransformationResponse(row, false);
+      Map<String, Object> responseRow = new HashMap<>();
+      responseRow.put("full_name", row.get("first_name") + " " + row.get("last_name"));
+      responseRow.put("migration_shard_id", request.getShardId() + "_" + row.get("id"));
+      MigrationTransformationResponse response =
+          new MigrationTransformationResponse(responseRow, false);
       return response;
     } else if (request.getTableName().equals("AllDatatypeTransformation")) {
       Map<String, Object> row = new HashMap<>(request.getRequestRow());
+      Map<String, Object> responseRow = new HashMap<>();
       // Filter event in case "varchar_column" = "example1"
       if (row.get("varchar_column").equals("example1")) {
         return new MigrationTransformationResponse(null, true);
@@ -59,20 +62,20 @@ public class CustomTransformationWithShardForBulkIT implements ISpannerMigration
       }
       // In case of backfill update the values for all the columns in all the rows except the
       // filtered row.
-      row.put("tinyint_column", (Long) row.get("tinyint_column") + 1);
-      row.put("text_column", row.get("text_column") + " append");
-      row.put("int_column", (Long) row.get("int_column") + 1);
-      row.put("bigint_column", (Long) row.get("bigint_column") + 1);
-      row.put("float_column", (double) row.get("float_column") + 1);
-      row.put("double_column", (double) row.get("double_column") + 1);
+      responseRow.put("tinyint_column", (Long) row.get("tinyint_column") + 1);
+      responseRow.put("text_column", row.get("text_column") + " append");
+      responseRow.put("int_column", (Long) row.get("int_column") + 1);
+      responseRow.put("bigint_column", (Long) row.get("bigint_column") + 1);
+      responseRow.put("float_column", (double) row.get("float_column") + 1);
+      responseRow.put("double_column", (double) row.get("double_column") + 1);
       Double value = Double.parseDouble((String) row.get("decimal_column"));
-      row.put("decimal_column", String.valueOf(value + 1));
-      row.put("bool_column", 1);
-      row.put("enum_column", "1");
-      row.put("blob_column", "576f726d64");
-      row.put("binary_column", "0102030405060708090A0B0C0D0E0F1011121314");
-      row.put("bit_column", 13);
-      row.put("year_column", (Long) row.get("year_column") + 1);
+      responseRow.put("decimal_column", String.valueOf(value + 1));
+      responseRow.put("bool_column", 1);
+      responseRow.put("enum_column", "1");
+      responseRow.put("blob_column", "576f726d64");
+      responseRow.put("binary_column", "0102030405060708090A0B0C0D0E0F1011121314");
+      responseRow.put("bit_column", 13);
+      responseRow.put("year_column", (Long) row.get("year_column") + 1);
       try {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         SimpleDateFormat dateTimeFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX");
@@ -80,62 +83,63 @@ public class CustomTransformationWithShardForBulkIT implements ISpannerMigration
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
         calendar.add(Calendar.DAY_OF_MONTH, 1);
-        row.put("date_column", dateFormat.format(calendar.getTime()));
+        responseRow.put("date_column", dateFormat.format(calendar.getTime()));
         Date dateTime = dateTimeFormat.parse((String) row.get("datetime_column"));
         calendar.setTime(dateTime);
         calendar.add(Calendar.SECOND, -1);
-        row.put("datetime_column", dateTimeFormat.format(calendar.getTime()));
+        responseRow.put("datetime_column", dateTimeFormat.format(calendar.getTime()));
         dateTime = dateTimeFormat.parse((String) row.get("timestamp_column"));
         calendar.setTime(dateTime);
         calendar.add(Calendar.SECOND, -1);
-        row.put("timestamp_column", dateTimeFormat.format(calendar.getTime()));
+        responseRow.put("timestamp_column", dateTimeFormat.format(calendar.getTime()));
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
         LocalTime time = LocalTime.parse((String) row.get("time_column"), formatter);
         // Add one hour to the time
         LocalTime newTime = time.plusHours(1);
-        row.put("time_column", newTime.format(formatter));
+        responseRow.put("time_column", newTime.format(formatter));
       } catch (Exception e) {
         throw new InvalidTransformationException(e);
       }
 
       // These types are currently only used bulk ITs for custom jars.
       if (row.containsKey("varbinary_column")) {
-        row.put("varbinary_column", "0102030405060708090A0B0C0D0E0F1011121314");
+        responseRow.put("varbinary_column", "0102030405060708090A0B0C0D0E0F1011121314");
       }
       if (row.containsKey("char_column")) {
-        row.put("char_column", "newchar");
+        responseRow.put("char_column", "newchar");
       }
       if (row.containsKey("longblob_column")) {
-        row.put("longblob_column", "576f726d64");
+        responseRow.put("longblob_column", "576f726d64");
       }
       if (row.containsKey("longtext_column")) {
-        row.put("longtext_column", row.get("longtext_column") + " append");
+        responseRow.put("longtext_column", row.get("longtext_column") + " append");
       }
       if (row.containsKey("mediumblob_column")) {
-        row.put("mediumblob_column", "576f726d64");
+        responseRow.put("mediumblob_column", "576f726d64");
       }
       if (row.containsKey("mediumint_column")) {
-        row.put("mediumint_column", (Long) row.get("mediumint_column") + 1);
+        responseRow.put("mediumint_column", (Long) row.get("mediumint_column") + 1);
       }
       if (row.containsKey("mediumtext_column")) {
-        row.put("mediumtext_column", row.get("mediumtext_column") + " append");
+        responseRow.put("mediumtext_column", row.get("mediumtext_column") + " append");
       }
       if (row.containsKey("set_column")) {
-        row.put("set_column", "v3");
+        responseRow.put("set_column", "v3");
       }
       if (row.containsKey("smallint_column")) {
-        row.put("smallint_column", (Long) row.get("smallint_column") + 1);
+        responseRow.put("smallint_column", (Long) row.get("smallint_column") + 1);
       }
       if (row.containsKey("tinyblob_column")) {
-        row.put("tinyblob_column", "576f726d64");
+        responseRow.put("tinyblob_column", "576f726d64");
       }
       if (row.containsKey("tinytext_column")) {
-        row.put("tinytext_column", row.get("tinytext_column") + " append");
+        responseRow.put("tinytext_column", row.get("tinytext_column") + " append");
       }
       if (row.containsKey("json_column")) {
-        row.put("json_column", "{\"k1\": \"v1\", \"k2\": \"v2\"}");
+        responseRow.put("json_column", "{\"k1\": \"v1\", \"k2\": \"v2\"}");
       }
-      MigrationTransformationResponse response = new MigrationTransformationResponse(row, false);
+      MigrationTransformationResponse response =
+          new MigrationTransformationResponse(responseRow, false);
       return response;
     }
     return new MigrationTransformationResponse(null, false);
