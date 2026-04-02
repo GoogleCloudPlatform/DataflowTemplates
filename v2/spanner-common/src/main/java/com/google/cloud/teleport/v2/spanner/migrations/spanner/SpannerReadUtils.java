b6147d034acd7e1995c120753a73b09d1e99822f
Shreya Khajanchi <khajanchi@google.com>
2026-04-02 09:50:04 +0530

    Added support for PostgreSQL dialect in reverse replication

--- v2/spanner-common/src/main/java/com/google/cloud/teleport/v2/spanner/migrations/spanner/SpannerReadUtils.java
+++ v2/spanner-common/src/main/java/com/google/cloud/teleport/v2/spanner/migrations/spanner/SpannerReadUtils.java
@@ -18,87 +18,125 @@ package com.google.cloud.teleport.v2.spanner.migrations.spanner;
 import com.google.cloud.ByteArray;
 import com.google.cloud.Date;
 import com.google.cloud.Timestamp;
+import com.google.cloud.spanner.Dialect;
 import com.google.cloud.spanner.Key;
 import com.google.cloud.spanner.Statement;
-import com.google.cloud.teleport.v2.spanner.ddl.Column;
+import com.google.cloud.spanner.Value;
 import com.google.cloud.teleport.v2.spanner.ddl.Ddl;
+import com.google.cloud.teleport.v2.spanner.ddl.IndexColumn;
 import com.google.cloud.teleport.v2.spanner.ddl.Table;
 import com.google.cloud.teleport.v2.spanner.type.Type;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class SpannerReadUtils {
-  // TODO: After beam release, use the latest client lib version which supports setting lock
+  // TODO: After beam release, use the latest client lib version which supports
+  // setting lock
   // hints via the read api. SQL string generation should be removed.
   public static Statement generateReadSQLWithExclusiveLock(
       String tableName, List<String> readColumnList, Key primaryKey, Ddl ddl) {
-    String columnNames = String.join(", ", readColumnList);
-    // TODO: Handle json type as PKs.
-    String whereClause =
-        String.join(
-            " AND ",
-            ddl.table(tableName).primaryKeys().stream()
-                .map(col -> col.name() + "=@" + col.name())
-                .collect(Collectors.toList()));
+
+    Dialect dialect = ddl.dialect();
+    boolean isPostgres = dialect == Dialect.POSTGRESQL;
+    Table table = ddl.table(tableName); // Lookup once
+    List<IndexColumn> pks = table.primaryKeys();
+
+    // Quote selected columns for Postgres
+    String columnNames =
+        isPostgres
+            ? readColumnList.stream()
+                .map(c -> quoteIdentifier(c, dialect))
+                .collect(Collectors.joining(", "))
+            : String.join(", ", readColumnList);
+
+    // Generate WHERE clause
+    String whereClause;
+    if (isPostgres) {
+      whereClause =
+          IntStream.range(0, pks.size())
+              .mapToObj(i -> quoteIdentifier(pks.get(i).name(), dialect) + "=$" + (i + 1))
+              .collect(Collectors.joining(" AND "));
+    } else {
+      whereClause =
+          pks.stream()
+              .map(col -> col.name() + "=@" + col.name())
+              .collect(Collectors.joining(" AND "));
+    }
+
+    // Build final SQL string
     String sql =
-        "@{LOCK_SCANNED_RANGES=exclusive} SELECT "
+        (isPostgres ? "/*@ LOCK_SCANNED_RANGES=exclusive */ " : "@{LOCK_SCANNED_RANGES=exclusive} ")
+            + "SELECT "
             + columnNames
             + " FROM "
-            + tableName
+            + quoteIdentifier(tableName, dialect)
             + " WHERE "
             + whereClause;
 
     Statement.Builder stmtBuilder = Statement.newBuilder(sql);
     int i = 0;
     for (Object value : primaryKey.getParts()) {
-      Table table = ddl.table(tableName);
-      String colName = table.primaryKeys().get(i).name();
-      Column key = table.column(colName);
-      Type keyColType = key.type();
+      String colName = pks.get(i).name();
+      Type keyColType = table.column(colName).type();
 
-      switch (keyColType.getCode()) {
-        case BOOL:
-        case PG_BOOL:
-          stmtBuilder.bind(colName).to((Boolean) value);
-          break;
-        case INT64:
-        case PG_INT8:
-          stmtBuilder.bind(colName).to((Long) value);
-          break;
-        case FLOAT64:
-        case PG_FLOAT8:
-          stmtBuilder.bind(colName).to((Double) value);
-          break;
-        case STRING:
-        case PG_VARCHAR:
-        case PG_TEXT:
-        case JSON:
-        case PG_JSONB:
-          stmtBuilder.bind(colName).to((String) value);
-          break;
-        case NUMERIC:
-        case PG_NUMERIC:
-          stmtBuilder.bind(colName).to((BigDecimal) value);
-          break;
-        case BYTES:
-        case PG_BYTEA:
-          stmtBuilder.bind(colName).to((ByteArray) value);
-          break;
-        case TIMESTAMP:
-        case PG_COMMIT_TIMESTAMP:
-        case PG_TIMESTAMPTZ:
-          stmtBuilder.bind(colName).to((Timestamp) value);
-          break;
-        case DATE:
-        case PG_DATE:
-          stmtBuilder.bind(colName).to((Date) value);
-          break;
-        default:
-          throw new IllegalArgumentException("Unsupported type: " + keyColType);
-      }
+      String bindName = isPostgres ? "p" + (i + 1) : colName;
+      bindValue(stmtBuilder, bindName, keyColType, value);
+
       i++;
     }
     return stmtBuilder.build();
+  }
+
+  private static void bindValue(
+      Statement.Builder stmtBuilder, String bindName, Type type, Object value) {
+    // TODO: Handle json type as PKs.
+    switch (type.getCode()) {
+      case BOOL:
+      case PG_BOOL:
+        stmtBuilder.bind(bindName).to((Boolean) value);
+        break;
+      case INT64:
+      case PG_INT8:
+        stmtBuilder.bind(bindName).to((Long) value);
+        break;
+      case FLOAT64:
+      case PG_FLOAT8:
+        stmtBuilder.bind(bindName).to((Double) value);
+        break;
+      case STRING:
+      case PG_VARCHAR:
+      case PG_TEXT:
+      case JSON:
+      case PG_JSONB:
+        stmtBuilder.bind(bindName).to((String) value);
+        break;
+      case NUMERIC:
+        stmtBuilder.bind(bindName).to((BigDecimal) value);
+        break;
+      case PG_NUMERIC:
+        stmtBuilder.bind(bindName).to(Value.pgNumeric(value.toString()));
+        break;
+      case BYTES:
+      case PG_BYTEA:
+        stmtBuilder.bind(bindName).to((ByteArray) value);
+        break;
+      case TIMESTAMP:
+      case PG_COMMIT_TIMESTAMP:
+      case PG_TIMESTAMPTZ:
+        stmtBuilder.bind(bindName).to((Timestamp) value);
+        break;
+      case DATE:
+      case PG_DATE:
+        stmtBuilder.bind(bindName).to((Date) value);
+        break;
+      default:
+        throw new IllegalArgumentException("Unsupported type: " + type);
+    }
+  }
+
+  private static String quoteIdentifier(String name, Dialect dialect) {
+    return (dialect == Dialect.POSTGRESQL) ? "\"" + name + "\"" : name;
   }
 }
