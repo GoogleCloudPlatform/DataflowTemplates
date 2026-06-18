c68cc10334d0a4e53f7ce81dfaf8561ce721a187
Aditya Bharadwaj <adibh@google.com>
2026-06-18 10:13:41 +0000

    first checking for reverse replication source seperation

---
+++ v2/spanner-to-sourcedb/src/main/java/com/google/cloud/teleport/v2/templates/source/postgres/PostgreSQLSourceConnector.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.cloud.teleport.v2.templates.source.postgres;
+
+import com.google.cloud.teleport.v2.spanner.migrations.connection.ConnectionHelperRequest;
+import com.google.cloud.teleport.v2.spanner.migrations.connection.IConnectionHelper;
+import com.google.cloud.teleport.v2.spanner.migrations.connection.JdbcConnectionHelper;
+import com.google.cloud.teleport.v2.spanner.migrations.shard.Shard;
+import com.google.cloud.teleport.v2.templates.dbutils.dao.source.IDao;
+import com.google.cloud.teleport.v2.templates.dbutils.dao.source.JdbcDao;
+import com.google.cloud.teleport.v2.templates.dbutils.dml.IDMLGenerator;
+import com.google.cloud.teleport.v2.templates.dbutils.processor.ISourceConnector;
+import java.util.List;
+
+public class PostgreSQLSourceConnector implements ISourceConnector {
+
+  private final IConnectionHelper connectionHelper = new JdbcConnectionHelper();
+
+  @Override
+  public IDMLGenerator getDmlGenerator() {
+    return new PostgreSQLDMLGenerator();
+  }
+
+  @Override
+  public IConnectionHelper getConnectionHelper() {
+    return connectionHelper;
+  }
+
+  @Override
+  public String getConnectionUrl(Shard shard) {
+    return "jdbc:postgresql://" + shard.getHost() + ":" + shard.getPort() + "/" + shard.getDbName();
+  }
+
+  @Override
+  public IDao getDao(Shard shard) {
+    return new JdbcDao(getConnectionUrl(shard), shard.getUserName(), getConnectionHelper());
+  }
+
+  @Override
+  public void initConnectionHelper(List<Shard> shards, int maxConnections) {
+    if (!connectionHelper.isConnectionPoolInitialized()) {
+      ConnectionHelperRequest request =
+          new ConnectionHelperRequest(
+              shards, null, maxConnections, "org.postgresql.Driver", null, "jdbc:postgresql://");
+      connectionHelper.init(request);
+    }
+  }
+}
