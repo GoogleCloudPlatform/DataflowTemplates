f547a39e41ec58672f4e3a20c6ded080c00c4c00
Szabolcs Rozsnyai <s.rozsnyai@gmail.com>
2026-06-22 18:45:27 +0200

    docs: restore accidentally removed TODOs and commentary in InputRecordProcessor

--- v2/spanner-to-sourcedb/src/main/java/com/google/cloud/teleport/v2/templates/dbutils/processor/InputRecordProcessor.java
+++ v2/spanner-to-sourcedb/src/main/java/com/google/cloud/teleport/v2/templates/dbutils/processor/InputRecordProcessor.java
@@ -150,6 +150,16 @@ public class InputRecordProcessor {
         return true; // filtered
       }
 
+      // TODO we need to handle it as proper Interface Level as of now we have handle Prepared
+      // TODO Statement and Raw Statement Differently
+      /*
+       * TODO:
+       * Note: The `SOURCE_CASSANDRA` case not covered in the unit tests.
+       * Answer: Currently, we have implemented unit tests for the Input Record Processor under the SourceWrittenFn.
+       *         These tests cover the majority of scenarios, but they are tightly coupled with the existing code.
+       *         Adding unit tests for SOURCE_CASSANDRA would require a significant refactoring of the entire unit test file.
+       *         Given the current implementation, such refactoring is deemed unnecessary as it would not provide substantial value or impact.
+       */
       switch (source) {
         case SOURCE_CASSANDRA:
           dao.write(dmlGeneratorResponse, null);
