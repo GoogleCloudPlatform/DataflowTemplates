ccc0d67f27ddae38fce46aab74ae5fdac30d18e7
Aditya Bharadwaj <adibh@google.com>
2026-06-29 09:33:31 +0000

    spotless fixes

--- v2/spanner-to-sourcedb/src/test/java/com/google/cloud/teleport/v2/templates/source/spanner/SpannerSpToSrcSourceConnectorTest.java
+++ v2/spanner-to-sourcedb/src/test/java/com/google/cloud/teleport/v2/templates/source/spanner/SpannerSpToSrcSourceConnectorTest.java
@@ -16,9 +16,9 @@
 package com.google.cloud.teleport.v2.templates.source.spanner;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
