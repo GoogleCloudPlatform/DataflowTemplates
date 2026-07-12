06d6601b73a46adaf4ab0e9363e39faa583026aa
shreyakhajanchi <92910380+shreyakhajanchi@users.noreply.github.com>
2026-07-12 12:08:27 +0530

    Fix the flaky load tests (#4011)
    * increasing innodb pool and log capacity
    
    * minor change
    
    ---------
    
    Co-authored-by: shreyakhajanchinew <khajanchi+@google.com>

--- v2/datastream-to-spanner/src/test/java/com/google/cloud/teleport/v2/templates/loadtesting/DataStreamToSpannerBlobTableLT.java
+++ v2/datastream-to-spanner/src/test/java/com/google/cloud/teleport/v2/templates/loadtesting/DataStreamToSpannerBlobTableLT.java
@@ -39,7 +39,7 @@ public class DataStreamToSpannerBlobTableLT extends DataStreamToSpannerLTBase {
 
     // Setup Datastream
     String hostIp =
-        secretClient.accessSecret("projects/269744978479/secrets/blob-table-ip/versions/2");
+        secretClient.accessSecret("projects/269744978479/secrets/blob-table-ip/versions/1");
     String username =
         secretClient.accessSecret("projects/269744978479/secrets/blob-table-user/versions/1");
     String password =
