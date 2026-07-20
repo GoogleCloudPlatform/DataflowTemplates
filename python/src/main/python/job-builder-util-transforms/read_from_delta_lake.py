5be2a6df30d240babe614ceb6f7b2b92c183d4b4
Derrick Williams <derrickaw@google.com>
2026-07-20 17:14:52 +0000

    point delta lake to the created expansion service jar

--- python/src/main/python/job-builder-util-transforms/read_from_delta_lake.py
+++ python/src/main/python/job-builder-util-transforms/read_from_delta_lake.py
@@ -1,9 +1,11 @@
 """Module containing transforms to read data from Delta Lake tables."""
 
 from typing import Mapping, Optional
+from apache_beam.options.pipeline_options import CrossLanguageOptions
 from apache_beam.transforms import PTransform
 from apache_beam.transforms import managed
 from apache_beam.transforms.external import BeamJarExpansionService
+from apache_beam.transforms.external import JavaJarExpansionService
 from apache_beam.transforms.external import SchemaAwareExternalTransform
 
 DELTA_LAKE_READ_URN = "beam:schematransform:org.apache.beam:delta_lake_read:v1"
@@ -51,11 +53,20 @@ class ReadFromDeltaLake(PTransform):
     ):
       return pbegin | managed.Read(delta_source, config=config)
     else:
+      options = pbegin.pipeline.options
+      beam_services = options.view_as(CrossLanguageOptions).beam_services or {}
+      if 'sdks:java:io:expansion-service:shadowJar' in beam_services:
+        expansion_service = BeamJarExpansionService(
+            'sdks:java:io:expansion-service:shadowJar'
+        )
+      else:
+        expansion_service = JavaJarExpansionService(
+            'https://storage.googleapis.com/dataflow-templates/extra-python-packages/2026-07-20/expansion-service-custom-0.2.0.jar'
+        )
+
       return pbegin | SchemaAwareExternalTransform(
           identifier=DELTA_LAKE_READ_URN,
-          expansion_service=BeamJarExpansionService(
-              'sdks:java:io:expansion-service:shadowJar'
-          ),
+          expansion_service=expansion_service,
           rearrange_based_on_discovery=True,
           **config,
       )
