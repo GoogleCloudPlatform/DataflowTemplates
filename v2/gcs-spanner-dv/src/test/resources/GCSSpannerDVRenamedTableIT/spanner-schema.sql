0c1500588c4e2cb84a727e99644a3919be3470af
aasthabharill <aasthabharill4@gmail.com>
2026-07-23 06:18:49 +0000

    initial changes

---
+++ v2/gcs-spanner-dv/src/test/resources/GCSSpannerDVRenamedTableIT/spanner-schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Members (
+    user_id INT64 NOT NULL,
+    event_id STRING(MAX) NOT NULL,
+    full_name STRING(MAX),
+    age INT64,
+    created_at TIMESTAMP,
+) PRIMARY KEY (user_id, event_id);
