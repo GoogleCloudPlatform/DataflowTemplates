3e36b9a793a0982c96413e548747e0841bf0f987
aasthabharill <77983396+aasthabharill@users.noreply.github.com>
2026-07-27 17:48:54 +0530

    Run Validation ITs using DirectRunner (#4057)
    * initial changes
    
    * fix
    
    * fix
    
    * gemini-review
    
    * improve code
    
    * add DirectRunner to other tests

--- v2/gcs-spanner-dv/src/test/java/com/google/cloud/teleport/v2/templates/GCSSpannerDVCoreMatchingIT.java
+++ v2/gcs-spanner-dv/src/test/java/com/google/cloud/teleport/v2/templates/GCSSpannerDVCoreMatchingIT.java
@@ -16,6 +16,7 @@
 package com.google.cloud.teleport.v2.templates;
 
 import com.google.cloud.spanner.Mutation;
+import com.google.cloud.teleport.metadata.DirectRunnerTest;
 import com.google.cloud.teleport.metadata.TemplateIntegrationTest;
 import com.google.cloud.teleport.v2.templates.GCSSpannerDVTestAsserts.MismatchedRecordDto;
 import com.google.cloud.teleport.v2.templates.GCSSpannerDVTestAsserts.TableValidationStatsDto;
@@ -40,7 +41,7 @@ import org.junit.runners.JUnit4;
  * (Spanner) across multiple tables, and correctly report on matches, missing rows, and data
  * discrepancies.
  */
-@Category(TemplateIntegrationTest.class)
+@Category({TemplateIntegrationTest.class, DirectRunnerTest.class})
 @RunWith(JUnit4.class)
 @TemplateIntegrationTest(GCSSpannerDV.class)
 public class GCSSpannerDVCoreMatchingIT extends GCSSpannerDVITBase {
