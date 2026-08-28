bf3e10cbba33718642f79039f7c3c3c0c6e5fb81
Derrick Williams <derrickaw@google.com>
2026-08-28 14:15:02 +0000

    address Delta client comment

--- yaml/src/test/java/com/google/cloud/teleport/templates/yaml/DeltaLakeToLakehouseYamlIT.java
+++ yaml/src/test/java/com/google/cloud/teleport/templates/yaml/DeltaLakeToLakehouseYamlIT.java
@@ -22,16 +22,38 @@ import static org.junit.Assert.assertEquals;
 import com.google.cloud.teleport.it.iceberg.IcebergResourceManager;
 import com.google.cloud.teleport.metadata.SkipDirectRunnerTest;
 import com.google.cloud.teleport.metadata.TemplateIntegrationTest;
+import io.delta.kernel.DataWriteContext;
+import io.delta.kernel.Operation;
+import io.delta.kernel.Table;
+import io.delta.kernel.Transaction;
+import io.delta.kernel.TransactionBuilder;
+import io.delta.kernel.TransactionCommitResult;
+import io.delta.kernel.data.ColumnVector;
+import io.delta.kernel.data.ColumnarBatch;
+import io.delta.kernel.data.FilteredColumnarBatch;
+import io.delta.kernel.defaults.engine.DefaultEngine;
+import io.delta.kernel.defaults.internal.data.DefaultColumnarBatch;
+import io.delta.kernel.engine.Engine;
+import io.delta.kernel.types.DataType;
+import io.delta.kernel.types.DoubleType;
+import io.delta.kernel.types.StringType;
+import io.delta.kernel.types.StructType;
+import io.delta.kernel.utils.CloseableIterable;
+import io.delta.kernel.utils.CloseableIterator;
+import io.delta.kernel.utils.DataFileStatus;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 import org.apache.beam.it.common.PipelineLauncher.LaunchConfig;
 import org.apache.beam.it.common.PipelineLauncher.LaunchInfo;
 import org.apache.beam.it.common.PipelineOperator;
 import org.apache.beam.it.common.utils.ResourceManagerUtils;
 import org.apache.beam.it.gcp.TemplateTestBase;
-import org.apache.beam.it.gcp.artifacts.utils.ParquetTestUtil;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.iceberg.Schema;
 import org.apache.iceberg.data.Record;
 import org.apache.iceberg.types.Types;
@@ -74,45 +96,145 @@ public class DeltaLakeToLakehouseYamlIT extends TemplateTestBase {
   }
 
   @Test
-  public void testDeltaLakeToLakehouse() throws IOException {
+  public void testDeltaLakeToLakehouse() throws Exception {
     // 1. Arrange: Create Delta Lake source table in GCS
     String deltaTableDir = "delta-table";
-    org.apache.avro.Schema avroSchema =
-        new org.apache.avro.Schema.Parser()
-            .parse(
-                "{\"type\":\"record\",\"name\":\"test_record\",\"fields\":["
-                    + "{\"name\":\"id\",\"type\":\"string\"},"
-                    + "{\"name\":\"state\",\"type\":\"string\"},"
-                    + "{\"name\":\"price\",\"type\":\"double\"}"
-                    + "]}");
-    org.apache.avro.generic.GenericRecord avroRecord =
-        new org.apache.avro.generic.GenericData.Record(avroSchema);
-    avroRecord.put("id", "007");
-    avroRecord.put("state", "CA");
-    avroRecord.put("price", 26.23);
-    byte[] parquetBytes = ParquetTestUtil.createParquetFile(avroSchema, List.of(avroRecord));
-
-    // Upload data Parquet file
-    gcsClient.createArtifact(deltaTableDir + "/part-00000.parquet", parquetBytes);
-
-    // Create and upload Delta Lake transaction log
-    String commitContent =
-        "{\"protocol\":{\"minReaderVersion\":1,\"minWriterVersion\":2}}\n"
-            + "{\"metaData\":{\"id\":\"test-id\",\"format\":{\"provider\":\"parquet\",\"options\":{}},"
-            + "\"schemaString\":\"{\\\"type\\\":\\\"struct\\\",\\\"fields\\\":["
-            + "{\\\"name\\\":\\\"id\\\",\\\"type\\\":\\\"string\\\",\\\"nullable\\\":true,\\\"metadata\\\":{}},"
-            + "{\\\"name\\\":\\\"state\\\",\\\"type\\\":\\\"string\\\",\\\"nullable\\\":true,\\\"metadata\\\":{}},"
-            + "{\\\"name\\\":\\\"price\\\",\\\"type\\\":\\\"double\\\",\\\"nullable\\\":true,\\\"metadata\\\":{}}"
-            + "]}\",\"partitionColumns\":[],\"configuration\":{},\"createdAt\":123456789}}\n"
-            + "{\"add\":{\"path\":\"part-00000.parquet\",\"partitionValues\":{},\"size\":"
-            + parquetBytes.length
-            + ","
-            + "\"modificationTime\":123456789,\"dataChange\":true}}";
-
-    gcsClient.createArtifact(
-        deltaTableDir + "/_delta_log/00000000000000000000.json", commitContent);
-
     String deltaTableGcsPath = getGcsPath(deltaTableDir);
+
+    Configuration configuration = new Configuration();
+    getGcsHadoopConfig().forEach(configuration::set);
+    Engine engine = DefaultEngine.create(configuration);
+    Table table = Table.forPath(engine, deltaTableGcsPath);
+
+    StructType deltaSchema =
+        new StructType()
+            .add("id", StringType.STRING)
+            .add("state", StringType.STRING)
+            .add("price", DoubleType.DOUBLE);
+
+    TransactionBuilder txnBuilder =
+        table.createTransactionBuilder(engine, "DeltaLakeToLakehouseYamlIT", Operation.CREATE_TABLE);
+    txnBuilder = txnBuilder.withSchema(engine, deltaSchema);
+    Transaction txn = txnBuilder.build(engine);
+    io.delta.kernel.data.Row txnState = txn.getTransactionState(engine);
+
+    ColumnVector idVector =
+        new ColumnVector() {
+          @Override
+          public DataType getDataType() {
+            return StringType.STRING;
+          }
+
+          @Override
+          public int getSize() {
+            return 1;
+          }
+
+          @Override
+          public void close() {}
+
+          @Override
+          public boolean isNullAt(int rowId) {
+            return false;
+          }
+
+          @Override
+          public String getString(int rowId) {
+            return "007";
+          }
+        };
+
+    ColumnVector stateVector =
+        new ColumnVector() {
+          @Override
+          public DataType getDataType() {
+            return StringType.STRING;
+          }
+
+          @Override
+          public int getSize() {
+            return 1;
+          }
+
+          @Override
+          public void close() {}
+
+          @Override
+          public boolean isNullAt(int rowId) {
+            return false;
+          }
+
+          @Override
+          public String getString(int rowId) {
+            return "CA";
+          }
+        };
+
+    ColumnVector priceVector =
+        new ColumnVector() {
+          @Override
+          public DataType getDataType() {
+            return DoubleType.DOUBLE;
+          }
+
+          @Override
+          public int getSize() {
+            return 1;
+          }
+
+          @Override
+          public void close() {}
+
+          @Override
+          public boolean isNullAt(int rowId) {
+            return false;
+          }
+
+          @Override
+          public double getDouble(int rowId) {
+            return 26.23;
+          }
+        };
+
+    ColumnVector[] vectors = new ColumnVector[] {idVector, stateVector, priceVector};
+    ColumnarBatch columnarBatch = new DefaultColumnarBatch(1, deltaSchema, vectors);
+    FilteredColumnarBatch filteredBatch =
+        new FilteredColumnarBatch(columnarBatch, Optional.empty());
+
+    CloseableIterator<FilteredColumnarBatch> data =
+        io.delta.kernel.internal.util.Utils.toCloseableIterator(
+            Collections.singletonList(filteredBatch).iterator());
+
+    CloseableIterator<FilteredColumnarBatch> physicalData =
+        Transaction.transformLogicalData(engine, txnState, data, Collections.emptyMap());
+
+    DataWriteContext writeContext =
+        Transaction.getWriteContext(engine, txnState, Collections.emptyMap());
+
+    CloseableIterator<DataFileStatus> dataFiles =
+        engine
+            .getParquetHandler()
+            .writeParquetFiles(
+                writeContext.getTargetDirectory(),
+                physicalData,
+                writeContext.getStatisticsColumns());
+
+    CloseableIterator<io.delta.kernel.data.Row> dataActions =
+        Transaction.generateAppendActions(engine, txnState, dataFiles, writeContext);
+
+    List<io.delta.kernel.data.Row> addActionsList = new ArrayList<>();
+    while (dataActions.hasNext()) {
+      addActionsList.add(dataActions.next());
+    }
+
+    CloseableIterable<io.delta.kernel.data.Row> dataActionsIterable =
+        CloseableIterable.inMemoryIterable(
+            io.delta.kernel.internal.util.Utils.toCloseableIterator(addActionsList.iterator()));
+
+    TransactionCommitResult commitResult = txn.commit(engine, dataActionsIterable);
+    if (commitResult.getVersion() < 0) {
+      throw new RuntimeException("Table creation/write failed");
+    }
 
     // 2. Arrange: Create destination Lakehouse table
     icebergResourceManager.createNamespace(namespace);
