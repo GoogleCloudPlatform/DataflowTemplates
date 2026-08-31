256bdf5e39a5741a292ead286c4313654c4c9da4
Derrick Williams <derrickaw@google.com>
2026-08-31 14:22:30 -0400

    fix java generation (#4197)

--- yaml/src/main/java/com/google/cloud/teleport/templates/yaml/PubSubToBigQueryYaml.java
+++ yaml/src/main/java/com/google/cloud/teleport/templates/yaml/PubSubToBigQueryYaml.java
@@ -138,7 +138,7 @@ public interface PubSubToBigQueryYaml {
       optional = false,
       description = "BigQuery table",
       helpText =
-          "BigQuery table location to write the output to or read from. The name  should be in the format <project>:<dataset>.<table_name>`. For write,  the table's schema must match input objects.",
+          "BigQuery table location to write the output to or read from. The name  should be in the format <project>:<dataset>.<table_name>. For write,  the table's schema must match input objects.",
       example = "")
   @Validation.Required
   String getTable();
