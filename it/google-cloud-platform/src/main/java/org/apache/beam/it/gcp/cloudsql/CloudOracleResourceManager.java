a958c785ad071fa32f5e476f20dcdace77984ca4
Derrick Williams <derrickaw@google.com>
2026-09-08 15:28:04 +0000

    revert resource manager changes, keep only ignore

--- it/google-cloud-platform/src/main/java/org/apache/beam/it/gcp/cloudsql/CloudOracleResourceManager.java
+++ it/google-cloud-platform/src/main/java/org/apache/beam/it/gcp/cloudsql/CloudOracleResourceManager.java
@@ -39,8 +39,6 @@ public class CloudOracleResourceManager extends CloudSqlResourceManager {
   private static final String DEFAULT_SYSTEM_IDENTIFIER = "xe";
 
   private static final int DEFAULT_ORACLE_PORT = 1521;
-  private static final String DEFAULT_ORACLE_USERNAME = "system";
-  private static final String DEFAULT_ORACLE_PASSWORD = "oracle";
 
   private String systemIdentifier;
 
@@ -112,16 +110,11 @@ public class CloudOracleResourceManager extends CloudSqlResourceManager {
     }
 
     @Override
-    protected String getDefaultUsername() {
-      return DEFAULT_ORACLE_USERNAME;
-    }
-
-    @Override
     protected void configureUsername() {
       if (System.getProperty("cloudOracleUsername") != null) {
         this.setUsername(System.getProperty("cloudOracleUsername"));
       } else {
-        this.setUsername(DEFAULT_ORACLE_USERNAME);
+        super.configureUsername();
       }
     }
 
@@ -129,12 +122,9 @@ public class CloudOracleResourceManager extends CloudSqlResourceManager {
     protected void configurePassword() {
       if (System.getProperty("cloudOraclePassword") != null) {
         this.setPassword(System.getProperty("cloudOraclePassword"));
-      } else if (System.getProperty("cloudOracleSysPassword") != null) {
-        this.setPassword(System.getProperty("cloudOracleSysPassword"));
       } else {
-        this.setPassword(DEFAULT_ORACLE_PASSWORD);
+        super.configurePassword();
       }
-      this.useStaticContainer();
     }
 
     public Builder setSystemIdentifier(String systemIdentifier) {
