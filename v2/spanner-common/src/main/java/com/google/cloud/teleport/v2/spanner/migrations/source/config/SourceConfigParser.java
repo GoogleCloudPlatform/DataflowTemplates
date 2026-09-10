42b7d5fd9fd86004ba4a5cfe2f926f097a3974cb
Pratick Chokhani <pratick.chokhani@gmail.com>
2026-09-10 13:03:57 +0530

    empty commit

--- v2/spanner-common/src/main/java/com/google/cloud/teleport/v2/spanner/migrations/source/config/SourceConfigParser.java
+++ v2/spanner-common/src/main/java/com/google/cloud/teleport/v2/spanner/migrations/source/config/SourceConfigParser.java
@@ -92,7 +92,6 @@ public class SourceConfigParser {
         String astraFileContent = FileLoader.readConfigFilePath(sourceConfigFilePath);
         Map<String, Object> astraConfigMap = parseConfigToConfigMap(astraFileContent);
         return mapper.convertValue(astraConfigMap, AstraConnectionConfig.class);
-      case SQLSERVER:
       case ORACLE:
       case MYSQL:
       case PG:
