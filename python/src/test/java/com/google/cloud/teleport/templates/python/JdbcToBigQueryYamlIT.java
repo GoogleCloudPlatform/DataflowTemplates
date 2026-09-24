e8cffe65cf9e5cb47b683d1dd2f944d20ffa3992
Derrick Williams <derrickaw@google.com>
2026-09-24 14:35:58 +0000

    remove jdbctobigqueryIT test with drivers for postgres as not needed anymore

--- python/src/test/java/com/google/cloud/teleport/templates/python/JdbcToBigQueryYamlIT.java
+++ python/src/test/java/com/google/cloud/teleport/templates/python/JdbcToBigQueryYamlIT.java
@@ -40,7 +40,7 @@ import org.apache.beam.it.common.PipelineLauncher.LaunchConfig;
 import org.apache.beam.it.common.PipelineLauncher.LaunchInfo;
 import org.apache.beam.it.common.PipelineOperator.Result;
 import org.apache.beam.it.common.utils.ResourceManagerUtils;
-import org.apache.beam.it.gcp.JDBCBaseIT;
+import org.apache.beam.it.gcp.TemplateTestBase;
 import org.apache.beam.it.gcp.bigquery.BigQueryResourceManager;
 import org.apache.beam.it.jdbc.JDBCResourceManager;
 import org.apache.beam.it.jdbc.PostgresResourceManager;
@@ -56,17 +56,13 @@ import org.junit.runners.JUnit4;
 @Category(TemplateIntegrationTest.class)
 @TemplateIntegrationTest(YAMLTemplate.class)
 @RunWith(JUnit4.class)
-public class JdbcToBigQueryYamlIT extends JDBCBaseIT {
+public class JdbcToBigQueryYamlIT extends TemplateTestBase {
 
   private PostgresResourceManager postgresResourceManager;
   private BigQueryResourceManager bigQueryResourceManager;
 
   private static final String YAML_PIPELINE = "JdbcToBigQueryYamlIT.yaml";
-  private static final String YAML_PIPELINE_WITHOUT_DRIVER_JARS =
-      "JdbcToBigQueryYamlITWithoutDriverJars.yaml";
   private static final String YAML_PIPELINE_GCS_PATH = "input/" + YAML_PIPELINE;
-  private static final String YAML_PIPELINE_WITHOUT_DRIVER_JARS_GCS_PATH =
-      "input/" + YAML_PIPELINE_WITHOUT_DRIVER_JARS;
   private static final String JDBC_TABLE_NAME = "source_table";
   private static final int ROW_COUNT = 10;
 
@@ -76,17 +72,11 @@ public class JdbcToBigQueryYamlIT extends JDBCBaseIT {
 
   @Before
   public void setUp() throws IOException {
-    // We need a real JDBC URL for the test, so we use the one from JDBCBaseIT constants
-    // For a real test against a specific DB, ensure its Docker container is running or accessible.
-    // For this example, we'll use Postgres.
     postgresResourceManager = PostgresResourceManager.builder(testName).build();
     bigQueryResourceManager =
         BigQueryResourceManager.builder(testName, PROJECT, credentials).build();
 
     gcsClient.createArtifact(YAML_PIPELINE_GCS_PATH, readYamlPipelineFile(YAML_PIPELINE));
-    gcsClient.createArtifact(
-        YAML_PIPELINE_WITHOUT_DRIVER_JARS_GCS_PATH,
-        readYamlPipelineFile(YAML_PIPELINE_WITHOUT_DRIVER_JARS));
   }
 
   @After
@@ -138,87 +128,13 @@ public class JdbcToBigQueryYamlIT extends JDBCBaseIT {
                 + "\"JDBC_USERNAME\": \"%s\", "
                 + "\"JDBC_PASSWORD\": \"%s\", "
                 + "\"JDBC_QUERY\": \"%s\", "
-                + "\"BQ_TABLE_SPEC\": \"%s\", "
-                + "\"JDBC_DRIVER_JARS\": \"%s\", "
-                + "\"JDBC_DRIVER_CLASS_NAME\": \"%s\""
-                + "}",
-            jdbcUrl,
-            username,
-            password,
-            query,
-            toTableSpecStandard(table),
-            postgresDriverGCSPath(),
-            POSTGRES_DRIVER);
-
-    LaunchConfig.Builder options =
-        LaunchConfig.builder(testName, specPath)
-            .addParameter("yaml_pipeline_file", getGcsPath(YAML_PIPELINE_GCS_PATH))
-            .addParameter("jinja_variables", jinjaVars);
-
-    // Act
-    LaunchInfo info = launchTemplate(options);
-    assertThatPipeline(info).isRunning();
-
-    Result result = pipelineOperator().waitUntilDone(createConfig(info));
-
-    // Assert
-    assertThatResult(result).isLaunchFinished();
-    TableResult actualRecords = bigQueryResourceManager.readTable(table);
-    assertThatBigQueryRecords(actualRecords).hasRecordsUnordered(expectedData);
-  }
-
-  @Test
-  public void testJdbcToBigQueryWithoutDriverJars() throws IOException {
-    // Arrange JDBC source
-    HashMap<String, String> columns = new HashMap<>();
-    columns.put(ROW_ID, "INTEGER NOT NULL");
-    columns.put(NAME, "VARCHAR(100)");
-    columns.put(AGE, "INTEGER NOT NULL");
-    JDBCResourceManager.JDBCSchema jdbcSchema = new JDBCResourceManager.JDBCSchema(columns, ROW_ID);
-    postgresResourceManager.createTable(JDBC_TABLE_NAME, jdbcSchema);
-
-    // Write data to JDBC before pipeline starts (for batch)
-    List<Map<String, Object>> expectedData = new ArrayList<>();
-    Random random = new Random();
-    for (int i = 1; i <= ROW_COUNT; i++) {
-      Map<String, Object> row =
-          ImmutableMap.of(
-              ROW_ID, i, NAME, RandomStringUtils.randomAlphabetic(10), AGE, random.nextInt(100));
-      expectedData.add(row);
-    }
-    postgresResourceManager.write(JDBC_TABLE_NAME, expectedData);
-
-    // Arrange BigQuery destination
-    List<Field> bqSchemaFields =
-        Arrays.asList(
-            Field.of(ROW_ID, StandardSQLTypeName.INT64),
-            Field.of(NAME, StandardSQLTypeName.STRING),
-            Field.of(AGE, StandardSQLTypeName.INT64));
-    Schema bqSchema = Schema.of(bqSchemaFields);
-    bigQueryResourceManager.createDataset(REGION);
-    TableId table = bigQueryResourceManager.createTable(testName, bqSchema);
-
-    // Prepare Jinja variables
-    String jdbcUrl = postgresResourceManager.getUri();
-    String username = postgresResourceManager.getUsername();
-    String password = postgresResourceManager.getPassword();
-    String query = String.format("SELECT %s, %s, %s FROM %s", ROW_ID, NAME, AGE, JDBC_TABLE_NAME);
-
-    String jinjaVars =
-        String.format(
-            "{"
-                + "\"JDBC_URL\": \"%s\", "
-                + "\"JDBC_USERNAME\": \"%s\", "
-                + "\"JDBC_PASSWORD\": \"%s\", "
-                + "\"JDBC_QUERY\": \"%s\", "
                 + "\"BQ_TABLE_SPEC\": \"%s\""
                 + "}",
             jdbcUrl, username, password, query, toTableSpecStandard(table));
 
     LaunchConfig.Builder options =
         LaunchConfig.builder(testName, specPath)
-            .addParameter(
-                "yaml_pipeline_file", getGcsPath(YAML_PIPELINE_WITHOUT_DRIVER_JARS_GCS_PATH))
+            .addParameter("yaml_pipeline_file", getGcsPath(YAML_PIPELINE_GCS_PATH))
             .addParameter("jinja_variables", jinjaVars);
 
     // Act
